b5a1e0c8a6f06ae774e3d461f8d499a8049d9383
Gilles van den Hoven <gilles0181@gmail.com>
2006-06-27 21:26:25 +0000

    bah, forgot to test it, this is the fix

--- form/form.js
+++ form/form.js
@@ -198,9 +198,7 @@ $.fn.serialize = function() {
 		if ((p == 'SELECT' || p == 'OPTGROUP') && !this.selected) return;
 
 		var n = this.name;
-		if (!n && p == 'OPTGROUP' && (par = par.parentNode))
-			n = par.name;
-
+		if (!n) n = (p == 'OPTGROUP') ? par.parentNode.name : par.name;
 		if (n == undefined) return;
 
 		a.push({name: n, value: this.value});
