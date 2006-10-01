644256759f959b30c6e041adb46714675421fb5d
Jörn Zaefferer <joern.zaefferer@gmail.com>
2006-10-01 17:40:12 +0000

    Added fix for bug #222

--- src/event/event.js
+++ src/event/event.js
@@ -80,7 +80,7 @@ jQuery.fn.extend({
 			var p = (e.type == "mouseover" ? e.fromElement : e.toElement) || e.relatedTarget;
 	
 			// Traverse up the tree
-			while ( p && p != this ) p = p.parentNode;
+			while ( p && p != this ) try { p = p.parentNode } catch(e) { p = null; };
 			
 			// If we actually just moused on to a sub-element, ignore it
 			if ( p == this ) return false;
