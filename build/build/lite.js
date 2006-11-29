c136717119ec986be67848ec45b56e23d77d51fd
Mike Alsup <malsup@gmail.com>
2006-11-29 23:25:33 +0000

    Fixed regEx to work with Unix an PC files.

--- build/build/lite.js
+++ build/build/lite.js
@@ -1,6 +1,6 @@
 load("build/js/writeFile.js");
 
-var blockMatch = /\s*\/\*\*\s*((.|\n)*?)\s*\*\/\n*/g;
+var blockMatch = /\s*\/\*\*\s*((.|\n|\r\n)*?)\s*\*\/\n*/g;
 var f = readFile(arguments[0]).replace( blockMatch, "\n" ).replace( /\n\n+/g, "\n\n" );
 
 writeFile( arguments[1], f );
