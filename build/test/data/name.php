47a6a32b27f7af24dec5dc963fa951177a091cd2
Jörn Zaefferer <joern.zaefferer@gmail.com>
2006-12-04 20:42:38 +0000

    Introduced before callback (#384) (updated test data)

--- build/test/data/name.php
+++ build/test/data/name.php
@@ -18,5 +18,11 @@ if($name == 'foo') {
 	echo "pan";
 	die();
 }
+$request = apache_request_headers();
+$request = $request['customHeader'];
+if(strlen($request) > 0) {
+	echo $request;
+	die();
+}
 echo 'ERROR <script type="text/javascript">ok( true, "name.php executed" );</script>';
 ?>
