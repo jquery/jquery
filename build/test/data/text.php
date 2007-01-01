ac78ad07c2a18a18ec7ce93d56824a55d56768bc
Mike Alsup <malsup@gmail.com>
2007-01-01 17:20:42 +0000

    Add new test file

---
+++ build/test/data/text.php
@@ -0,0 +1,12 @@
+Lorem ipsum dolor sit amet
+consectetuer adipiscing elit
+Sed lorem leo
+lorem leo consectetuer adipiscing elit
+Sed lorem leo
+rhoncus sit amet
+elementum at
+bibendum at, eros
+Cras at mi et tortor egestas vestibulum
+sed Cras at mi vestibulum
+Phasellus sed felis sit amet
+orci dapibus semper.
