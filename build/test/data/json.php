8ec860d637ca667e8050e0755d55fc1496301d96
Jörn Zaefferer <joern.zaefferer@gmail.com>
2007-01-10 15:03:40 +0000

    Disable error reporting in PHP test files

--- build/test/data/json.php
+++ build/test/data/json.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 $json = $_REQUEST['json'];
 if($json) {
 	echo '[ {"name": "John", "age": 21}, {"name": "Peter", "age": 25 } ]';
--- build/test/data/name.php
+++ build/test/data/name.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 $wait = $_REQUEST['wait'];
 if($wait) {
 	sleep($wait);
