c66935db9d6068087f1fca83626db0b9280343e2
Brandon Aaron <brandon.aaron@gmail.com>
2007-02-17 20:58:04 +0000

    Removing fix for #910 causing Safari to crash

--- src/jquery/jquery.js
+++ src/jquery/jquery.js
@@ -1470,7 +1470,7 @@ jQuery.extend({
 			if ( arg.length === 0 )
 				return;
 			
-			if ( arg[0] == undefined || (jQuery.browser.msie && jQuery.nodeName(arg,"form")) )
+			if ( arg[0] == undefined )
 				r.push( arg );
 			else
 				r = jQuery.merge( r, arg );
