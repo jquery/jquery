43c4b64f328c5d6f2642f6dcf33eb27c377c0491
Ariel Flesler <aflesler@gmail.com>
2008-07-23 15:21:27 +0000

    jquery core: closes #3179. Fallback browser version for non supported browsers.

--- src/core.js
+++ src/core.js
@@ -1221,7 +1221,7 @@ var userAgent = navigator.userAgent.toLowerCase();
 
 // Figure out what browser is being used
 jQuery.browser = {
-	version: (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [])[1],
+	version: (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [0,'0'])[1],
 	safari: /webkit/.test( userAgent ),
 	opera: /opera/.test( userAgent ),
 	msie: /msie/.test( userAgent ) && !/opera/.test( userAgent ),
