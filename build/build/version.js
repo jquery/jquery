2f536b3d4d6864994cc88ea72638e06609bc12d1
Jörn Zaefferer <joern.zaefferer@gmail.com>
2009-01-18 13:04:32 +0000

    core: replaced version.js build script in ant build; wasn't used in make anymore anyway

--- build/build/version.js
+++
@@ -1,4 +0,0 @@
-load("build/js/writeFile.js");
-
-var file = arguments[0];
-writeFile(file, readFile(file).replace(new RegExp("@VERSION", "g"), readFile("version.txt").replace( /^\s+|\s+$/g, "" )));
