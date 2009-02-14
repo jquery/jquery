d75c899fe78c52d89e5fb757ded979aca126d37b
John Resig <jeresig@gmail.com>
2009-02-14 17:25:33 +0000

    Duplication checks are now handled directly in Sizzle, no need to do extra work in .find().

--- src/core.js
+++ src/core.js
@@ -269,19 +269,15 @@ jQuery.fn = jQuery.prototype = {
 	push: [].push,
 
 	find: function( selector ) {
-		if ( this.length === 1 && !/,/.test(selector) ) {
+		if ( this.length === 1 ) {
 			var ret = this.pushStack( [], "find", selector );
 			ret.length = 0;
 			jQuery.find( selector, this[0], ret );
 			return ret;
 		} else {
-			var elems = jQuery.map(this, function(elem){
+			return this.pushStack( jQuery.unique(jQuery.map(this, function(elem){
 				return jQuery.find( selector, elem );
-			});
-
-			return this.pushStack( /[^+>] [^+>]/.test( selector ) ?
-				jQuery.unique( elems ) :
-				elems, "find", selector );
+			})), "find", selector );
 		}
 	},
 
