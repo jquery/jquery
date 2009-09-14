3eb56b09b8ce903355b9173b6e60b2354a6c7ba7
John Resig <jeresig@gmail.com>
2009-09-14 23:20:23 +0000

    Another fix for [6537] - make sure that .innerHTML isn't used on non-HTML documents and that we only attempt to use .innerHTML on DOM Elements.

--- src/manipulation.js
+++ src/manipulation.js
@@ -189,15 +189,15 @@ jQuery.fn.extend({
 
 		// See if we can take a shortcut and just use innerHTML
 		} else if ( typeof value === "string" && !/<script/i.test( value ) &&
+			this[0] && !jQuery.isXMLDoc( this[0] ) &&
 			!wrapMap[ (rtagName.exec( value ) || ["", ""])[1].toLowerCase() ] ) {
 
 			for ( var i = 0, l = this.length; i < l; i++ ) {
 				// Remove element nodes and prevent memory leaks
 				if ( this[i].nodeType === 1 ) {
 					cleanData( this[i].getElementsByTagName("*") );
+					this[i].innerHTML = value;
 				}
-
-				this[i].innerHTML = value;
 			}
 
 		} else {
