0f0fd1aee34b81411419ae3b152e03dfdf58b4f4
jeresig <jeresig@gmail.com>
2009-12-03 11:45:38 -0500

    Re-arranged many of the selector tests, breaking them into smaller test groups and into more-appropriate sections.

--- test/unit/selector.js
+++ test/unit/selector.js
@@ -171,7 +171,6 @@ test("name", function() {
 	a.remove();
 });
 
-
 test("multiple", function() {
 	expect(4);
 	
@@ -182,7 +181,7 @@ test("multiple", function() {
 });
 
 test("child and adjacent", function() {
-	expect(49);
+	expect(24);
 	t( "Child", "p > a", ["simon1","google","groups","mark","yahoo","simon"] );
 	t( "Child", "p> a", ["simon1","google","groups","mark","yahoo","simon"] );
 	t( "Child", "p >a", ["simon1","google","groups","mark","yahoo","simon"] );
@@ -199,6 +198,7 @@ test("child and adjacent", function() {
 	t( "Adjacent", "p[lang=en] + p", ["sap"] );
 	t( "Adjacent", "a.GROUPS + code + a", ["mark"] );
 	t( "Comma, Child, and Adjacent", "a + a, code > a", ["groups","anchor1","anchor2"] );
+	t( "Element Preceded By", "p ~ div", ["foo", "moretests","tabindex-tests", "liveHandlerOrder"] );
 
 	t( "Verify deep class selector", "div.blah > p > a", [] );
 
@@ -210,7 +210,69 @@ test("child and adjacent", function() {
 	same( jQuery("> *:first", document.getElementById("nothiddendiv")).get(), q("nothiddendivchild"), "Verify child context positional selctor" );
 
 	t( "Non-existant ancestors", ".fototab > .thumbnails > a", [] );
+});
+
+test("attributes", function() {
+	expect(37);
+	t( "Attribute Exists", "a[title]", ["google"] );
+	t( "Attribute Exists", "*[title]", ["google"] );
+	t( "Attribute Exists", "[title]", ["google"] );
+	t( "Attribute Exists", "a[ title ]", ["google"] );
 	
+	t( "Attribute Equals", "a[rel='bookmark']", ["simon1"] );
+	t( "Attribute Equals", 'a[rel="bookmark"]', ["simon1"] );
+	t( "Attribute Equals", "a[rel=bookmark]", ["simon1"] );
+	t( "Attribute Equals", "a[href='http://www.google.com/']", ["google"] );
+	t( "Attribute Equals", "a[ rel = 'bookmark' ]", ["simon1"] );
+
+	document.getElementById("anchor2").href = "#2";
+	t( "href Attribute", "p a[href^=#]", ["anchor2"] );
+	t( "href Attribute", "p a[href*=#]", ["simon1", "anchor2"] );
+
+	t( "for Attribute", "form label[for]", ["label-for"] );
+	t( "for Attribute in form", "#form [for=action]", ["label-for"] );
+
+	t( "Attribute containing []", "input[name^='foo[']", ["hidden2"] );
+	t( "Attribute containing []", "input[name^='foo[bar]']", ["hidden2"] );
+	t( "Attribute containing []", "input[name*='[bar]']", ["hidden2"] );
+	t( "Attribute containing []", "input[name$='bar]']", ["hidden2"] );
+	t( "Attribute containing []", "input[name$='[bar]']", ["hidden2"] );
+	t( "Attribute containing []", "input[name$='foo[bar]']", ["hidden2"] );
+	t( "Attribute containing []", "input[name*='foo[bar]']", ["hidden2"] );
+	
+	t( "Multiple Attribute Equals", "#form input[type='radio'], #form input[type='hidden']", ["radio1", "radio2", "hidden1"] );
+	t( "Multiple Attribute Equals", "#form input[type='radio'], #form input[type=\"hidden\"]", ["radio1", "radio2", "hidden1"] );
+	t( "Multiple Attribute Equals", "#form input[type='radio'], #form input[type=hidden]", ["radio1", "radio2", "hidden1"] );
+	
+	t( "Attribute selector using UTF8", "span[lang=中文]", ["台北"] );
+	
+	t( "Attribute Begins With", "a[href ^= 'http://www']", ["google","yahoo"] );
+	t( "Attribute Ends With", "a[href $= 'org/']", ["mark"] );
+	t( "Attribute Contains", "a[href *= 'google']", ["google","groups"] );
+	t( "Attribute Is Not Equal", "#ap a[hreflang!='en']", ["google","groups","anchor1"] );
+
+	t("Empty values", "#select1 option[value='']", ["option1a"]);
+	t("Empty values", "#select1 option[value!='']", ["option1b","option1c","option1d"]);
+	
+	t("Select options via :selected", "#select1 option:selected", ["option1a"] );
+	t("Select options via :selected", "#select2 option:selected", ["option2d"] );
+	t("Select options via :selected", "#select3 option:selected", ["option3b", "option3c"] );
+	
+	t( "Grouped Form Elements", "input[name='foo[bar]']", ["hidden2"] );
+	
+	t( ":not() Existing attribute", "#form select:not([multiple])", ["select1", "select2"]);
+	t( ":not() Equals attribute", "#form select:not([name=select1])", ["select2", "select3"]);
+	t( ":not() Equals quoted attribute", "#form select:not([name='select1'])", ["select2", "select3"]);
+});
+
+test("pseudo - child", function() {
+	expect(31);
+	t( "First Child", "p:first-child", ["firstp","sndp"] );
+	t( "Last Child", "p:last-child", ["sap"] );
+	t( "Only Child", "a:only-child", ["simon1","anchor1","yahoo","anchor2","liveLink1","liveLink2"] );
+	t( "Empty", "ul:empty", ["firstUL"] );
+	t( "Is A Parent", "p:parent", ["firstp","ap","sndp","en","sap","first"] );
+
 	t( "First Child", "p:first-child", ["firstp","sndp"] );
 	t( "Nth Child", "p:nth-child(1)", ["firstp","sndp"] );
 	t( "Not Nth Child", "p:not(:nth-child(1))", ["ap","en","sap","first"] );
@@ -249,90 +311,24 @@ test("child and adjacent", function() {
 	t( "Nth-child", "#form select:first option:nth-child(-n+3)", ["option1a", "option1b", "option1c"] );
 });
 
-test("attributes", function() {
-	expect(37);
-	t( "Attribute Exists", "a[title]", ["google"] );
-	t( "Attribute Exists", "*[title]", ["google"] );
-	t( "Attribute Exists", "[title]", ["google"] );
-	t( "Attribute Exists", "a[ title ]", ["google"] );
-	
-	t( "Attribute Equals", "a[rel='bookmark']", ["simon1"] );
-	t( "Attribute Equals", 'a[rel="bookmark"]', ["simon1"] );
-	t( "Attribute Equals", "a[rel=bookmark]", ["simon1"] );
-	t( "Attribute Equals", "a[href='http://www.google.com/']", ["google"] );
-	t( "Attribute Equals", "a[ rel = 'bookmark' ]", ["simon1"] );
+test("pseudo - misc", function() {
+	expect(6);
 
-	document.getElementById("anchor2").href = "#2";
-	t( "href Attribute", "p a[href^=#]", ["anchor2"] );
-	t( "href Attribute", "p a[href*=#]", ["simon1", "anchor2"] );
+	t( "Headers", ":header", ["qunit-header", "qunit-banner", "qunit-userAgent"] );
+	t( "Has Children - :has()", "p:has(a)", ["firstp","ap","en","sap"] );
 
-	t( "for Attribute", "form label[for]", ["label-for"] );
-	t( "for Attribute in form", "#form [for=action]", ["label-for"] );
-
-	jQuery("form input")[0].test = 0;
-	jQuery("form input")[1].test = 1;
-
-  // Disabled tests - expandos don't work in all browsers
-	//t( "Expando attribute", "form input[test]", ["text1", "text2"] );
-	//t( "Expando attribute value", "form input[test=0]", ["text1"] );
-	//t( "Expando attribute value", "form input[test=1]", ["text2"] );
-	
-	t( "Attribute containing []", "input[name^='foo[']", ["hidden2"] );
-	t( "Attribute containing []", "input[name^='foo[bar]']", ["hidden2"] );
-	t( "Attribute containing []", "input[name*='[bar]']", ["hidden2"] );
-	t( "Attribute containing []", "input[name$='bar]']", ["hidden2"] );
-	t( "Attribute containing []", "input[name$='[bar]']", ["hidden2"] );
-	t( "Attribute containing []", "input[name$='foo[bar]']", ["hidden2"] );
-	t( "Attribute containing []", "input[name*='foo[bar]']", ["hidden2"] );
-	
-	t( "Multiple Attribute Equals", "#form input[type='radio'], #form input[type='hidden']", ["radio1", "radio2", "hidden1"] );
-	t( "Multiple Attribute Equals", "#form input[type='radio'], #form input[type=\"hidden\"]", ["radio1", "radio2", "hidden1"] );
-	t( "Multiple Attribute Equals", "#form input[type='radio'], #form input[type=hidden]", ["radio1", "radio2", "hidden1"] );
-	
-	t( "Attribute selector using UTF8", "span[lang=中文]", ["台北"] );
-	
-	t( "Attribute Begins With", "a[href ^= 'http://www']", ["google","yahoo"] );
-	t( "Attribute Ends With", "a[href $= 'org/']", ["mark"] );
-	t( "Attribute Contains", "a[href *= 'google']", ["google","groups"] );
-	t( "Attribute Is Not Equal", "#ap a[hreflang!='en']", ["google","groups","anchor1"] );
-
-	t("Empty values", "#select1 option[value='']", ["option1a"]);
-	t("Empty values", "#select1 option[value!='']", ["option1b","option1c","option1d"]);
-	
-	t("Select options via :selected", "#select1 option:selected", ["option1a"] );
-	t("Select options via :selected", "#select2 option:selected", ["option2d"] );
-	t("Select options via :selected", "#select3 option:selected", ["option3b", "option3c"] );
-	
-	t( "Grouped Form Elements", "input[name='foo[bar]']", ["hidden2"] );
-	
-	t( ":not() Existing attribute", "#form select:not([multiple])", ["select1", "select2"]);
-	t( ":not() Equals attribute", "#form select:not([name=select1])", ["select2", "select3"]);
-	t( ":not() Equals quoted attribute", "#form select:not([name='select1'])", ["select2", "select3"]);
-});
-
-test("pseudo (:) selectors", function() {
-	expect(75);
-	t( "First Child", "p:first-child", ["firstp","sndp"] );
-	t( "Last Child", "p:last-child", ["sap"] );
-	t( "Only Child", "a:only-child", ["simon1","anchor1","yahoo","anchor2","liveLink1","liveLink2"] );
-	t( "Empty", "ul:empty", ["firstUL"] );
-	/* Temporarily disabled some tests - Opera 10 doesn't appear to support
-	   disabled/enabled/checked properly.
-	t( "Enabled UI Element", "#form input:not([type=hidden]):enabled", ["text1","radio1","radio2","check1","check2","hidden2","name","search"] );
-	t( "Disabled UI Element", "#form input:disabled", ["text2"] );
-	t( "Checked UI Element", "#form input:checked", ["radio2","check1"] );
-	*/
-	t( "Selected Option Element", "#form option:selected", ["option1a","option2d","option3b","option3c"] );
 	t( "Text Contains", "a:contains('Google')", ["google","groups"] );
 	t( "Text Contains", "a:contains('Google Groups')", ["groups"] );
 
 	t( "Text Contains", "a:contains('Google Groups (Link)')", ["groups"] );
 	t( "Text Contains", "a:contains('(Link)')", ["groups"] );
+});
 
-	t( "Element Preceded By", "p ~ div", ["foo", "moretests","tabindex-tests", "liveHandlerOrder"] );
+
+test("pseudo - :not", function() {
+	expect(17);
 	t( "Not", "a.blog:not(.link)", ["mark"] );
 	t( "Not - multiple", "#form option:not(:contains('Nothing'),#option1b,:selected)", ["option1c", "option1d", "option2b", "option2c", "option3d", "option3e"] );
-	//t( "Not - complex", "#form option:not([id^='opt']:nth-child(-n+3))", [ "option1a", "option1d", "option2d", "option3d", "option3e"] );
 	t( "Not - recursive", "#form option:not(:not(:selected))[id^='option3']", [ "option3b", "option3c"] );
 
 	t( ":not() failing interior", "p:not(.foo)", ["firstp","ap","sndp","en","sap","first"] );
@@ -350,7 +346,10 @@ test("pseudo (:) selectors", function() {
 	t( ":not Multiple", "p:not(p,a)", [] );
 	t( ":not Multiple", "p:not(a,p,b)", [] );
 	t( ":not Multiple", ":input:not(:image,:input,:submit)", [] );
-	
+});
+
+test("pseudo - position", function() {	
+	expect(25);
 	t( "nth Element", "p:nth(1)", ["ap"] );
 	t( "First Element", "p:first", ["firstp"] );
 	t( "Last Element", "p:last", ["first"] );
@@ -359,7 +358,30 @@ test("pseudo (:) selectors", function() {
 	t( "Position Equals", "p:eq(1)", ["ap"] );
 	t( "Position Greater Than", "p:gt(0)", ["ap","sndp","en","sap","first"] );
 	t( "Position Less Than", "p:lt(3)", ["firstp","ap","sndp"] );
-	t( "Is A Parent", "p:parent", ["firstp","ap","sndp","en","sap","first"] );
+
+	t( "Check position filtering", "div#nothiddendiv:eq(0)", ["nothiddendiv"] );
+	t( "Check position filtering", "div#nothiddendiv:last", ["nothiddendiv"] );
+	t( "Check position filtering", "div#nothiddendiv:not(:gt(0))", ["nothiddendiv"] );
+	t( "Check position filtering", "#foo > :not(:first)", ["en", "sap"] );
+	t( "Check position filtering", "select > :not(:gt(2))", ["option1a", "option1b", "option1c"] );
+	t( "Check position filtering", "select:lt(2) :not(:first)", ["option1b", "option1c", "option1d", "option2a", "option2b", "option2c", "option2d"] );
+	t( "Check position filtering", "div.nothiddendiv:eq(0)", ["nothiddendiv"] );
+	t( "Check position filtering", "div.nothiddendiv:last", ["nothiddendiv"] );
+	t( "Check position filtering", "div.nothiddendiv:not(:lt(0))", ["nothiddendiv"] );
+
+	t( "Check element position", "div div:eq(0)", ["nothiddendivchild"] );
+	t( "Check element position", "div div:eq(5)", ["t2037"] );
+	t( "Check element position", "div div:eq(27)", ["hide"] );
+	t( "Check element position", "div div:first", ["nothiddendivchild"] );
+	t( "Check element position", "div > div:first", ["nothiddendivchild"] );
+	t( "Check element position", "#dl div:first div:first", ["foo"] );
+	t( "Check element position", "#dl div:first > div:first", ["foo"] );
+	t( "Check element position", "div#nothiddendiv:first > div:first", ["nothiddendivchild"] );
+});
+
+test("pseudo - visibility", function() {
+	expect(13);
+
 	t( "Is Visible", "#form input:visible", [] );
 	t( "Is Visible", "div:visible:not(#qunit-testrunner-toolbar):lt(2)", ["nothiddendiv", "nothiddendivchild"] );
 	t( "Is Hidden", "#form input:hidden", ["text1","text2","radio1","radio2","check1","check2","hidden1","hidden2","name","search"] );
@@ -381,26 +403,11 @@ test("pseudo (:) selectors", function() {
 	t( "Is Visible", '#nothiddendivchild:visible', ['nothiddendivchild'] );
 	t( "Is Not Visible", '#nothiddendivchild:hidden', [] );
 	$div.width('').height('').css({ fontSize: '', lineHeight: '' });
+});
 
-	t( "Check position filtering", "div#nothiddendiv:eq(0)", ["nothiddendiv"] );
-	t( "Check position filtering", "div#nothiddendiv:last", ["nothiddendiv"] );
-	t( "Check position filtering", "div#nothiddendiv:not(:gt(0))", ["nothiddendiv"] );
-	t( "Check position filtering", "#foo > :not(:first)", ["en", "sap"] );
-	t( "Check position filtering", "select > :not(:gt(2))", ["option1a", "option1b", "option1c"] );
-	t( "Check position filtering", "select:lt(2) :not(:first)", ["option1b", "option1c", "option1d", "option2a", "option2b", "option2c", "option2d"] );
-	t( "Check position filtering", "div.nothiddendiv:eq(0)", ["nothiddendiv"] );
-	t( "Check position filtering", "div.nothiddendiv:last", ["nothiddendiv"] );
-	t( "Check position filtering", "div.nothiddendiv:not(:lt(0))", ["nothiddendiv"] );
+test("pseudo - form", function() {
+	expect(8);
 
-	t( "Check element position", "div div:eq(0)", ["nothiddendivchild"] );
-	t( "Check element position", "div div:eq(5)", ["t2037"] );
-	t( "Check element position", "div div:eq(27)", ["hide"] );
-	t( "Check element position", "div div:first", ["nothiddendivchild"] );
-	t( "Check element position", "div > div:first", ["nothiddendivchild"] );
-	t( "Check element position", "#dl div:first div:first", ["foo"] );
-	t( "Check element position", "#dl div:first > div:first", ["foo"] );
-	t( "Check element position", "div#nothiddendiv:first > div:first", ["nothiddendivchild"] );
-	
 	t( "Form element :input", "#form :input", ["text1", "text2", "radio1", "radio2", "check1", "check2", "hidden1", "hidden2", "name", "search", "button", "area1", "select1", "select2", "select3"] );
 	t( "Form element :radio", "#form :radio", ["radio1", "radio2"] );
 	t( "Form element :checkbox", "#form :checkbox", ["check1", "check2"] );
@@ -409,6 +416,5 @@ test("pseudo (:) selectors", function() {
 	t( "Form element :checkbox:checked", "#form :checkbox:checked", ["check1"] );
 	t( "Form element :radio:checked, :checkbox:checked", "#form :radio:checked, #form :checkbox:checked", ["radio2", "check1"] );
 
-	t( "Headers", ":header", ["qunit-header", "qunit-banner", "qunit-userAgent"] );
-	t( "Has Children - :has()", "p:has(a)", ["firstp","ap","en","sap"] );
+	t( "Selected Option Element", "#form option:selected", ["option1a","option2d","option3b","option3c"] );
 });
