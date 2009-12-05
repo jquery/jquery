b776e2b79a5b051fba3091b0b5057ae14950f7cc
jeresig <jeresig@gmail.com>
2009-12-05 14:33:40 -0500

    Made sure that a null speed doesn't get passed around. Patch from daltonlp. Fixes #5557.

--- src/fx.js
+++ src/fx.js
@@ -219,7 +219,7 @@ jQuery.each({
 jQuery.extend({
 
 	speed: function(speed, easing, fn) {
-		var opt = typeof speed === "object" ? speed : {
+		var opt = speed && typeof speed === "object" ? speed : {
 			complete: fn || !fn && easing ||
 				jQuery.isFunction( speed ) && speed,
 			duration: speed,
