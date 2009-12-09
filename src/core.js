d3dc2d1234af10cb856519a01a97358eb4103008
Dave Methvin <dave.methvin@gmail.com>
2009-12-09 11:46:07 +0800

    Get browser version rather than rendering engine version. Don't recognize Chrome as Safari. See results of different userAgent strings here: http://spreadsheets.google.com/ccc?key=0Aj5JJFjq9rZDdHdxXzRLVnY0SzFpRTBOUDc4VmhzRVE

--- src/core.js
+++ src/core.js
@@ -624,8 +624,8 @@ jQuery.extend({
 	// It's included for backwards compatibility and plugins,
 	// although they should work to migrate away.
 	browser: {
-		version: (/.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/.exec(userAgent) || [0,'0'])[1],
-		safari: /webkit/.test( userAgent ),
+		version: (/.*?(?:firefox|safari|opera|msie)[\/ ]([\d.]+)/.exec(userAgent) || [0,'0'])[1],
+		safari: /safari/.test( userAgent ),
 		opera: /opera/.test( userAgent ),
 		msie: /msie/.test( userAgent ) && !/opera/.test( userAgent ),
 		mozilla: /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test( userAgent )
