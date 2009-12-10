80913618b0a54a1788a4e0edc3820cf6ddf04fec
John Resig <jeresig@gmail.com>
2009-12-09 23:56:47 -0800

    Made the licensing verbiage a little more explicit. Thanks to Dan Montgomery for the suggestion.

--- src/intro.js
+++ src/intro.js
@@ -3,7 +3,7 @@
  * http://jquery.com/
  *
  * Copyright (c) 2009 John Resig
- * Dual licensed under the MIT and GPL licenses.
+ * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://docs.jquery.com/License
  *
  * Date: 
