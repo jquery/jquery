6bc222e7a19d620a79a0b2af46c98664218a81a3
jeresig <jeresig@gmail.com>
2009-12-17 15:04:07 -0500

    Removed 'abbr' from self-closing tags list. Fixes #5167.

--- src/manipulation.js
+++ src/manipulation.js
@@ -1,7 +1,7 @@
 var rinlinejQuery = / jQuery\d+="(?:\d+|null)"/g,
 	rleadingWhitespace = /^\s+/,
 	rxhtmlTag = /(<([\w:]+)[^>]*?)\/>/g,
-	rselfClosing = /^(?:abbr|br|col|img|input|link|meta|param|hr|area|embed)$/i,
+	rselfClosing = /^(?:area|br|col|embed|hr|img|input|link|meta|param)$/i,
 	rtagName = /<([\w:]+)/,
 	rtbody = /<tbody/i,
 	rhtml = /<|&\w+;/,
