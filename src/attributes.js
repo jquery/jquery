27d65b59f96460987abb84dadc3a75dde8826b3a
jeresig <jeresig@gmail.com>
2009-12-18 11:30:04 -0500

    Backing out the change from 841f9ff7a1815b521044aeeb39ccbe70fa688201, it was ineffectual in Internet Explorer. Keeping the broken test case, for now, so that we can check into the issue some more.

--- src/attributes.js
+++ src/attributes.js
@@ -268,14 +268,7 @@ jQuery.extend({
 						throw "type property can't be changed";
 					}
 
-					// browsers index elements by id/name on forms, give priority to attributes.
-					if( jQuery.nodeName( elem, "form" ) ) {
-						// convert the value to a string (all browsers do this but IE) see #1070
-						elem.setAttribute( name, "" + value );
-
-					} else {
-						elem[ name ] = value;
-					}
+					elem[ name ] = value;
 				}
 
 				// browsers index elements by id/name on forms, give priority to attributes.
