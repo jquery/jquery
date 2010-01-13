fbf09d55fb4426d77b12adc467bf64653de075de
jaubourg <aubourg.julian@gmail.com>
2010-01-13 05:00:01 +0100

    Only bind xdr transport on IE
    Signed-off-by: jaubourg <aubourg.julian@gmail.com>

--- src/transports/xdr.js
+++ src/transports/xdr.js
@@ -1,70 +1,74 @@
-jQuery.ajax.bindTransport( function (s) {
-	
-	// Only for cross domain when it is supported through XDomainRequest
-	if ( s.crossDomain && jQuery.support.crossDomainRequest == "xdr" ) {
-	
-		// Timeout is handled by the implementation
-		if ( s.timeout ) {
-			s.xDomainRequestTimeout = s.timeout;
-			s.timeout = null;
-		}
+if ( jQuery.support.crossDomainRequest === "xdr" ) {
+
+	jQuery.ajax.bindTransport( function (s) {
 		
-		var xdr,
-			abortStatusText;
+		// Only for cross domain
+		if ( s.crossDomain ) {
+		
+			// Timeout is handled by the implementation
+			if ( s.timeout ) {
+				s.xDomainRequestTimeout = s.timeout;
+				s.timeout = null;
+			}
 			
-		return {
-			
-			send: function(_, complete) {
+			var xdr,
+				abortStatusText;
 				
-				var done = function(status,statusText,response,responseHeaders) {
-					// Cleanup (IE wants null for event handlers, not undefined)
-					xdr.onerror = xdr.onload = xdr.ontimeout = noop;
-					xdr = undefined;
-					// Complete & dereference
-					complete(status,statusText,response,responseHeaders);
-				};
+			return {
 				
-				xdr = new XDomainRequest();
-				xdr.open(s.type,s.url);
-				
-				xdr.onerror = function() {
-					done(abortStatusText ? 0 : 404, abortStatusText || "error");
-				};
-				
-				xdr.onload = function() {
-					done(
-						200,
-						"success",
-						handleDataTypes( s , xdr.contentType , xdr.responseText ),
-						"Content-type: " + xdr.contentType
-					);
-				};
-				
-				if ( s.xDomainRequestTimeout ) {
-					xdr.ontimeout = function() {
-						done(0, "timeout");
+				send: function(_, complete) {
+					
+					var done = function(status,statusText,response,responseHeaders) {
+						// Cleanup (IE wants null for event handlers, not undefined)
+						xdr.onerror = xdr.onload = xdr.ontimeout = noop;
+						xdr = undefined;
+						// Complete & dereference
+						complete(status,statusText,response,responseHeaders);
 					};
-					xdr.timeout = s.xDomainRequestTimeout;
-				}
 					
-				try {
+					xdr = new XDomainRequest();
+					xdr.open(s.type,s.url);
 					
-					xdr.send( s.type === "POST" || s.type === "PUT" ? s.data : null );
+					xdr.onerror = function() {
+						done(abortStatusText ? 0 : 404, abortStatusText || "error");
+					};
 					
-				} catch(e) {
+					xdr.onload = function() {
+						done(
+							200,
+							"success",
+							handleDataTypes( s , xdr.contentType , xdr.responseText ),
+							"Content-type: " + xdr.contentType
+						);
+					};
 					
-					done(0, "error", "" + e);
+					if ( s.xDomainRequestTimeout ) {
+						xdr.ontimeout = function() {
+							done(0, "timeout");
+						};
+						xdr.timeout = s.xDomainRequestTimeout;
+					}
+						
+					try {
+						
+						xdr.send( s.type === "POST" || s.type === "PUT" ? s.data : null );
+						
+					} catch(e) {
+						
+						done(0, "error", "" + e);
+						
+					}
 					
-				}
+				},
 				
-			},
-			
-			abort: function(statusText) {
-				if ( xdr ) {
-					abortStatusText = statusText;
-					xdr.abort();
-				}
+				abort: function(statusText) {
+					if ( xdr ) {
+						abortStatusText = statusText;
+						xdr.abort();
+					}
+				}	
 			}	
-		}	
-	}
-});
+		}
+	});
+	
+}
