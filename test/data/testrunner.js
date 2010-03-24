59124f92b52f24f2547afaecc55afe10e2955cb6
jeresig <jeresig@gmail.com>
2010-03-24 16:04:10 -0400

    Temporarily disable ajax tests when running in TestSwam.

--- test/data/testrunner.js
+++ test/data/testrunner.js
@@ -7,5 +7,9 @@ jQuery.noConflict(); // Allow the test to run with other libs or jQuery's.
 	if ( !url || url.indexOf("http") !== 0 ) {
 		return;
 	}
+
+	// (Temporarily) Disable Ajax tests to reduce network strain
+	QUnit.isLocal = true;
+
 	document.write("<scr" + "ipt src='http://swarm.jquery.org/js/inject.js?" + (new Date).getTime() + "'></scr" + "ipt>");
 })();
