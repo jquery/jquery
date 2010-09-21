23d59dd6011cd93bfa7f2395a595303ce71bfc95
Jake Archibald <jaffathecake@gmail.com>
2010-09-21 10:36:08 +0100

    Improving accuracy of number detection in data- attributes
    Also faster in IE

--- test/unit/data.js
+++ test/unit/data.js
@@ -158,7 +158,7 @@ test(".data(String) and .data(String, Object)", function() {
 });
 
 test("data-* attributes", function() {
-	expect(23);
+	expect(24);
 	var div = jQuery("<div>"),
 		child = jQuery("<div data-myobj='old data' data-ignored=\"DOM\"></div>");
 		
@@ -184,21 +184,23 @@ test("data-* attributes", function() {
 		.attr("data-false", "false")
 		.attr("data-five", "5")
 		.attr("data-point", "5.5")
+		.attr("data-expopoint", "5.5e5")
 		.attr("data-pointbad", "5..5")
 		.attr("data-pointbad2", "-.")
 		.attr("data-badjson", "{123}")
 		.attr("data-null", "null")
 		.attr("data-string", "test");
 	
-	equals( child.data('true'), true, "Primitive true read from attribute");
-	equals( child.data('false'), false, "Primitive false read from attribute");
-	equals( child.data('five'), 5, "Primitive number read from attribute");
-	equals( child.data('point'), 5.5, "Primitive number read from attribute");
-	equals( child.data('pointbad'), "5..5", "Bad number read from attribute");
-	equals( child.data('pointbad2'), "-.", "Bad number read from attribute");
-	equals( child.data('badjson'), "{123}", "Bad number read from attribute");
-	equals( child.data('null'), null, "Primitive null read from attribute");
-	equals( child.data('string'), "test", "Typical string read from attribute");
+	strictEqual( child.data('true'), true, "Primitive true read from attribute");
+	strictEqual( child.data('false'), false, "Primitive false read from attribute");
+	strictEqual( child.data('five'), 5, "Primitive number read from attribute");
+	strictEqual( child.data('point'), 5.5, "Primitive number read from attribute");
+	strictEqual( child.data('expopoint'), 5.5e5, "Primitive number read from attribute");
+	strictEqual( child.data('pointbad'), "5..5", "Bad number read from attribute");
+	strictEqual( child.data('pointbad2'), "-.", "Bad number read from attribute");
+	strictEqual( child.data('badjson'), "{123}", "Bad number read from attribute");
+	strictEqual( child.data('null'), null, "Primitive null read from attribute");
+	strictEqual( child.data('string'), "test", "Typical string read from attribute");
 
 	child.remove();
 	
