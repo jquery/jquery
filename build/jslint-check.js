25828768d7bc2d76438244e69d6eeb69c36227e6
John Resig <jeresig@gmail.com>
2010-10-17 10:50:17 -0400

    Handle the case where JSLint complains about arguments in try/catch already being defined (we use the name 'e' consistently for catch(e) - will work to standardize on that now).

--- build/jslint-check.js
+++ build/jslint-check.js
@@ -12,8 +12,8 @@ var ok = {
 	"Use '===' to compare with 'null'.": true,
 	"Use '!==' to compare with 'null'.": true,
 	"Expected an assignment or function call and instead saw an expression.": true,
-	"Expected a 'break' statement before 'case'.": true
-
+	"Expected a 'break' statement before 'case'.": true,
+	"'e' is already defined.": true
 };
 
 var e = JSLINT.errors, found = 0, w;
