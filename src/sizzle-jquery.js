424500bcf068a234b13cd9ef33d94580c35eeb86
jeresig <jeresig@gmail.com>
2010-10-22 01:18:58 -0400

    Make sure that +, >, ~ leading, rooted, selectors go to the old engine. Thanks to @rkatic for the catch! Fixes #7220.

--- src/sizzle-jquery.js
+++ src/sizzle-jquery.js
@@ -2,6 +2,3 @@ jQuery.find = Sizzle;
 jQuery.expr = Sizzle.selectors;
 jQuery.expr[":"] = jQuery.expr.filters;
 jQuery.unique = Sizzle.uniqueSort;
-jQuery.text = Sizzle.getText;
-jQuery.isXMLDoc = Sizzle.isXML;
-jQuery.contains = Sizzle.contains;
