b78e3fc39f96d63a60ea66e3d066815626e633d8
Xavi <xavi.rmz@gmail.com>
2011-01-09 20:51:20 -0500

    Bug 7931; Inverted logic in scrollTop/Left (i.e. made

--- src/offset.js
+++ src/offset.js
@@ -263,34 +263,34 @@ jQuery.each( ["Left", "Top"], function( i, name ) {
 	jQuery.fn[ method ] = function(val) {
 		var elem, win;
 
-		if ( val !== undefined ) {
-			// Set the scroll offset
-			return this.each(function() {
-				win = getWindow( this );
-
-				if ( win ) {
-					win.scrollTo(
-						!i ? val : jQuery(win).scrollLeft(),
-						 i ? val : jQuery(win).scrollTop()
-					);
-
-				} else {
-					this[ method ] = val;
-				}
-			});
+		if(val === undefined) {
+			elem = this[0];
+			if( !elem ) {
+				return null;
+			}
+		
+			win = getWindow( elem );
+			// Return the scroll offset
+			return win ? ("pageXOffset" in win) ? win[ i ? "pageYOffset" : "pageXOffset" ] :
+				jQuery.support.boxModel && win.document.documentElement[ method ] ||
+					win.document.body[ method ] :
+				elem[ method ];
 		}
 		
-		elem = this[0];
-		if( !elem ) {
-			return null;
-		}
-		
-		win = getWindow( elem );
-		// Return the scroll offset
-		return win ? ("pageXOffset" in win) ? win[ i ? "pageYOffset" : "pageXOffset" ] :
-			jQuery.support.boxModel && win.document.documentElement[ method ] ||
-				win.document.body[ method ] :
-			elem[ method ];
+		// Set the scroll offset
+		return this.each(function() {
+			win = getWindow( this );
+
+			if ( win ) {
+				win.scrollTo(
+					!i ? val : jQuery(win).scrollLeft(),
+					 i ? val : jQuery(win).scrollTop()
+				);
+
+			} else {
+				this[ method ] = val;
+			}
+		});
 	};
 });
 
