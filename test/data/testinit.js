e17978f74789b8ed6479fea6da0ff8f559311026
Timmy Willison <tim.willison@thisismedium.com>
2011-01-20 20:26:12 -0500

    Fix for ticket #2773

--- test/data/testinit.js
+++ test/data/testinit.js
@@ -94,26 +94,3 @@ function url(value) {
 		}
 	}
 }());
-
-/**
- * Shortcuts for log functions
- */
-window.log = function () {
-	log.history = log.history || [];
-	log.history.push( arguments );
-	if ( this.console ) {
-		console.log( Array.prototype.slice.call( arguments ) );
-	}
-};
-window.debug = function () {
-	debug.history = debug.history || [];
-	debug.history.push( arguments );
-	if ( this.console ) {
-		console.debug( Array.prototype.slice.call( arguments ) );
-	}
-};
-window.dir = function () {
-	if ( this.console ) {
-		console.dir( arguments );
-	}
-};
