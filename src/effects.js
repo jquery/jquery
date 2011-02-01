4e7d6eb17e8b3c70001f8952ad63903eb9e98f30
louisremi <louisremi@louisremi-laptop.(none)>
2011-02-01 18:26:41 +0100

    timerId has to be set to true, to avoid starting multiple animation queues

--- src/effects.js
+++ src/effects.js
@@ -359,9 +359,9 @@ jQuery.fx.prototype = {
 		t.elem = this.elem;
 
 		if ( t() && jQuery.timers.push(t) && !timerId ) {
-			jQuery.support.requestAnimationFrame ?
-				window[jQuery.support.requestAnimationFrame](fx.tick):
-				timerId = setInterval(fx.tick, fx.interval);
+			timerId = jQuery.support.requestAnimationFrame ?
+				!window[jQuery.support.requestAnimationFrame](fx.tick):
+				setInterval(fx.tick, fx.interval);
 		}
 	},
 
