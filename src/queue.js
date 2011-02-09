cd749168fb84f73cfeb59ad085d42dd9f18b6990
louisremi <louisremi@louisremi-laptop.(none)>
2011-02-09 11:46:41 +0100

    little improvement to fn.delay coding style

--- src/queue.js
+++ src/queue.js
@@ -80,7 +80,7 @@ jQuery.fn.extend({
 	// Based off of the plugin by Clint Helfers, with permission.
 	// http://blindsignals.com/index.php/2009/07/jquery-delay/
 	delay: function( time, type ) {
-		time = jQuery.fx ? jQuery.fx.speeds[time] || time : time;
+		time = jQuery.fx && jQuery.fx.speeds[time] || time;
 		type = type || "fx";
 
 		return this.queue( type, function() {
