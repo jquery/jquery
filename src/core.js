edf63d0f362f16b1ba0b3b207032bdc0a14e55ff
Jordan Boesch <jboesch26@gmail.com>
2011-02-20 12:53:37 -0600

    * reverting to "isObj = length === undefined" for speed purposes.

--- src/core.js
+++ src/core.js
@@ -714,9 +714,13 @@ jQuery.extend({
 
 	// arg is for internal usage only
 	map: function( elems, callback, arg ) {
-	  var ret = [], value;
+	  var ret = [],
+        value,
+        length = elems.length,
+        // same object detection used in jQuery.each, not full-proof but very speedy.
+        isObj = length === undefined;
 
-      if ( jQuery.isPlainObject( elems ) ) {
+      if ( isObj ) {
         for ( name in elems ) {
           value = callback( elems[ name ], name, arg );
               
@@ -727,7 +731,7 @@ jQuery.extend({
       } else {
         // Go through the array, translating each of the items to their
         // new value (or values).
-        for ( var i = 0, length = elems.length; i < length; i++ ) {
+        for ( var i = 0; i < length; i++ ) {
           value = callback( elems[ i ], i, arg );
 
           if ( value != null ) {
