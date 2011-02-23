02ee07ff793c8650ee57baa2fbfdb636e1f6ff3d
Andreas <jandy@typeofnan.com>
2011-02-23 07:46:32 +0100

    extended now() to use native Date.now() if available

--- src/core.js
+++ src/core.js
@@ -788,9 +788,15 @@ jQuery.extend({
 		return length ? fn( elems[0], key ) : undefined;
 	},
 
-	now: function() {
-		return (new Date()).getTime();
-	},
+	now: (function() {
+		return 'now' in Date && jQuery.isFunction(Date.now) ?
+			function() {
+				return Date.now();
+			} :
+			function() {
+				return +new Date();
+			}
+	}()),
 
 	// Create a simple deferred (one callbacks list)
 	_Deferred: function() {
