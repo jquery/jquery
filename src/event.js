cbafdbaebd8ab58a34c5c59f5e99c1813b72529d
Alexis Abril <alexis.abril@gmail.com>
2011-04-06 15:08:49 -0500

    Removed whitespace and updated comment.

--- src/event.js
+++ src/event.js
@@ -901,8 +901,8 @@ if ( document.addEventListener ) {
 
 jQuery.each(["bind", "one"], function( i, name ) {
 	jQuery.fn[ name ] = function( type, data, fn ) {
-    // Handles hover alias similar to $.live.
-    // Potential area to consider centralizing aliases for events vs string type comparisons    
+		// Handles hover alias similar to $.fn.live.
+		// Potential area to consider centralizing aliases for events vs string type comparisons
 		if( type === "hover" ) {
 			type = {
 				mouseenter : fn,
