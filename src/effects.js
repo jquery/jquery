1eab99b6c496200857dc5a76eef54ed55fce7966
Louis-Rémi Babé <public@lrbabe.com>
2011-04-21 01:56:07 -0700

    The interval should be an integer. 16ms is 62.5 fps, close enough to 60fps.

--- src/effects.js
+++ src/effects.js
@@ -415,7 +415,7 @@ jQuery.fx.prototype = {
 				};
 				requestAnimationFrame( raf );
 			} else {
-				timerId = setInterval( fx.tick, 16.6 );
+				timerId = setInterval( fx.tick, 16 );
 			}
 		}
 	},
