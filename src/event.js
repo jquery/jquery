282b158030c65b88896efb47536c6a3ba69973f2
Jordan Boesch <jboesch26@gmail.com>
2011-04-23 13:50:23 -0600

    removing check for this.nodeName as thats being done in jQuery.nodeName

--- src/event.js
+++ src/event.js
@@ -707,7 +707,7 @@ if ( !jQuery.support.submitBubbles ) {
 
 	jQuery.event.special.submit = {
 		setup: function( data, namespaces ) {
-			if ( this.nodeName && !jQuery.nodeName( this, "form" ) ) {
+			if ( !jQuery.nodeName( this, "form" ) ) {
 				jQuery.event.add(this, "click.specialSubmit", function( e ) {
 					var elem = e.target,
 						type = elem.type;
