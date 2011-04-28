b8a9c2ba343f420ff6cce8100250d4e067fd300a
timmywil <tim.willison@thisismedium.com>
2011-04-28 16:31:51 -0400

    Syntax in testrunner

--- test/data/testrunner.js
+++ test/data/testrunner.js
@@ -2,8 +2,9 @@ jQuery.noConflict(); // Allow the test to run with other libs or jQuery's.
 
 // jQuery-specific QUnit.reset
 (function() {
-	var reset = QUnit.reset;
-	var ajaxSettings = jQuery.ajaxSettings
+	var reset = QUnit.reset,
+		ajaxSettings = jQuery.ajaxSettings;
+
 	QUnit.reset = function() {
 		reset.apply(this, arguments);
 		jQuery.event.global = {};
