f8a112f08e9349432cfa6bfc2da5d4275aceb11a
John Resig <jeresig@gmail.com>
2011-05-02 16:32:02 -0400

    Ensure that an endline is in place at the end of the minified jQuery file.

--- build/post-compile.js
+++ build/post-compile.js
@@ -4,4 +4,4 @@ var print = require("sys").print,
 	src = require("fs").readFileSync(process.argv[2], "utf8");
 
 // Previously done in sed but reimplemented here due to portability issues
-print(src.replace(/^(\s*\*\/)(.+)/m, "$1\n$2;"));
+print(src.replace(/^(\s*\*\/)(.+)/m, "$1\n$2;").replace(/([^;]*)$/, "$1;"));
