bd68e49568f272ebbfaf0cde09ea6a31efe403e8
Aaron Spaulding <Aaron@sachimp.com>
2011-06-04 22:25:11 -0400

    add a couple other test cases and fixes for thing that might affect the width

--- src/support.js
+++ src/support.js
@@ -141,6 +141,9 @@ jQuery.support = (function() {
 
 	// Figure out if the W3C box model works as expected
 	div.style.width = div.style.padding = "1px";
+	div.style.border = 0;
+	div.style.overflow = "hidden";
+	div.style.display = "block";
 
 	// We don't want to do body-related feature tests on frameset
 	// documents, which lack a body. So we use
@@ -189,7 +192,7 @@ jQuery.support = (function() {
 
 		// Check if elements with layout shrink-wrap their children
 		// (IE 6 does this)
-		div.style.display = "";
+		div.style.display = "block";
 		div.innerHTML = "<div style='width:5px;'></div>";
 		support.shrinkWrapBlocks = ( div.offsetWidth !== 3 );
 	}
