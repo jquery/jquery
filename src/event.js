7323ea9077b4b102a82ee25de3a75e5cda780f6c
vijaykramesh <vijay.krishna.ramesh@gmail.com>
2011-06-07 12:16:14 -0700

    Updating fix to have single declaration per line

--- src/event.js
+++ src/event.js
@@ -710,7 +710,8 @@ if ( !jQuery.support.submitBubbles ) {
 		setup: function( data, namespaces ) {
 			if ( !jQuery.nodeName( this, "form" ) ) {
 				jQuery.event.add(this, "click.specialSubmit", function( e ) {
-					var elem = e.target, type = jQuery.nodeName( elem, "input" ) ? elem.type : "";
+					var elem = e.target, 
+                                            type = jQuery.nodeName( elem, "input" ) ? elem.type : "";
 
 					if ( (type === "submit" || type === "image") && jQuery( elem ).closest("form").length ) {
 						trigger( "submit", this, arguments );
@@ -718,7 +719,8 @@ if ( !jQuery.support.submitBubbles ) {
 				});
 
 				jQuery.event.add(this, "keypress.specialSubmit", function( e ) {
-					var elem = e.target, type = jQuery.nodeName( elem, "input" ) ? elem.type : "";
+					var elem = e.target, 
+                                            type = jQuery.nodeName( elem, "input" ) ? elem.type : "";
 
 					if ( (type === "text" || type === "password") && jQuery( elem ).closest("form").length && e.keyCode === 13 ) {
 						trigger( "submit", this, arguments );
