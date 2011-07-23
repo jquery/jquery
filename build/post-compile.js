7a1e8809632be7fb04b8e2fe4061bd47bb2ec485
Rick Waldron <waldron.rick@gmail.com>
2011-07-23 19:18:41 -0400

    Mine and munge date from full source license header

--- build/post-compile.js
+++ build/post-compile.js
@@ -5,14 +5,25 @@ var print = require( "sys" ).print,
 	src = fs.readFileSync( process.argv[2], "utf8" ),
 	version = fs.readFileSync( "version.txt", "utf8" ),
 	// License Template
-	license = "/*! jQuery v@VERSION @DATE http://jquery.com/ | http://jquery.org/license */";
+	license = "/*! jQuery v@VERSION @DATE http://jquery.com/ | http://jquery.org/license */",
+	date;
 
-
-license = license.replace( "@VERSION", version )
-						.replace( "@DATE", (new Date()).toLocaleString() );
 
 // Previously done in sed but reimplemented here due to portability issues
 src = src.replace( /^(\s*\*\/)(.+)/m, "$1\n$2" ) + ";";
+
+// Mine & Munge Date information
+src.split( "\n" ).forEach(function( line, idx ) {
+	var data = line.split( " * Date: " );
+
+	if ( data.length && data[ 1 ] ) {
+		date = data[ 1 ];
+	}
+});
+
+// Set minimal license block vars
+license = license.replace( "@VERSION", version )
+						.replace( "@DATE", date );
 
 // Replace license block with minimal license
 src = src.replace( /\/\/.*?\/?\*.+?(?=\n|\r|$)|\/\*[\s\S]*?\/\/[\s\S]*?\*\//, license );
