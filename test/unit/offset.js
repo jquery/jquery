3c0af9dd4881fb456f03f65c549e85ac3ca27329
Rick Waldron <waldron.rick@gmail.com>
2011-08-03 13:30:57 -0400

    Test correction

--- test/unit/offset.js
+++ test/unit/offset.js
@@ -388,7 +388,7 @@ testoffset("scroll", function( jQuery, win ) {
 	equals( jQuery(window).scrollLeft(), 0, "jQuery(window).scrollLeft() other window" );
 	equals( jQuery(document).scrollTop(), 0, "jQuery(window).scrollTop() other document" );
 	equals( jQuery(document).scrollLeft(), 0, "jQuery(window).scrollLeft() other document" );
-	
+
 	// Tests scrollTop/Left with empty jquery objects
 	notEqual( jQuery().scrollTop(100), null, "jQuery().scrollTop(100) testing setter on empty jquery object" );
 	notEqual( jQuery().scrollLeft(100), null, "jQuery().scrollLeft(100) testing setter on empty jquery object" );
@@ -401,8 +401,8 @@ testoffset("scroll", function( jQuery, win ) {
 testoffset("body", function( jQuery ) {
 	expect(2);
 
-	equals( jQuery("body").offset().top, 1, "jQuery('#body').offset().top" );
-	equals( jQuery("body").offset().left, 1, "jQuery('#body').offset().left" );
+	equals( jQuery("body").offset().top, 0, "jQuery('#body').offset().top" );
+	equals( jQuery("body").offset().left, 0, "jQuery('#body').offset().left" );
 });
 
 test("Chaining offset(coords) returns jQuery object", function() {
@@ -441,12 +441,12 @@ test("offsetParent", function(){
 
 test("fractions (see #7730 and #7885)", function() {
 	expect(2);
-	
+
 	jQuery('body').append('<div id="fractions"/>');
-	
+
 	var expected = { top: 1000, left: 1000 };
 	var div = jQuery('#fractions');
-	
+
 	div.css({
 		position: 'absolute',
 		left: '1000.7432222px',
@@ -454,14 +454,14 @@ test("fractions (see #7730 and #7885)", function() {
 		width: 100,
 		height: 100
 	});
-	
+
 	div.offset(expected);
-	
+
 	var result = div.offset();
 
 	equals( result.top, expected.top, "Check top" );
 	equals( result.left, expected.left, "Check left" );
-	
+
 	div.remove();
 });
 
