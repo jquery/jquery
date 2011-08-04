700c16b0859a8c982869d5501934bed9e51d3662
timmywil <timmywillisn@gmail.com>
2011-08-04 18:14:35 -0400

    Remove unnecessary regex

--- src/attributes.js
+++ src/attributes.js
@@ -7,7 +7,6 @@ var rclass = /[\n\t\r]/g,
 	rfocusable = /^(?:button|input|object|select|textarea)$/i,
 	rclickable = /^a(?:rea)?$/i,
 	rboolean = /^(?:autofocus|autoplay|async|checked|controls|defer|disabled|hidden|loop|multiple|open|readonly|required|scoped|selected)$/i,
-	rinvalidChar = /\:|^on/,
 	nodeHook, boolHook;
 
 jQuery.fn.extend({
