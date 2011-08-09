8daafbf42154ebae87bb0a20575799aff0e0c9ce
Oleg <markelog@gmail.com>
2011-08-09 06:05:48 +0400

    Fix for ticket #10006

--- src/effects.js
+++ src/effects.js
@@ -41,9 +41,18 @@ jQuery.fn.extend({
 					// Set elements which have been overridden with display: none
 					// in a stylesheet to whatever the default browser style is
 					// for such an element
-					if ( display === "" && jQuery.css( elem, "display" ) === "none" ) {
+					// For documentFragment, Mozilla will get real css value.
+					// Opera, Chrome and Safari will get empty string 
+					// IE will get default display
+					if ( display === "" && (computedDisplay = jQuery.css( elem, "display" ), computedDisplay === "none" || computedDisplay == "") ) {
 						jQuery._data(elem, "olddisplay", defaultDisplay(elem.nodeName));
-					}
+						
+					// In IE, sourceIndex === 0 means either documentFragment or html element
+					// for documentFragment it already will be default display	
+					} else if ( elem.sourceIndex === 0 ) {  
+						jQuery._data(elem, "olddisplay", computedDisplay); 
+						
+					}  
 				}
 			}
 
