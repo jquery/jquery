52e6baaaddeb67541750e58858c00fed364fb089
Rick Waldron <waldron.rick@gmail.com>
2011-09-07 15:33:05 -0400

    Switch split regex to literal space. See: http://jsperf.com/literal-space-vs-regex

--- src/manipulation.js
+++ src/manipulation.js
@@ -542,7 +542,7 @@ function safeFragment( context ) {
 	var nodeNames = (
 		"abbr article aside audio canvas datalist details figcaption figure footer " +
 		"header hgroup mark meter nav output progress section subline summary time video"
-	).split(/\s+/),
+	).split( " " ),
 	safeFrag = context.createDocumentFragment();
 
 	if ( safeFrag.createElement ) {
