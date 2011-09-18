a625c0f9cc1a4dbb46fe8382fcbd1b45f21d9654
Mathias Bynens <mathias@qiwi.be>
2011-09-18 21:26:20 +0200

    Add a comment explaining why `document.getElementsByTagName("body")[0]` is used (and not `document.body`).

--- src/support.js
+++ src/support.js
@@ -138,7 +138,11 @@ jQuery.support = (function() {
 	// Figure out if the W3C box model works as expected
 	div.style.width = div.style.paddingLeft = "1px";
 
-	body = document.getElementsByTagName( "body" )[ 0 ];
+	// We don't want to do body-related feature tests on frameset
+	// documents, which lack a body. So we use
+	// document.getElementsByTagName("body")[0], which is undefined in
+	// frameset documents, while document.body isn’t. (7398)
+	body = document.getElementsByTagName("body")[ 0 ];
 	// We use our own, invisible, body unless the body is already present
 	// in which case we use a div (#9239)
 	testElement = document.createElement( body ? "div" : "body" );
