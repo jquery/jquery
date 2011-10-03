4b0fcd6fdee634f365751bb6dc3041c15fecce4d
Mike Sherov <mike.sherov@gmail.com>
2011-10-03 18:16:53 -0400

    make sure box-sizing:border-box children of a hidden div report inner/outer/Width()/Height() correctly, fixes #10413

--- test/unit/dimensions.js
+++ test/unit/dimensions.js
@@ -268,6 +268,31 @@ test("getting dimensions shouldnt modify runtimeStyle see #9233", function() {
 	$div.remove();
 });
 
+test("child with box-sizing:border-box of a hidden elem has accurate inner/outer/Width()/Height()  see #10413", function() {
+	expect(8);
+
+	// setup html
+	var $divNormal       = jQuery("<div>").css({ boxSizing: "border-box", width: "100px", height: "100px", border: "10px solid white", padding: "2px", margin: "3px" }),
+		$divChild        = $divNormal.clone(),
+		$divHiddenParent = jQuery("<div>").css( "display", "none" ).append( $divChild ).appendTo("body");
+	$divNormal.appendTo("body");
+
+	// tests that child div of a hidden div works the same as a normal div
+	equals( $divChild.width(), $divNormal.width(), "child of a hidden element width() is wrong see #10413" );
+	equals( $divChild.innerWidth(), $divNormal.innerWidth(), "child of a hidden element innerWidth() is wrong see #10413" );
+	equals( $divChild.outerWidth(), $divNormal.outerWidth(), "child of a hidden element outerWidth() is wrong see #10413" );
+	equals( $divChild.outerWidth(true), $divNormal.outerWidth( true ), "child of a hidden element outerWidth( true ) is wrong see #10413" );
+
+	equals( $divChild.height(), $divNormal.height(), "child of a hidden element height() is wrong see #10413" );
+	equals( $divChild.innerHeight(), $divNormal.innerHeight(), "child of a hidden element innerHeight() is wrong see #10413" );
+	equals( $divChild.outerHeight(), $divNormal.outerHeight(), "child of a hidden element outerHeight() is wrong see #10413" );
+	equals( $divChild.outerHeight(true), $divNormal.outerHeight( true ), "child of a hidden element outerHeight( true ) is wrong see #10413" );
+
+	// teardown html
+	$divHiddenParent.remove();
+	$divNormal.remove();
+});
+
 test("outerHeight()", function() {
 	expect(11);
 
