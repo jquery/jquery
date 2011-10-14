a211ea27d29d8f0f25a6b16572bde76cea2c6a62
Matt Mueller <mattmuelle@gmail.com>
2011-10-14 02:41:35 -0700

    Removed irrelevant comment.

--- src/core.js
+++ src/core.js
@@ -668,8 +668,6 @@ jQuery.extend({
 
 		if ( array != null ) {
 			// The window, strings (and functions) also have 'length'
-			// The extra typeof function check is to prevent crashes
-			// in Safari 2 (See: #3039)
 			// Tweaked logic slightly to handle Blackberry 4.7 RegExp issues #6930
 			var type = jQuery.type( array );
 
