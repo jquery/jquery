dbf40fa5283f4b286e0485db82208f0112a7c09b
jrburke <jrburke@gmail.com>
2011-11-07 21:28:11 -0800

    Add standard function wrapping.

--- src/exports.js
+++ src/exports.js
@@ -1,3 +1,5 @@
+(function( jQuery ) {
+
 // Expose jQuery to the global object
 window.jQuery = window.$ = jQuery;
 
@@ -16,3 +18,5 @@ window.jQuery = window.$ = jQuery;
 if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
 	define( "jquery", [], function () { return jQuery; } );
 }
+
+})( jQuery );
