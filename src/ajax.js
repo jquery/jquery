b2a35f4725d17dfacda174148eacf48fdcac939d
Mike Sherov <mike.sherov@gmail.com>
2011-12-06 22:06:05 -0500

    getting JSHint to work the same as our current JSLint works, fixes #10692.
    also, still needs to wait on this sizzle PR: https://github.com/jquery/sizzle/pull/82

--- src/ajax.js
+++ src/ajax.js
@@ -21,21 +21,21 @@ var r20 = /%20/g,
 	_load = jQuery.fn.load,
 
 	/* Prefilters
-	 * 1) They are useful to introduce custom dataTypes (see ajax/jsonp.js for an example)
-	 * 2) These are called:
-	 *    - BEFORE asking for a transport
-	 *    - AFTER param serialization (s.data is a string if s.processData is true)
-	 * 3) key is the dataType
-	 * 4) the catchall symbol "*" can be used
-	 * 5) execution will start with transport dataType and THEN continue down to "*" if needed
-	 */
+	* 1) They are useful to introduce custom dataTypes (see ajax/jsonp.js for an example)
+	* 2) These are called:
+	*    - BEFORE asking for a transport
+	*    - AFTER param serialization (s.data is a string if s.processData is true)
+	* 3) key is the dataType
+	* 4) the catchall symbol "*" can be used
+	* 5) execution will start with transport dataType and THEN continue down to "*" if needed
+	*/
 	prefilters = {},
 
 	/* Transports bindings
-	 * 1) key is the dataType
-	 * 2) the catchall symbol "*" can be used
-	 * 3) selection will start with transport dataType and THEN go to "*" if needed
-	 */
+	* 1) key is the dataType
+	* 2) the catchall symbol "*" can be used
+	* 3) selection will start with transport dataType and THEN go to "*" if needed
+	*/
 	transports = {},
 
 	// Document location
