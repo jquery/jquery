98422024f72e52bd6af835cd61f0f7d0f9ed9adb
Mike Sherov <mike.sherov@gmail.com>
2011-12-07 22:20:13 -0500

    what was I thinking? this is a much better way to only use the right properties

--- src/css.js
+++ src/css.js
@@ -332,20 +332,19 @@ function getWidthOrHeight( elem, name, extra ) {
 
 	// Start with offset property
 	var val = name === "width" ? elem.offsetWidth : elem.offsetHeight,
-		expandIndex = name === "width" ? [ 1, 3 ] : [ 0, 2 ],
-		i = 0,
-		len = 2;
+		i = name === "width" ? 1 : 0,
+		len = 4;
 
 	if ( val > 0 ) {
 		if ( extra !== "border" ) {
-			for ( ; i < len; i++ ) {
+			for ( ; i < len; i+=2 ) {
 				if ( !extra ) {
-					val -= parseFloat( jQuery.css( elem, "padding" + cssExpand[ expandIndex[ i ] ] ) ) || 0;
+					val -= parseFloat( jQuery.css( elem, "padding" + cssExpand[ i ] ) ) || 0;
 				}
 				if ( extra === "margin" ) {
-					val += parseFloat( jQuery.css( elem, extra + cssExpand[ expandIndex[ i ] ] ) ) || 0;
+					val += parseFloat( jQuery.css( elem, extra + cssExpand[ i ] ) ) || 0;
 				} else {
-					val -= parseFloat( jQuery.css( elem, "border" + cssExpand[ expandIndex[ i ] ] + "Width" ) ) || 0;
+					val -= parseFloat( jQuery.css( elem, "border" + cssExpand[ i ] + "Width" ) ) || 0;
 				}
 			}
 		}
@@ -363,13 +362,13 @@ function getWidthOrHeight( elem, name, extra ) {
 
 	// Add padding, border, margin
 	if ( extra ) {
-		for ( ; i < len; i++ ) {
-			val += parseFloat( jQuery.css( elem, "padding" + cssExpand[ expandIndex[ i ] ] ) ) || 0;
+		for ( ; i < len; i+=2 ) {
+			val += parseFloat( jQuery.css( elem, "padding" + cssExpand[ i ] ) ) || 0;
 			if ( extra !== "padding" ) {
-				val += parseFloat( jQuery.css( elem, "border" + cssExpand[ expandIndex[ i ] ] + "Width" ) ) || 0;
+				val += parseFloat( jQuery.css( elem, "border" + cssExpand[ i ] + "Width" ) ) || 0;
 			}
 			if ( extra === "margin" ) {
-				val += parseFloat( jQuery.css( elem, extra + cssExpand[ expandIndex[ i ] ]) ) || 0;
+				val += parseFloat( jQuery.css( elem, extra + cssExpand[ i ]) ) || 0;
 			}
 		}
 	}
