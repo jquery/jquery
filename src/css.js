1f92012e8df0e031370cb5501589d6c6d9e67d60
cmc3cn <59194618@qq.com>
2011-12-30 14:26:33 +0800

    The curCSS function only need 2 arguments

--- src/css.js
+++ src/css.js
@@ -35,7 +35,7 @@ jQuery.extend({
 			get: function( elem, computed ) {
 				if ( computed ) {
 					// We should always get a number back from opacity
-					var ret = curCSS( elem, "opacity", "opacity" );
+					var ret = curCSS( elem, "opacity" );
 					return ret === "" ? "1" : ret;
 
 				} else {
