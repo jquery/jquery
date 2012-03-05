d09f2c830b10e3912d36519c1bcbcabf75accbe2
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-03-05 14:52:36 -0500

    Porting selector.js, sizer
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- grunt.js
+++ grunt.js
@@ -4,6 +4,18 @@ config.init({
   meta: {
     banner: "/*! jQuery v@<%= pkg.version %> jquery.com | jquery.org/license */"
   },
+  compare_size: {
+    files: [
+      "dist/jquery.js",
+      "dist/jquery.min.js"
+    ]
+  },
+  selector: {
+    "src/selector.js": [
+      "src/sizzle-jquery.js",
+      "src/sizzle/sizzle.js"
+    ]
+  },
   build: {
     "dist/jquery.js": [
       "src/intro.js",
@@ -53,7 +65,8 @@ config.init({
       smarttabs: true,
       predef: [
         "define",
-        "DOMParser"
+        "DOMParser",
+        "__dirname"
       ],
       maxerr: 100
     },
@@ -71,13 +84,117 @@ config.init({
 });
 
 // Default task.
-task.registerTask("default", "build lint min");
+task.registerTask("default", "selector build lint min compare_size");
+
+
+// Compare size to master
+task.registerBasicTask("compare_size", "Compare size of this branch to master", function(data) {
+  var files = file.expand(data),
+      done = this.async(),
+      sizecache = __dirname + "/build/.sizecache.json",
+      sources = {
+        min: file.read(files[1]),
+        max: file.read(files[0])
+      },
+      oldsizes = {},
+      sizes = {};
+
+  try {
+    oldsizes = JSON.parse(file.read(sizecache));
+  } catch(e) {
+    oldsizes = {};
+  }
+
+  // Obtain the current branch and continue...
+  task.helper("git_current_branch", function(err, branch) {
+    var key, diff;
+
+    // Derived and adapted from Corey Frang's original `sizer`
+    log.writeln( "jQuery Size - compared to master" );
+
+    sizes["jquery.js"] = sources.max.length;
+    sizes["jquery.min.js"] = sources.min.length;
+    sizes["jquery.min.js.gz"] = task.helper("gzip", sources.min).length;
+
+    for ( key in sizes ) {
+      diff = oldsizes[ key ] && ( sizes[ key ] - oldsizes[ key ] );
+      if ( diff > 0 ) {
+        diff = "+" + diff;
+      }
+      console.log( "%s %s %s",
+        task.helper("lpad",  sizes[ key ], 8 ),
+        task.helper("lpad",  diff ? "(" + diff + ")" : "(-)", 8 ),
+        key
+      );
+    }
+
+    if ( branch === "master" ) {
+      // If master, write to file - this makes it easier to compare
+      // the size of your current code state to the master branch,
+      // without returning to the master to reset the cache
+      file.write( sizecache, JSON.stringify(sizes) );
+
+      // TODO: Multi key:val entries in the sizecache, using branch name as key?
+    }
+    done();
+  });
+
+  // Fail task if errors were logged.
+  if (task.hadErrors()) { return false; }
+
+  // Otherwise, print a success message.
+  // log.writeln("File '" + name + "' created.");
+});
+
+task.registerHelper("git_current_branch", function(done) {
+  task.helper("child_process", {
+    cmd: "git",
+    args: ["branch", "--no-color"]
+  }, function(err, result) {
+    var branch;
+
+    result.split("\n").forEach(function(branch) {
+      var matches = /^\* (.*)/.exec( branch );
+      if ( matches != null && matches.length && matches[ 1 ] ) {
+        done( null, matches[ 1 ] );
+      }
+    });
+  });
+});
+
+task.registerHelper("lpad", function(str, len, chr) {
+  return ( Array( len + 1 ).join( chr || " " ) + str ).substr( -len );
+});
+
+// Build src/selector.js
+task.registerBasicTask("selector", "Build src/selector.js", function(data, name) {
+  var files = file.expand(data),
+      sizzle = {
+        api: file.read(files[ 0 ]),
+        src: file.read(files[ 1 ])
+      },
+      compiled;
+
+  // sizzle-jquery.js -> sizzle after "EXPOSE", replace window.Sizzle
+  compiled = sizzle.src.replace("window.Sizzle = Sizzle;", sizzle.api);
+  verbose.write("Injected sizzle-jquery.js into sizzle.js");
+
+  // Write concatenated source to file
+  file.write(name, compiled);
+
+
+  // Fail task if errors were logged.
+  if (task.hadErrors()) { return false; }
+
+  // Otherwise, print a success message.
+  log.writeln("File '" + name + "' created.");
+});
+
 
 // Special concat/build task to handle various jQuery build requirements
 task.registerBasicTask("build", "Concatenate source, embed date/version", function(data, name) {
   // Concat specified files.
-  var files = file.expand(data),
-      compiled = "";
+  var compiled = "";
 
   file.expand(data).forEach(function(filepath) {
     compiled += file.read(filepath).replace(/.function..jQuery...\{/g, "").replace(/\}...jQuery..;/g, "");
@@ -86,7 +203,7 @@ task.registerBasicTask("build", "Concatenate source, embed date/version", functi
   // Embed Date
   // Embed Version
   compiled = compiled.replace("@DATE", new Date())
-                .replace("@VERSION", pkg.version);
+                .replace("@VERSION", config("pkg.version"));
 
   // Write concatenated source to file
   file.write(name, compiled);
