453f7c8a2f7ce4a6c9f73dd3d1c6c6a2a5cb0ffc
Sindre Sorhus <sindresorhus@gmail.com>
2012-03-22 10:59:29 +0100

    Add testcase to #11469 and make it more succinct
    [jsperf](http://jsperf.com/indexof-vs-regex2)

--- src/effects.js
+++ src/effects.js
@@ -636,8 +636,8 @@ jQuery.extend( jQuery.fx, {
 
 // Ensure props that can't be negative don't go there on undershoot easing
 jQuery.each( fxAttrs.concat.apply( [], fxAttrs ), function( i, prop ) {
-	// exclude marginTop, marginLeft, marginBottom and marginRight from this list
-	if ( prop.indexOf( "margin" ) ) {
+	// Exclude marginTop, marginLeft, marginBottom and marginRight from this list
+	if ( prop.indexOf("margin") === -1 ) {
 		jQuery.fx.step[ prop ] = function( fx ) {
 			jQuery.style( fx.elem, prop, Math.max(0, fx.now) + fx.unit );
 		};
