4d7ef581f14c5eeb74bc18a9127aa7e6e9d51897
Richard Gibson <richard.gibson@gmail.com>
2012-04-19 09:45:42 -0400

    improve clarity

--- src/deferred.js
+++ src/deferred.js
@@ -6,11 +6,12 @@ var // Static reference to slice
 jQuery.extend({
 
 	Deferred: function( func ) {
-		var doneList = jQuery.Callbacks( "once memory" ),
-			failList = jQuery.Callbacks( "once memory" ),
-			progressList = jQuery.Callbacks( "memory" ),
+		var doneList = jQuery.Callbacks("once memory"),
+			failList = jQuery.Callbacks("once memory"),
+			progressList = jQuery.Callbacks("memory"),
 			state = "pending",
 			tuples = [
+				// action, add listener, listener list, final state
 				[ "resolve", "done", doneList, "resolved" ],
 				[ "reject", "fail", failList, "rejected" ],
 				[ "notify", "progress", progressList ]
@@ -38,6 +39,7 @@ jQuery.extend({
 						jQuery.each( tuples, function( i, tuple ) {
 							var action = tuple[ 0 ],
 								fn = fns[ i ];
+							// deferred[ done | fail | progress ] for forwarding actions to newDefer
 							deferred[ tuple[1] ]( jQuery.isFunction( fn ) ?
 								function() {
 									var returned = fn.apply( this, arguments );
@@ -77,9 +79,9 @@ jQuery.extend({
 			deferred[ tuple[0] ] = tuple[ 2 ].fire;
 			deferred[ tuple[0] + "With" ] = tuple[ 2 ].fireWith;
 
+			// deferred[ done | fail ]( state: [ resolved | rejected ], [ failList | doneList].disable, ... )
 			if ( i <= 1 ) {
-				// deferred[ done | fail ]( state: [ resolved | rejected ], [ failList | doneList].disable, ... )
-				deferred[ tuple[1] ]( function() {
+				deferred[ tuple[1] ](function() {
 					state = tuple[ 3 ];
 				}, tuples[ 1 - i ][ 2 ].disable, progressList.lock );
 			}
@@ -99,6 +101,8 @@ jQuery.extend({
 		function resolveFunc( i ) {
 			return function( value ) {
 				args[ i ] = arguments.length > 1 ? sliceDeferred.call( arguments ) : value;
+
+				// if this was the last subordinate, resolve the master
 				if ( !( --remaining ) ) {
 					deferred.resolveWith( deferred, args );
 				}
@@ -106,21 +110,26 @@ jQuery.extend({
 		}
 		function progressFunc( i ) {
 			return function( value ) {
-				pValues[ i ] = arguments.length > 1 ? sliceDeferred.call( arguments ) : value;
-				deferred.notifyWith( promise, pValues );
+				progressValues[ i ] = arguments.length > 1 ? sliceDeferred.call( arguments ) : value;
+				deferred.notifyWith( promise, progressValues );
 			};
 		}
 
 		var args = sliceDeferred.call( arguments ),
 			i = 0,
 			length = args.length,
-			pValues = new Array( length ),
+			progressValues = new Array( length ),
+
+			// the count of uncompleted subordinates
 			remaining = length !== 1 || ( firstParam && jQuery.isFunction( firstParam.promise ) ) ? length : 0,
+
+			// the master Deferred. If args consist of only a single Deferred, just use that.
 			deferred = remaining === 1 ?
 				firstParam :
 				jQuery.Deferred(),
 			promise = deferred.promise();
 
+		// add listeners to Deferred subordinates; treat others as resolved
 		if ( length > 1 ) {
 			for ( ; i < length; i++ ) {
 				if ( args[ i ] && jQuery.isFunction( args[ i ].promise ) ) {
@@ -131,6 +140,7 @@ jQuery.extend({
 			}
 		}
 
+		// if we're not waiting on anything, resolve the master
 		if ( !remaining ) {
 			deferred.resolveWith( deferred, args );
 		}
