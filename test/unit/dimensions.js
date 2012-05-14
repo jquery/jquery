ca48d490407b198e022b0d0294a7493602155f74
Matt Farmer <matt@frmr.me>
2012-05-14 14:12:14 -0400

    Fix #6724, though the Mobile Safari wound remains. Closes gh-764.

--- test/unit/dimensions.js
+++ test/unit/dimensions.js
@@ -9,7 +9,7 @@ function fn( val ) {
 }
 
 function testWidth( val ) {
-	expect(8);
+	expect(9);
 
 	var $div = jQuery("#nothiddendiv");
 	$div.width( val(30) );
@@ -33,6 +33,8 @@ function testWidth( val ) {
 	var blah = jQuery("blah");
 	equal( blah.width( val(10) ), blah, "Make sure that setting a width on an empty set returns the set." );
 	equal( blah.width(), null, "Make sure 'null' is returned on an empty set");
+
+	equal( jQuery(window).width(), document.documentElement.clientWidth, "Window width is equal to width reported by window/document." );
 
 	jQuery.removeData($div[0], "olddisplay", true);
 }
@@ -63,7 +65,7 @@ test("width(Function(args))", function() {
 });
 
 function testHeight( val ) {
-	expect(8);
+	expect(9);
 
 	var $div = jQuery("#nothiddendiv");
 	$div.height( val(30) );
@@ -87,6 +89,8 @@ function testHeight( val ) {
 	var blah = jQuery("blah");
 	equal( blah.height( val(10) ), blah, "Make sure that setting a height on an empty set returns the set." );
 	equal( blah.height(), null, "Make sure 'null' is returned on an empty set");
+
+	equal( jQuery(window).height(), document.documentElement.clientHeight, "Window width is equal to width reported by window/document." );
 
 	jQuery.removeData($div[0], "olddisplay", true);
 }
