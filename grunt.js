6a7f2492d0c15c007f539d400f2e83884d42fe82
Dave Methvin <dave.methvin@gmail.com>
2012-06-11 22:04:43 -0400

    Run the `dist` task *after* minifying so we copy the new one.

--- grunt.js
+++ grunt.js
@@ -121,7 +121,7 @@ module.exports = function( grunt ) {
 	});
 
 	// Default grunt.
-	grunt.registerTask( "default", "submodules selector build:*:* dist:* lint min compare_size" );
+	grunt.registerTask( "default", "submodules selector build:*:* lint min dist:* compare_size" );
 
 	// Short list as a high frequency watch task
 	grunt.registerTask( "dev", "selector build:*:* lint" );
