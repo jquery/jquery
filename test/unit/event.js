9bb3494ce99889e05a7333e38e4da9579ce6af8e
Timo Tijhof <krinklemail@gmail.com>
2012-06-18 04:42:24 +0200

    Fix #11925, Pass eventHandle to special.teardown. Closes gh-831.
    * Added unit test to confirm.
      The third assertion fails without the fix in ./src/event.js

--- test/unit/event.js
+++ test/unit/event.js
@@ -1382,6 +1382,21 @@ test("Submit event can be stopped (#11049)", function() {
 	form.remove();
 });
 
+test("on(beforeunload) creates/deletes window property instead of adding/removing event listener", function() {
+	expect(3);
+
+	equal( window.onbeforeunload, null, "window property is null/undefined up until now" );
+
+	var handle = function () {};
+	jQuery(window).on( "beforeunload", handle );
+
+	equal( typeof window.onbeforeunload, "function", "window property is set to a function");
+
+	jQuery(window).off( "beforeunload", handle );
+
+	equal( window.onbeforeunload, null, "window property has been unset to null/undefined" );
+})
+
 test("jQuery.Event( type, props )", function() {
 
 	expect(5);
