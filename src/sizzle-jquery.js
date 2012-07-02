800f47a5476a64501d7bbaaf43e498963cf804d8
timmywil <timmywillisn@gmail.com>
2012-07-02 15:24:33 -0400

    Sizzle: minor adjustments and a small fix in the child selector regex

--- src/sizzle-jquery.js
+++ src/sizzle-jquery.js
@@ -2,7 +2,7 @@
 Sizzle.attr = jQuery.attr;
 jQuery.find = Sizzle;
 jQuery.expr = Sizzle.selectors;
-jQuery.expr[":"] = jQuery.expr.filters;
+jQuery.expr[":"] = jQuery.expr.pseudos;
 jQuery.unique = Sizzle.uniqueSort;
 jQuery.text = Sizzle.getText;
 jQuery.isXMLDoc = Sizzle.isXML;
