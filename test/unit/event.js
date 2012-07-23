e761e0c6e0561093ff1e52d0cba6ace04daa8798
Richard Gibson <richard.gibson@gmail.com>
2012-07-23 12:48:29 -0400

    Support event delegation with relative selectors. Fixes #10762. Closes gh-860.

--- test/unit/event.js
+++ test/unit/event.js
@@ -2342,17 +2342,20 @@ test("jQuery.off using dispatched jQuery.Event", function() {
 		.remove();
 });
 
-test( "delegated event with delegateTarget-relative selector (#)", function() {
-	expect(1);
-	var markup = jQuery( '<ul><li><ul id="u1"><li id="f1"></li></ul></li>' ).appendTo("body");
+test( "delegated event with delegateTarget-relative selector", function() {
+	expect(2);
+	var markup = jQuery( '<ul><li><a id="a0"></a><ul id="ul0"><li><a id="a0_0"></a></li><li><a id="a0_1"></a></li></ul></li></ul>' ).appendTo("body");
 
 	markup
-		.find("#u1")
-			.on( "click", "li:first", function() {
-				ok( this.id === "f1" , "first li under #u1 was clicked" );
+		.on( "click", ">li>a", function() {
+			ok( this.id === "a0", "child li was clicked" );
+		})
+		.find("#ul0")
+			.on( "click", "li:first>a", function() {
+				ok( this.id === "a0_0" , "first li under #u10 was clicked" );
 			})
-			.find("#f1").click().end()
 		.end()
+		.find("a").click().end()
 		.remove();
 });
 
