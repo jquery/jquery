da4d60929778490094fbf1f9c6abd9ea7d0e0c99
Mike Sherov <mike.sherov@gmail.com>
2012-07-25 08:49:51 -0400

    Followup #12018, fix a test fail converting to pixels. Close gh-874.

--- src/css.js
+++ src/css.js
@@ -290,7 +290,7 @@ if ( window.getComputedStyle ) {
 			// Chrome < 17 and Safari 5.0 uses "computed value" instead of "used value" for margin-right
 			// Safari 5.1.7 (at least) returns percentage for a larger set of values, but width seems to be reliably pixels
 			// this is against the CSSOM draft spec: http://dev.w3.org/csswg/cssom/#resolved-values
-			if ( rnumnonpx.test( ret ) && !rposition.test( name ) ) {
+			if ( rnumnonpx.test( ret ) && rmargin.test( name ) ) {
 				width = style.width;
 				minWidth = style.minWidth;
 				maxWidth = style.maxWidth;
