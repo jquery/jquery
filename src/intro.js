060eabc9e82cce3d5c3867da0ea738655a7c1618
Dave Methvin <dave.methvin@gmail.com>
2012-08-08 22:12:10 -0400

    Finish the license changes, including Sizzle.

--- src/intro.js
+++ src/intro.js
@@ -2,14 +2,12 @@
  * jQuery JavaScript Library v@VERSION
  * http://jquery.com/
  *
- * Copyright (c) 2012 jQuery Foundation and other contributors
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://jquery.org/license
- *
  * Includes Sizzle.js
  * http://sizzlejs.com/
- * Copyright 2012, The Dojo Foundation
- * Released under the MIT, BSD, and GPL Licenses.
+ *
+ * Copyright 2012 jQuery Foundation and other contributors
+ * Released under the MIT license
+ * http://jquery.org/license
  *
  * Date: @DATE
  */
