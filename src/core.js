73cb6668c91d6617e18935f501112f480f99c55f
Marcus Ekwall <marcus.ekwall@gmail.com>
2012-08-23 16:21:48 +0300

    Revert to `core_slice`

--- src/core.js
+++ src/core.js
@@ -250,7 +250,7 @@ jQuery.fn = jQuery.prototype = {
 	},
 
 	slice: function( b, e ) {
-		return this.pushStack(  Array.prototype.slice.call( this, b, e ),
+		return this.pushStack(  core_slice.slice.call( this, b, e ),
                         "slice", "" + ( !e ? b : b+","+e ) );
 	},
 
