2d0c2df735098f84843293d640abfab534e1ff4e
Richard Gibson <richard.gibson@gmail.com>
2012-09-26 16:51:35 -0400

    Fix #12357: be more kind to non-browser environments

--- src/support.js
+++ src/support.js
@@ -19,10 +19,9 @@ jQuery.support = (function() {
 
 	all = div.getElementsByTagName("*");
 	a = div.getElementsByTagName("a")[ 0 ];
-	a.style.cssText = "top:1px;float:left;opacity:.5";
 
 	// Can't get basic test support
-	if ( !all || !all.length ) {
+	if ( !all || !a || !all.length ) {
 		return {};
 	}
 
@@ -31,6 +30,7 @@ jQuery.support = (function() {
 	opt = select.appendChild( document.createElement("option") );
 	input = div.getElementsByTagName("input")[ 0 ];
 
+	a.style.cssText = "top:1px;float:left;opacity:.5";
 	support = {
 		// IE strips leading whitespace when .innerHTML is used
 		leadingWhitespace: ( div.firstChild.nodeType === 3 ),
