6aaf01ff0e33da3dd89863e08279e0611357e7ae
David Bonner <dbonner@cogolabs.com>
2012-10-15 16:54:24 -0400

    make deprecates unittest behave like css
    hopefully this will keep deprecated tests from running when we built
    without deprecated.

--- test/unit/deprecated.js
+++ test/unit/deprecated.js
@@ -1,28 +1,26 @@
+if ( jQuery.browser ) {
 module("deprecated");
 
-// Start jQuery.browser tests
-if ( jQuery.browser && jQuery.uaMatch ) {
-	if ( jQuery.get && !isLocal ) {
-		asyncTest( "browser", function() {
-			jQuery.get( "data/ua.txt", function( data ) {
-				var uas = data.split( "\n" );
-				expect( (uas.length - 1) * 2 );
+if ( jQuery.get && !isLocal ) {
+	asyncTest( "browser", function() {
+		jQuery.get( "data/ua.txt", function( data ) {
+			var uas = data.split( "\n" );
+			expect( (uas.length - 1) * 2 );
 
-				jQuery.each(uas, function() {
-					var parts = this.split( "\t" ),
-							agent = parts[2],
-							ua;
+			jQuery.each(uas, function() {
+				var parts = this.split( "\t" ),
+						agent = parts[2],
+						ua;
 
-					if ( agent ) {
-						ua = jQuery.uaMatch( agent );
-						equal( ua.browser, parts[0], "browser (" + agent + ")" );
-						equal( ua.version, parts[1], "version (" + agent + ")" );
-					}
-				});
-
-				start();
+				if ( agent ) {
+					ua = jQuery.uaMatch( agent );
+					equal( ua.browser, parts[0], "browser (" + agent + ")" );
+					equal( ua.version, parts[1], "version (" + agent + ")" );
+				}
 			});
+
+			start();
 		});
-	}
+	});
 }
-// End of jQuery.browser tests
+}
