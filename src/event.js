dc9913a0bfc169e2b2eb075fe0390f2b98e077f3
Dave Methvin <dave.methvin@gmail.com>
2012-11-01 17:51:34 -0400

    Fix #12828. Remove non-standard event properties.

--- src/event.js
+++ src/event.js
@@ -435,8 +435,7 @@ jQuery.event = {
 	},
 
 	// Includes some event props shared by KeyEvent and MouseEvent
-	// *** attrChange attrName relatedNode srcElement  are not normalized, non-W3C, deprecated, will be removed in 1.8 ***
-	props: "attrChange attrName relatedNode srcElement altKey bubbles cancelable ctrlKey currentTarget eventPhase metaKey relatedTarget shiftKey target timeStamp view which".split(" "),
+	props: "altKey bubbles cancelable ctrlKey currentTarget eventPhase metaKey relatedTarget shiftKey target timeStamp view which".split(" "),
 
 	fixHooks: {},
 
