bbfc009ff4158028d76690d4d087a7fd14a7a67b
Thiago de Arruda <tpadilha84@gmail.com>
2012-11-30 09:58:02 -0300

    Fixed 'contents' method to work when 'childNodes' is null

--- src/traversing.js
+++ src/traversing.js
@@ -183,7 +183,7 @@ jQuery.each({
 	contents: function( elem ) {
 		return jQuery.nodeName( elem, "iframe" ) ?
 			elem.contentDocument || elem.contentWindow.document :
-			jQuery.merge( [], elem.childNodes );
+			jQuery.merge( [], elem.childNodes || []);
 	}
 }, function( name, fn ) {
 	jQuery.fn[ name ] = function( until, selector ) {
