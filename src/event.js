d2f0839e86aced4500247a654ac06ec393a5b77c
Richard Gibson <richard.gibson@gmail.com>
2012-12-05 21:29:48 -0500

    reduce size

--- src/event.js
+++ src/event.js
@@ -2,51 +2,52 @@ var rformElems = /^(?:input|select|textarea)$/i,
 	rkeyEvent = /^key/,
 	rmouseEvent = /^(?:mouse|contextmenu)|click/,
 	rfocusMorph = /^(?:focusinfocus|focusoutblur)$/,
-	rtypenamespace = /^([^.]*)(?:\.(.+)|)$/;
+	rtypenamespace = /^([^.]*)(?:\.(.+)|)$/,
 
-// Fix focusin/focusout events
-function fixFocus() {
-	if ( !jQuery.support.focusinBubbles || !jQuery.support.focusOrder ) {
-		jQuery.each({ focus: "focusin", blur: "focusout" }, function( orig, fix ) {
+	// Fixing focusin/focusout events must wait for document.body
+	fixFocus = function() {
+		if ( !jQuery.support.focusinBubbles || !jQuery.support.focusOrder ) {
+			jQuery.each({ focus: "focusin", blur: "focusout" }, function( orig, fix ) {
 
-			// Attach a single capturing handler while someone wants focusin/focusout
-			var attaches = 0,
-				handler = function( event ) {
-					jQuery.event.simulate( fix, event.target, jQuery.event.fix( event ), true );
-				};
+				// Attach a single capturing handler while someone wants focusin/focusout
+				var attaches = 0,
+					handler = function( event ) {
+						jQuery.event.simulate( fix, event.target, jQuery.event.fix( event ), true );
+					};
 
-			// Create "bubbling" focus and blur events
-			if ( !jQuery.support.focusinBubbles ) {
-				jQuery.event.special[ fix ] = {
-					setup: function() {
-						if ( attaches++ === 0 ) {
-							document.addEventListener( orig, handler, true );
+				// Create "bubbling" focus and blur events
+				if ( !jQuery.support.focusinBubbles ) {
+					jQuery.event.special[ fix ] = {
+						setup: function() {
+							if ( attaches++ === 0 ) {
+								document.addEventListener( orig, handler, true );
+							}
+						},
+						teardown: function() {
+							if ( --attaches === 0 ) {
+								document.removeEventListener( orig, handler, true );
+							}
 						}
-					},
-					teardown: function() {
-						if ( --attaches === 0 ) {
-							document.removeEventListener( orig, handler, true );
+					};
+				} else {
+
+					// Correct focusin/focusout "bubbling"
+					jQuery.event.special[ orig ].preDispatch = function( event, handlers ) {
+						var self = this;
+
+						// If we have corresponded events, wait until they fire first
+						if ( handlers[ fix ] ) {
+							setTimeout(function() {
+								jQuery.event.dispatch.call( self, event );
+							}, 0 );
+
+							return false;
 						}
-					}
-				};
-			} else {
-
-				// Correct focusin/focusout "bubbling"
-				jQuery.event.special[ orig ].preDispatch = function( event, handlers ) {
-
-					// If we have corresponded events, wait until they fire first
-					if ( handlers[ fix ] ) {
-						setTimeout(function( self ) {
-							jQuery.event.dispatch.call( self, event );
-						}, 0, this );
-
-						return false;
-					}
-				};
-			}
-		});
-	}
-}
+					};
+				}
+			});
+		}
+	};
 
 /*
  * Helper functions for managing events -- not part of the public interface.
@@ -373,17 +374,17 @@ jQuery.event = {
 
 	dispatch: function( event ) {
 		var i, j, cur, ret, selMatch, matched, matches, handleObj, sel, handlers, delegateCount, special,
+			handlerQueue = [],
 			args = core_slice.call( arguments ),
 			events = jQuery._data( this, "events" ) || {},
-			handlerQueue = [],
 			isNative = !event[ jQuery.expando ];
 
 		// Make a writable jQuery.Event from the native event object if required
 		event = isNative ? jQuery.event.fix( event ) : event;
 
 		handlers = events[ event.type ] || [];
-		delegateCount = handlers.delegateCount;
 		special = jQuery.event.special[ event.type ] || {};
+		delegateCount = handlers.delegateCount;
 
 		// Use the fix-ed jQuery.Event rather than the (read-only) native event
 		args[0] = event;
@@ -864,10 +865,6 @@ if ( !jQuery.support.changeBubbles ) {
 	};
 }
 
-// We have to wait until document is loaded, support tests rely on that
-// In case jQuery is placed in the end of the document, we have to check for body element
-document.body ? fixFocus() : jQuery( fixFocus );
-
 jQuery.fn.extend({
 
 	on: function( types, selector, data, fn, /*INTERNAL*/ one ) {
@@ -1017,3 +1014,11 @@ jQuery.each( ("blur focus focusin focusout load resize scroll unload click dblcl
 		jQuery.event.fixHooks[ name ] = jQuery.event.mouseHooks;
 	}
 });
+
+// We have to wait until document is loaded, support tests rely on that
+// In case jQuery is placed in the end of the document, we have to check for body element
+if ( document.body ) {
+	fixFocus();
+} else {
+	jQuery( fixFocus );
+}
