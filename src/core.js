af06c3c771f5935a993a7c741105eac78dd10fa7
Rick Waldron <waldron.rick@gmail.com>
2012-12-17 17:05:41 -0500

    2.0: Remove inArray shim
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- src/core.js
+++ src/core.js
@@ -615,25 +615,7 @@ jQuery.extend({
 	},
 
 	inArray: function( elem, arr, i ) {
-		var len;
-
-		if ( arr ) {
-			if ( core_indexOf ) {
-				return core_indexOf.call( arr, elem, i );
-			}
-
-			len = arr.length;
-			i = i ? i < 0 ? Math.max( 0, len + i ) : i : 0;
-
-			for ( ; i < len; i++ ) {
-				// Skip accessing in sparse arrays
-				if ( i in arr && arr[ i ] === elem ) {
-					return i;
-				}
-			}
-		}
-
-		return -1;
+		return core_indexOf.call( arr, elem, i );
 	},
 
 	merge: function( first, second ) {
