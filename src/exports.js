b7570dec50eab3379c39935d5f4ce73ce9ee1b7b
isaacs <i@izs.me>
2012-12-26 09:28:57 -0800

    Support node-like module loaders
    Also, do not pollute the global namespace in Node module loaders.

--- src/exports.js
+++ src/exports.js
@@ -1,18 +1,26 @@
-// Expose jQuery to the global object
-window.jQuery = window.$ = jQuery;
+if ( typeof module === "object" && typeof module.exports === "object" ) {
+	// Expose jQuery as module.exports in loaders that implement the Node
+	// module pattern (including browserify). Do not create the global, since
+	// the user will be storing it themselves locally, and globals are frowned
+	// upon in the Node module world.
+	module.exports = jQuery;
+} else {
+	// Otherwise expose jQuery to the global object as usual
+	window.jQuery = window.$ = jQuery;
 
-// Expose jQuery as an AMD module, but only for AMD loaders that
-// understand the issues with loading multiple versions of jQuery
-// in a page that all might call define(). The loader will indicate
-// they have special allowances for multiple jQuery versions by
-// specifying define.amd.jQuery = true. Register as a named module,
-// since jQuery can be concatenated with other files that may use define,
-// but not use a proper concatenation script that understands anonymous
-// AMD modules. A named AMD is safest and most robust way to register.
-// Lowercase jquery is used because AMD module names are derived from
-// file names, and jQuery is normally delivered in a lowercase file name.
-// Do this after creating the global so that if an AMD module wants to call
-// noConflict to hide this version of jQuery, it will work.
-if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
-	define( "jquery", [], function () { return jQuery; } );
+	// Expose jQuery as an AMD module, but only for AMD loaders that
+	// understand the issues with loading multiple versions of jQuery
+	// in a page that all might call define(). The loader will indicate
+	// they have special allowances for multiple jQuery versions by
+	// specifying define.amd.jQuery = true. Register as a named module,
+	// since jQuery can be concatenated with other files that may use define,
+	// but not use a proper concatenation script that understands anonymous
+	// AMD modules. A named AMD is safest and most robust way to register.
+	// Lowercase jquery is used because AMD module names are derived from
+	// file names, and jQuery is normally delivered in a lowercase file name.
+	// Do this after creating the global so that if an AMD module wants to call
+	// noConflict to hide this version of jQuery, it will work.
+	if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
+		define( "jquery", [], function () { return jQuery; } );
+	}
 }
