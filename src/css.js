45ac759147902099918871b9dee91b0c096b37d1
Oleg <markelog@gmail.com>
2013-01-08 02:04:27 +0400

    Move patch from try-catch

--- src/css.js
+++ src/css.js
@@ -211,19 +211,18 @@ jQuery.extend({
 				value += "px";
 			}
 
+			// Fixes #8908, it can be done more correctly by specifing setters in cssHooks,
+			// but it would mean to define eight (for every problematic property) identical functions
+			if ( !jQuery.support.clearCloneStyle && value === "" && name.indexOf("background") === 0 ) {
+				style[ name ] = "inherit";
+			}
+
 			// If a hook was provided, use that value, otherwise just set the specified value
 			if ( !hooks || !("set" in hooks) || (value = hooks.set( elem, value, extra )) !== undefined ) {
 
 				// Wrapped to prevent IE from throwing errors when 'invalid' values are provided
 				// Fixes bug #5509
 				try {
-
-					// Fixes #8908, it can be done more correctly by specifing setters in cssHooks,
-					// but it would mean to define eight (for every problematic property) identical functions
-					if ( !jQuery.support.clearCloneStyle && value === "" && name.indexOf("background") === 0 ) {
-						style[ name ] = "inherit";
-					}
-
 					style[ name ] = value;
 				} catch(e) {}
 			}
