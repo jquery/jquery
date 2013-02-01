0e2977583c0455eda940a28b2499cad2cbf24ee4
Dave Methvin <dave.methvin@gmail.com>
2013-02-01 17:17:39 -0500

    Fix #13335. Remove "use strict".

--- src/intro.js
+++ src/intro.js
@@ -12,4 +12,9 @@
  * Date: @DATE
  */
 (function( window, undefined ) {
-"use strict";
+
+// Can't do this because several apps including ASP.NET trace
+// the stack via arguments.caller.callee and Firefox dies if
+// you try to trace through "use strict" call chains. (#13335)
+// Support: Firefox 18+
+//"use strict";
