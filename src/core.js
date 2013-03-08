a9d484cba07a2096a684d1dfbdbbd0f5724f6911
Brandon Johnson <bjohn465@gmail.com>
2013-03-07 18:53:17 -0700

    Adding space after `for` statement

--- src/core.js
+++ src/core.js
@@ -476,7 +476,7 @@ jQuery.extend({
 		// Support: IE<9
 		// Handle iteration over inherited properties before own properties.
 		if ( jQuery.support.ownLast ) {
-			for( key in obj ) {
+			for ( key in obj ) {
 				return core_hasOwn.call( obj, key );
 			}
 		}
