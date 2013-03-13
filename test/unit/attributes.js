8fd6479661a89626ca44cd4562a560811653a035
ruado1987 <ruado1987@gmail.com>
2013-03-13 22:28:17 +0800

    force IE9 to behave like other browsers when a non-matching value is set on a select-one element

--- test/unit/attributes.js
+++ test/unit/attributes.js
@@ -850,10 +850,9 @@ test("val() with non-matching values on dropdown list", function() {
 	expect( 3 );
 	
 	jQuery("#select5").val( "" );
-	equal( jQuery("#select5").val(), "3", "Non-matching set on select-one" );
+	equal( jQuery("#select5").val(), 3, "Non-matching set on select-one" );
 	
-	var select6 = jQuery("<select multiple id=\"select6\"><option value=\"1\">A</option><option value=\"2\">B</option></select>")
-		.appendTo("#form");
+	var select6 = jQuery("<select multiple id=\"select6\"><option value=\"1\">A</option><option value=\"2\">B</option></select>").appendTo("#form");
 	jQuery(select6).val( "nothing" );
 	equal( jQuery(select6).val(), null, "Non-matching set (single value) on select-multiple" );
 	
