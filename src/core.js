e8cf33685f6be5388b6a512f10c083a53c7a21b2
773000785 <773000785@qq.com>
2013-04-26 19:35:35 +0800

    Update core.js
    It can be more shorten in $.each.

--- src/core.js
+++ src/core.js
@@ -544,45 +544,23 @@ jQuery.extend({
 			length = obj.length,
 			isArray = isArraylike( obj );
 
-		if ( args ) {
-			if ( isArray ) {
-				for ( ; i < length; i++ ) {
-					value = callback.apply( obj[ i ], args );
+		if ( isArray ) {
+                    for ( ; i < length; i++ ) {
+                        value = callback.apply( obj[i], args || [ i, obj[i] ] );
 
-					if ( value === false ) {
-						break;
-					}
-				}
-			} else {
-				for ( i in obj ) {
-					value = callback.apply( obj[ i ], args );
+                        if ( value === false ) {
+                            break;
+                        }
+                    }
+                } else {
+                    for ( i in obj ) {
+                        value = callback.apply( obj[i], args || [ i, obj[i] ] );
 
-					if ( value === false ) {
-						break;
-					}
-				}
-			}
-
-		// A special, fast, case for the most common use of each
-		} else {
-			if ( isArray ) {
-				for ( ; i < length; i++ ) {
-					value = callback.call( obj[ i ], i, obj[ i ] );
-
-					if ( value === false ) {
-						break;
-					}
-				}
-			} else {
-				for ( i in obj ) {
-					value = callback.call( obj[ i ], i, obj[ i ] );
-
-					if ( value === false ) {
-						break;
-					}
-				}
-			}
-		}
+                        if ( value === false ) {
+                            break;
+                        }
+                    }
+                }
 
 		return obj;
 	},
