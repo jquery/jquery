8d3c0506c860e887d0005fd12da53f1d11ce2883
Timmy Willison <timmywillisn@gmail.com>
2013-05-06 15:42:00 -0400

    Check nodeType of elements passed to selector-native's find. Fixes #13577.

--- src/selector-native.js
+++ src/selector-native.js
@@ -62,11 +62,21 @@ var selector_hasDuplicate,
 
 jQuery.extend({
 	find: function( selector, context, results, seed ) {
-		var elem,
+		var elem, nodeType,
 			i = 0;
 
 		results = results || [];
 		context = context || document;
+
+		// Same basic safeguard as Sizzle
+		if ( !selector || typeof selector !== "string" ) {
+			return results;
+		}
+
+		// Early return if context is not an element or document
+		if ( (nodeType = context.nodeType) !== 1 && nodeType !== 9 ) {
+			return [];
+		}
 
 		if ( seed ) {
 			while ( (elem = seed[i++]) ) {
