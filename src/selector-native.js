6d04ebfafb39aa0d5659be30fc98ddabc13f2609
Timmy Willison <timmywillisn@gmail.com>
2013-05-06 15:41:22 -0400

    No ticket: boolean -> bool in selector-native

--- src/selector-native.js
+++ src/selector-native.js
@@ -135,7 +135,7 @@ jQuery.extend({
 	expr: {
 		attrHandle: {},
 		match: {
-			boolean: /^(?:checked|selected|async|autofocus|autoplay|controls|defer|disabled|hidden|ismap|loop|multiple|open|readonly|required|scoped)$/i,
+			bool: /^(?:checked|selected|async|autofocus|autoplay|controls|defer|disabled|hidden|ismap|loop|multiple|open|readonly|required|scoped)$/i,
 			needsContext: /^[\x20\t\r\n\f]*[>+~]/
 		}
 	}
