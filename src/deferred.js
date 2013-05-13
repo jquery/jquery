87abfd40ac5304aadab462a3c3df1f99f63b0481
ukyo <ukyo.web@gmail.com>
2013-05-13 22:14:03 +0900

    Deferred.then catch an Error thrown by callback

--- src/deferred.js
+++ src/deferred.js
@@ -24,14 +24,18 @@ jQuery.extend({
 								fn = jQuery.isFunction( fns[ i ] ) && fns[ i ];
 							// deferred[ done | fail | progress ] for forwarding actions to newDefer
 							deferred[ tuple[1] ](function() {
-								var returned = fn && fn.apply( this, arguments );
-								if ( returned && jQuery.isFunction( returned.promise ) ) {
-									returned.promise()
-										.done( newDefer.resolve )
-										.fail( newDefer.reject )
-										.progress( newDefer.notify );
-								} else {
-									newDefer[ action + "With" ]( this === promise ? newDefer.promise() : this, fn ? [ returned ] : arguments );
+								try {
+									var returned = fn && fn.apply( this, arguments );
+									if ( returned && jQuery.isFunction( returned.promise ) ) {
+										returned.promise()
+											.done( newDefer.resolve )
+											.fail( newDefer.reject )
+											.progress( newDefer.notify );
+									} else {
+										newDefer[ action + "With" ]( this === promise ? newDefer.promise() : this, fn ? [ returned ] : arguments );
+									}
+								} catch ( err ) {
+									newDefer.rejectWith( this === promise ? newDefer.promise() : this, [ err ] );
 								}
 							});
 						});
