53095883ef423260270de50b387eb5b17963d333
Julian Aubourg <j@ubourg.net>
2013-05-23 02:11:37 +0200

    Handles HEAD requests as if they always were 204. Fixes #13922.

--- src/ajax.js
+++ src/ajax.js
@@ -615,7 +615,7 @@ jQuery.extend({
 				}
 
 				// if no content
-				if ( status === 204 ) {
+				if ( status === 204 || s.type === "HEAD" ) {
 					statusText = "nocontent";
 
 				// if not modified
