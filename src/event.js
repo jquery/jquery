3d387fbc07e9e7ba164a9a3920fefe3ea7b9bac5
silverwind <silv3rwind@gmail.com>
2013-06-22 02:34:02 +0200

    Remove getPreventDefault usage

--- src/event.js
+++ src/event.js
@@ -620,8 +620,7 @@ jQuery.Event = function( src, props ) {
 
 		// Events bubbling up the document may have been marked as prevented
 		// by a handler lower down the tree; reflect the correct value.
-		this.isDefaultPrevented = ( src.defaultPrevented ||
-			src.getPreventDefault && src.getPreventDefault() ) ? returnTrue : returnFalse;
+		this.isDefaultPrevented = src.defaultPrevented ? returnTrue : returnFalse;
 
 	// Event type
 	} else {
