1f67d07c60c37e60052db37fc03d42af482c2d03
Timmy Willison <timmywillisn@gmail.com>
2013-07-04 14:00:26 -0400

    Support CommonJS environments by accentuating the need for a window with a document. Fixes #13768.

--- src/intro.js
+++ src/intro.js
@@ -11,7 +11,41 @@
  *
  * Date: @DATE
  */
-(function( window, undefined ) {
+
+(function ( window, factory ) {
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		// Expose a jQuery-making factory as module.exports in loaders that implement the Node
+		// module pattern (including browserify).
+		// This accentuates the need for a real window in the environment
+		// e.g. var jQuery = require("jquery")(window);
+		module.exports = function( w ) {
+			w = w || window;
+			if ( !w.document ) {
+				throw new Error("jQuery requires a window with a document");
+			}
+			return factory( w );
+		};
+	} else {
+		// Execute the factory to produce jQuery
+		var jQuery = factory( window );
+
+		// Register as a named AMD module, since jQuery can be concatenated with other
+		// files that may use define, but not via a proper concatenation script that
+		// understands anonymous AMD modules. A named AMD is safest and most robust
+		// way to register. Lowercase jquery is used because AMD module names are
+		// derived from file names, and jQuery is normally delivered in a lowercase
+		// file name. Do this after creating the global so that if an AMD module wants
+		// to call noConflict to hide this version of jQuery, it will work.
+		if ( typeof define === "function" && define.amd ) {
+			define( "jquery", [], function() {
+				return jQuery;
+			});
+		}
+	}
+
+// Pass this, window may not be defined yet
+}(this, function ( window, undefined ) {
 
 // Can't do this because several apps including ASP.NET trace
 // the stack via arguments.caller.callee and Firefox dies if
