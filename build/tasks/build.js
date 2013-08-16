213eaad880b4f11b65394f4f9b6c44da92954670
Oleg Gaidarenko <markelog@gmail.com>
2013-08-16 09:48:00 -0400

    Move jQuery specific tasks to independent files. Close gh-1334.
    Also:
    	* Confirm build task to the style guide
    	* Use grunt API to load jQuery specific tasks
    	* Add "use strict"; statements
    
    Conflicts:
    	Gruntfile.js
    	build/tasks/build.js

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -5,11 +5,13 @@
 
 module.exports = function( grunt ) {
 
-	var fs = require("fs"),
+	"use strict";
+
+	var fs = require( "fs" ),
 		srcFolder = __dirname + "/../src/",
 		rdefineEnd = /\}\);[^}\w]*$/,
 		// This is temporary until the skipSemiColonInsertion option makes it to NPM
-		requirejs = require("./r"),
+		requirejs = require( "../r" ),
 		config = {
 			baseUrl: "src",
 			name: "jquery",
@@ -114,8 +116,8 @@ module.exports = function( grunt ) {
 			 */
 			excluder = function( flag ) {
 				var m = /^(\+|\-|)([\w\/-]+)$/.exec( flag ),
-					exclude = m[1] === "-",
-					module = m[2];
+					exclude = m[ 1 ] === "-",
+					module = m[ 2 ];
 
 				if ( exclude ) {
 					// Can't exclude certain modules
@@ -216,8 +218,8 @@ module.exports = function( grunt ) {
 	//
 	//   grunt build:*:*:+ajax:-dimensions:-effects:-offset
 	grunt.registerTask( "custom", function() {
-		var args = [].slice.call(arguments),
-			modules = args.length ? args[0].replace(/,/g, ":") : "";
+		var args = [].slice.call( arguments ),
+			modules = args.length ? args[ 0 ].replace( /,/g, ":" ) : "";
 
 		grunt.log.writeln( "Creating custom build...\n" );
 
---
+++ build/tasks/testswarm.js
@@ -0,0 +1,50 @@
+module.exports = function( grunt ) {
+
+	"use strict";
+
+	grunt.registerTask( "testswarm", function( commit, configFile ) {
+		var jobName,
+			testswarm = require( "testswarm" ),
+			runs = {},
+			done = this.async(),
+			pull = /PR-(\d+)/.exec( commit ),
+			config = grunt.file.readJSON( configFile ).jquery,
+			tests = grunt.config([ this.name, "tests" ]);
+
+		if ( pull ) {
+			jobName = "jQuery pull <a href='https://github.com/jquery/jquery/pull/" +
+				pull[ 1 ] + "'>#" + pull[ 1 ] + "</a>";
+		} else {
+			jobName = "jQuery commit #<a href='https://github.com/jquery/jquery/commit/" +
+				commit + "'>" + commit.substr( 0, 10 ) + "</a>";
+		}
+
+		tests.forEach(function( test ) {
+			runs[ test ] = config.testUrl + commit + "/test/index.html?module=" + test;
+		});
+
+		testswarm.createClient( {
+			url: config.swarmUrl,
+			pollInterval: 10000,
+			timeout: 1000 * 60 * 30
+		} )
+		.addReporter( testswarm.reporters.cli )
+		.auth( {
+			id: config.authUsername,
+			token: config.authToken
+		})
+		.addjob(
+			{
+				name: jobName,
+				runs: runs,
+				runMax: config.runMax,
+				browserSets: config.browserSets
+			}, function( err, passed ) {
+				if ( err ) {
+					grunt.log.error( err );
+				}
+				done( passed );
+			}
+		);
+	});
+};
