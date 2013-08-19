215cc09067f08aa67ed467c667efccbbf3bf4468
Michał Gołębiowski <m.goleb@gmail.com>
2013-08-19 19:31:15 +0200

    No ticket. Add a support note about defaultPrevented missing in Android < 4.0

--- src/event.js
+++ src/event.js
@@ -633,7 +633,10 @@ jQuery.Event = function( src, props ) {
 		// Events bubbling up the document may have been marked as prevented
 		// by a handler lower down the tree; reflect the correct value.
 		this.isDefaultPrevented = ( src.defaultPrevented ||
-			src.getPreventDefault && src.getPreventDefault() ) ? returnTrue : returnFalse;
+			// Support: Android < 4.0
+			src.getPreventDefault && src.getPreventDefault() ) ?
+			returnTrue :
+			returnFalse;
 
 	// Event type
 	} else {
