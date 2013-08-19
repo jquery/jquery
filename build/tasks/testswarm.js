a542590687ea2dd32d6209da50031edda5e5ba44
Timmy Willison <timmywillisn@gmail.com>
2013-08-19 15:49:18 -0400

    Fix cherry-pick issue. The master branch tests should not be run in oldIE.

--- build/tasks/testswarm.js
+++ build/tasks/testswarm.js
@@ -38,7 +38,7 @@ module.exports = function( grunt ) {
 				name: jobName,
 				runs: runs,
 				runMax: config.runMax,
-				browserSets: config.browserSets
+				browserSets: "popular-no-old-ie"
 			}, function( err, passed ) {
 				if ( err ) {
 					grunt.log.error( err );
