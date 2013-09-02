187936c0875bbaebf8878156073b8bbedd29732e
Amey Sakhadeo <me@ameyms.com>
2013-09-02 08:25:09 +0530

    Making unit tests terse, Adding unit test for $.grep on sparse arrays

--- test/unit/core.js
+++ test/unit/core.js
@@ -925,30 +925,24 @@ test("jQuery.merge()", function() {
 });
 
 test("jQuery.grep()", function() {
-	expect(7);
+	expect(8);
 
 	var search = function (v) {
-		if(v % 2 === 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-
+		return v % 2 === 0;
 	};
+
 	deepEqual( jQuery.grep([], search), [], "Empty array" );
+	deepEqual( jQuery.grep(new Array(4), search), [], "Sparse array" );
 
 	deepEqual( jQuery.grep([ 1, 2, 3, 4, 5, 6 ], search), [ 2, 4, 6 ], "Satisfying elements present" );
 	deepEqual( jQuery.grep([ 1, 3, 5, 7], search), [], "Satisfying elements absent" );
 
 
-	deepEqual( jQuery.grep([ 1, 2, 3, 4, 5, 6 ], search, true), [ 1, 3, 5 ], "Satisfying elements present but selection inverted" );
-	deepEqual( jQuery.grep([ 1, 3, 5, 7], search, true), [1, 3, 5, 7], "Satisfying elements absent and selection inverted" );
+	deepEqual( jQuery.grep([ 1, 2, 3, 4, 5, 6 ], search, true), [ 1, 3, 5 ], "Satisfying elements present and grep inverted" );
+	deepEqual( jQuery.grep([ 1, 3, 5, 7], search, true), [1, 3, 5, 7], "Satisfying elements absent and grep inverted" );
 
-	deepEqual( jQuery.grep([ 1, 2, 3, 4, 5, 6 ], search, false), [ 2, 4, 6 ], "Satisfying elements present but selection explicitly uninverted" );
-	deepEqual( jQuery.grep([ 1, 3, 5, 7], search, false), [], "Satisfying elements absent and selection explicitly uninverted" );
-
-
+	deepEqual( jQuery.grep([ 1, 2, 3, 4, 5, 6 ], search, false), [ 2, 4, 6 ], "Satisfying elements present but grep explicitly uninverted" );
+	deepEqual( jQuery.grep([ 1, 3, 5, 7], search, false), [], "Satisfying elements absent and grep explicitly uninverted" );
 });
 
 test("jQuery.extend(Object, Object)", function() {
