679536ee4b7a92ae64a5f58d90e9cc38c001e807
Mike Sidorov <mikes.ekb@gmail.com>
2013-09-12 12:32:44 -0500

    charAt -> string indexing. Close gh-1359.

--- src/core/init.js
+++ src/core/init.js
@@ -23,7 +23,7 @@ var rootjQuery,
 
 		// Handle HTML strings
 		if ( typeof selector === "string" ) {
-			if ( selector.charAt(0) === "<" && selector.charAt( selector.length - 1 ) === ">" && selector.length >= 3 ) {
+			if ( selector[0] === "<" && selector[ selector.length - 1 ] === ">" && selector.length >= 3 ) {
 				// Assume that strings that start and end with <> are HTML and skip the regex check
 				match = [ null, selector, null ];
 
