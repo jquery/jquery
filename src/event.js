9b6f0745805941a6d7b7c16c02cf56ab00021c82
Dave Methvin <dave.methvin@gmail.com>
2013-09-17 18:51:54 -0400

    Fix #14180. Allow cross-frame use of focusin/out. Close gh-1369.

--- src/event.js
+++ src/event.js
@@ -727,22 +727,29 @@ jQuery.each({
 if ( !support.focusinBubbles ) {
 	jQuery.each({ focus: "focusin", blur: "focusout" }, function( orig, fix ) {
 
-		// Attach a single capturing handler while someone wants focusin/focusout
-		var attaches = 0,
-			handler = function( event ) {
+		// Attach a single capturing handler on the document while someone wants focusin/focusout
+		var handler = function( event ) {
 				jQuery.event.simulate( fix, event.target, jQuery.event.fix( event ), true );
 			};
 
 		jQuery.event.special[ fix ] = {
 			setup: function() {
-				if ( attaches++ === 0 ) {
-					document.addEventListener( orig, handler, true );
+				var doc = this.ownerDocument,
+					attaches = data_priv.access( doc, "focusCount" );
+
+				if ( !attaches ) {
+					doc.addEventListener( orig, handler, true );
 				}
+				data_priv.access( doc, "focusCount", ( attaches || 0 ) + 1 );
 			},
 			teardown: function() {
-				if ( --attaches === 0 ) {
-					document.removeEventListener( orig, handler, true );
+				var doc = this.ownerDocument,
+					attaches = data_priv.access( doc, "focusCount" ) - 1;
+
+				if ( !attaches ) {
+					doc.removeEventListener( orig, handler, true );
 				}
+				data_priv.access( doc, "focusCount", attaches );
 			}
 		};
 	});
