2622c651e00b609de47cfe63d63d3e4accbd55cf
gucong <gucong@gmail.com>
2013-09-18 12:43:59 +0800

    grunt

--- src/deprecated.js
+++ src/deprecated.js
@@ -13,27 +13,34 @@ jQuery.fn.andSelf = jQuery.fn.addBack;
 var ie = document.documentMode,
 	nav = navigator,
 	ua = nav.userAgent,
-	result = {};
+	result = {},
+	opera;
 
 function getver(name, split) {
 	return new RegExp("\\b" + name + (split || "/") + "([\\w.]+)\\b").test(ua) ? RegExp.$1 : true;
 }
 
-if( ie || !document.querySelector ){
-	result.ie = ie || (document.compatMode == "CSS1Compat" ? "XMLHttpRequest" in window ? 7 : 6 : 5);
-} else if ( window.opera && opera.version ) {
-	//老版本Opera(<=12)，>=15以后采用Chrome内核
-	result.opera = opera.version();
+if ( window.opera && jQuery.isFunction(opera = window.opera.version) ) {
+	//Opera lte 12, Opera 15+ is Chrome core
+	result.opera = opera();
+} else if( ie || !document.querySelector ){
+	//IE core
+	result.ie = ie || (document.compatMode === "CSS1Compat" ? "XMLHttpRequest" in window ? 7 : 6 : 5);
 } else if ( window.netscape ) {
+	//gecko core
 	result.gecko = getver("rv", ":");
 } else {
+	//user appVersion, not userAnget
 	ua = nav.appVersion;
+
+	//webkit core
 	result.webkit = getver("\\w*WebKit");
+
 	if( window.chrome ){
-		//判定为Chrome
+		//Chrome
 		result.chrome = getver("Chrome");
 	} else if ( /^Apple/.test(nav.vendor) ){
-		//判定为Safari
+		//Safari
 		result.safari = getver("Version");
 	}
 }
