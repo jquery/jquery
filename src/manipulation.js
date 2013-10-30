fd7b44321d74e85f41d06cc2cfacbfb6f1bfb829
Chris Antaki <ChrisAntaki@gmail.com>
2013-10-30 13:08:52 -0700

    Each loop was changed to for

--- src/manipulation.js
+++ src/manipulation.js
@@ -312,16 +312,20 @@ jQuery.extend({
 jQuery.fn.extend({
 	text: function( value ) {
 		return access( this, function( value ) {
+			var elem,
+				i = 0;
+
 			if ( value === undefined ) {
 				return jQuery.text( this );
 			}
 
-			return this.each(function() {
-				if ( this.nodeType === 1 || this.nodeType === 11 || this.nodeType === 9 ) {
-					this.textContent = value;
+			for ( ; (elem = this[i]) != null; i++ ) {
+				if ( elem.nodeType === 1 || elem.nodeType === 11 || elem.nodeType === 9 ) {
+					elem.textContent = value;
 				}
-			});
+			}
 
+			return this;
 		}, null, value, arguments.length );
 	},
 
