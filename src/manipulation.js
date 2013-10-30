5663c5974371af176efd36f0e6d94c334bb1fab9
Chris Antaki <ChrisAntaki@gmail.com>
2013-10-30 13:28:20 -0700

    Spacing was fixed

--- src/manipulation.js
+++ src/manipulation.js
@@ -477,7 +477,7 @@ jQuery.fn.extend({
 				return this.empty().append( ( this[0] && this[0].ownerDocument || document ).createTextNode( value ) );
 			}
 
-			for ( ; (elem = this[i]) != null; i++ ) {
+			for ( ; (elem = this[ i ]) != null; i++ ) {
 				if ( elem.nodeType === 1 || elem.nodeType === 11 || elem.nodeType === 9 ) {
 					elem.textContent = value;
 				}
