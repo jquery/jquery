78646db0b4ead9e877fecf7825959a83aa048cbf
Jason Bedard <github@jbedard.ca>
2013-10-31 22:36:38 -0700

    Event: changing fixHooks, propHooks to ES5 getter methods
    - removes the copy loop in jQuery.event.fix
    - avoids accessing properties such as client/offset/page/screen X/Y
      which may cause style recalc or layouts

--- src/event.js
+++ src/event.js
@@ -93,6 +93,36 @@ function on( elem, types, selector, data, fn, one ) {
 	} );
 }
 
+function addEventGetter( unused, name ) {
+	Object.defineProperty( jQuery.Event.prototype, name, {
+		enumerable: true,
+		configurable: true,
+
+		get: function() {
+			var value, hook;
+
+			if ( this.originalEvent ) {
+				if ( hook = jQuery.event.propHooks[ name ] ) {
+					value = hook( this.originalEvent );
+				} else {
+					value = this.originalEvent[ name ];
+				}
+			}
+
+			return value;
+		},
+
+		set: function( value ) {
+			Object.defineProperty( this, name, {
+				enumerable: true,
+				configurable: true,
+				writable: true,
+				value: value
+			} );
+		}
+	} );
+}
+
 /*
  * Helper functions for managing events -- not part of the public interface.
  * Props to Dean Edwards' addEvent library for many of the ideas.
@@ -398,82 +428,84 @@ jQuery.event = {
 		return handlerQueue;
 	},
 
-	// Includes some event props shared by KeyEvent and MouseEvent
-	props: ( "altKey bubbles cancelable ctrlKey currentTarget detail eventPhase " +
-		"metaKey relatedTarget shiftKey target timeStamp view which" ).split( " " ),
+	// Includes all common event props including KeyEvent and MouseEvent specific props
+	props: ( "altKey bubbles cancelable ctrlKey detail eventPhase " +
+		"metaKey shiftKey view which char charCode key keyCode " +
+		"button buttons clientX clientY offsetX offsetY pageX pageY screenX screenY toElement"
+	).split( " " ),
 
 	fixHooks: {},
 
-	keyHooks: {
-		props: "char charCode key keyCode".split( " " ),
-		filter: function( event, original ) {
+	propHooks: {
+		which: function( event ) {
+			var button = event.button;
 
 			// Add which for key events
-			if ( event.which == null ) {
-				event.which = original.charCode != null ? original.charCode : original.keyCode;
+			if ( event.which == null && rkeyEvent.test( event.type ) ) {
+				return event.charCode != null ? event.charCode : event.keyCode;
 			}
 
-			return event;
-		}
-	},
+			// Add which for click: 1 === left; 2 === middle; 3 === right
+			if ( !event.which && button !== undefined && rmouseEvent.test( event.type ) ) {
+				return ( button & 1 ? 1 : ( button & 2 ? 3 : ( button & 4 ? 2 : 0 ) ) );
+			}
 
-	mouseHooks: {
-		props: ( "button buttons clientX clientY offsetX offsetY pageX pageY " +
-			"screenX screenY toElement" ).split( " " ),
-		filter: function( event, original ) {
-			var eventDoc, doc, body,
-				button = original.button;
+			return event.which;
+		},
 
-			// Calculate pageX/Y if missing and clientX/Y available
-			if ( event.pageX == null && original.clientX != null ) {
+		pageX: function( event ) {
+			var eventDoc, doc, body;
+
+			// Calculate pageX if missing and clientX available
+			if ( event.pageX == null && event.clientX != null ) {
 				eventDoc = event.target.ownerDocument || document;
 				doc = eventDoc.documentElement;
 				body = eventDoc.body;
 
-				event.pageX = original.clientX +
+				return event.clientX +
 					( doc && doc.scrollLeft || body && body.scrollLeft || 0 ) -
 					( doc && doc.clientLeft || body && body.clientLeft || 0 );
-				event.pageY = original.clientY +
-					( doc && doc.scrollTop  || body && body.scrollTop  || 0 ) -
-					( doc && doc.clientTop  || body && body.clientTop  || 0 );
 			}
 
-			// Add which for click: 1 === left; 2 === middle; 3 === right
-			// Note: button is not normalized, so don't use it
-			if ( !event.which && button !== undefined ) {
-				event.which = ( button & 1 ? 1 : ( button & 2 ? 3 : ( button & 4 ? 2 : 0 ) ) );
+			return event.pageX;
+		},
+
+		pageY: function( event ) {
+			var eventDoc, doc, body;
+
+			// Calculate pageY if missing and clientY available
+			if ( event.pageY == null && event.clientY != null ) {
+				eventDoc = event.target.ownerDocument || document;
+				doc = eventDoc.documentElement;
+				body = eventDoc.body;
+
+				return event.clientY +
+					( doc && doc.scrollTop || body && body.scrollTop || 0 ) -
+					( doc && doc.clientTop || body && body.clientTop || 0 );
 			}
 
-			return event;
+			return event.pageY;
 		}
 	},
 
-	fix: function( event ) {
-		if ( event[ jQuery.expando ] ) {
-			return event;
+	fix: function( originalEvent ) {
+		if ( originalEvent[ jQuery.expando ] ) {
+			return originalEvent;
 		}
 
 		// Create a writable copy of the event object and normalize some properties
-		var i, prop, copy,
-			type = event.type,
-			originalEvent = event,
-			fixHook = this.fixHooks[ type ];
+		var event,
+			fixHook = this.fixHooks[ originalEvent.type ];
 
-		if ( !fixHook ) {
-			this.fixHooks[ type ] = fixHook =
-				rmouseEvent.test( type ) ? this.mouseHooks :
-				rkeyEvent.test( type ) ? this.keyHooks :
-				{};
+		// Setup any prop hooks added since the last fix
+		if ( this.props.length ) {
+			jQuery.each( this.props.splice( 0 ), addEventGetter );
 		}
-		copy = fixHook.props ? this.props.concat( fixHook.props ) : this.props;
+		if ( fixHook && fixHook.props && fixHook.props.length ) {
+			jQuery.each( fixHook.props.splice( 0 ), addEventGetter );
+		}
 
 		event = new jQuery.Event( originalEvent );
-
-		i = copy.length;
-		while ( i-- ) {
-			prop = copy[ i ];
-			event[ prop ] = originalEvent[ prop ];
-		}
 
 		// Support: Safari <=6 - 7 only
 		// Target should not be a text node (#504, #13143)
@@ -481,7 +513,7 @@ jQuery.event = {
 			event.target = event.target.parentNode;
 		}
 
-		return fixHook.filter ? fixHook.filter( event, originalEvent ) : event;
+		return fixHook && fixHook.filter ? fixHook.filter( event, originalEvent ) : event;
 	},
 
 	special: {
@@ -568,6 +600,11 @@ jQuery.Event = function( src, props ) {
 				src.returnValue === false ?
 			returnTrue :
 			returnFalse;
+
+		// Create target properties
+		this.target = src.target;
+		this.currentTarget = src.currentTarget;
+		this.relatedTarget = src.relatedTarget;
 
 	// Event type
 	} else {
