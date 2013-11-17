91586997e0a4d1159cd2407fa53fb16ecbc540a5
Michał Gołębiowski <m.goleb@gmail.com>
2013-11-17 01:06:44 +0100

    Change window to global in the most outer IIFE parameters. (cherry-picked from dc649a33e081c7beb083b04956731aa410eb6b3f)
    In the most outer IIFE it’s not yet known if the global is window or not.
    Using the window variable to denote the global was misleading in that case,
    especially that the code didn’t make such assumption, requiring to provide
    a Web-like window separately. Renaming window to global clears the confusion.

--- src/intro.js
+++ src/intro.js
@@ -12,7 +12,7 @@
  * Date: @DATE
  */
 
-(function ( window, factory ) {
+(function( global, factory ) {
 
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
 		// For CommonJS and CommonJS-like environments where a proper window is present,
@@ -22,20 +22,20 @@
 		// This accentuates the need for the creation of a real window
 		// e.g. var jQuery = require("jquery")(window);
 		// See ticket #14549 for more info
-		module.exports = window.document ?
-			factory( window ) :
+		module.exports = global.document ?
+			factory( global ) :
 			function( w ) {
 				if ( !w.document ) {
-					throw new Error("jQuery requires a window with a document");
+					throw new Error( "jQuery requires a window with a document" );
 				}
 				return factory( w );
 			};
 	} else {
-		factory( window );
+		factory( global );
 	}
 
 // Pass this, window may not be defined yet
-}(this, function ( window ) {
+}(this, function( window ) {
 
 // Can't do this because several apps including ASP.NET trace
 // the stack via arguments.caller.callee and Firefox dies if
