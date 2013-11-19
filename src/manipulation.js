d800e5c8257bae50afd31214a10ca7c826fedd7c
Adam Wulf <adam.wulf@gmail.com>
2013-11-19 17:10:22 -0600

    when appending a text node to a <table> element, jquery crashes. This commit resolves this issue and allows jquery to continue.

--- src/manipulation.js
+++ src/manipulation.js
@@ -100,7 +100,7 @@ function fixDefaultChecked( elem ) {
 // Manipulating tables requires a tbody
 function manipulationTarget( elem, content ) {
 	return jQuery.nodeName( elem, "table" ) &&
-		jQuery.nodeName( content.nodeType !== 11 ? content : content.firstChild, "tr" ) ?
+		jQuery.nodeName( content.nodeType !== 11 || !content.firstChild ? content : content.firstChild, "tr" ) ?
 
 		elem.getElementsByTagName("tbody")[0] ||
 			elem.appendChild( elem.ownerDocument.createElement("tbody") ) :
