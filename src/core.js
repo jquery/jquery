0784d753cc2972f71ff3b0b66d379043c1a39f6a
MBurchard <MBurchard@users.noreply.github.com>
2013-11-30 20:18:37 +0100

    Add arguments detection
    An arguments array is detected as object. I suggest to add it directly to the detection, that one can check for arguments.

--- src/core.js
+++ src/core.js
@@ -494,7 +494,7 @@ jQuery.extend({
 });
 
 // Populate the class2type map
-jQuery.each("Boolean Number String Function Array Date RegExp Object Error".split(" "), function(i, name) {
+jQuery.each("Boolean Number String Function Array Date RegExp Object Error Arguments".split(" "), function(i, name) {
 	class2type[ "[object " + name + "]" ] = name.toLowerCase();
 });
 
