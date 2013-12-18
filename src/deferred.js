2f1290a8fd618f8ea49b34ed472bf43f1d6ddcbd
Dj Gilcrease <dj.gilcrease@kanbansolutions.com>
2013-12-18 09:47:46 -0500

    Make jQuery.Defered / promise closer to conforming to the specification around exceptions. I have not checked the full compliance tests from https://github.com/promises-aplus/promises-tests but I have setup a test that shows the exception handling working as expected http://jsfiddle.net/yEXL4/1/ This is related to Issue 14510

--- src/deferred.js
+++ src/deferred.js
@@ -29,14 +29,18 @@ jQuery.extend({
 							var fn = jQuery.isFunction( fns[ i ] ) && fns[ i ];
 							// deferred[ done | fail | progress ] for forwarding actions to newDefer
 							deferred[ tuple[1] ](function() {
-								var returned = fn && fn.apply( this, arguments );
-								if ( returned && jQuery.isFunction( returned.promise ) ) {
-									returned.promise()
-										.done( newDefer.resolve )
-										.fail( newDefer.reject )
-										.progress( newDefer.notify );
-								} else {
-									newDefer[ tuple[ 0 ] + "With" ]( this === promise ? newDefer.promise() : this, fn ? [ returned ] : arguments );
+								try {
+									var returned = fn && fn.apply( this, arguments );
+									if ( returned && jQuery.isFunction( returned.promise ) ) {
+										returned.promise()
+											.done( newDefer.resolve )
+											.fail( newDefer.reject )
+											.progress( newDefer.notify );
+									} else {
+										newDefer[ tuple[ 0 ] + "With" ]( this === promise ? newDefer.promise() : this, fn ? [ returned ] : arguments );
+									}
+								} catch(reason) {
+									newDefer.reject(reason);
 								}
 							});
 						});
