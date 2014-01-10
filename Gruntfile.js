3118f58c465286d538404669d09026b882b2149e
Dave Methvin <dave.methvin@gmail.com>
2014-01-09 20:51:21 -0500

    Build: Happy New Year! Thanks @marlonlandaverde

--- Gruntfile.js
+++ Gruntfile.js
@@ -120,7 +120,7 @@ module.exports = function( grunt ) {
 						ascii_only: true
 					},
 					banner: "/*! jQuery v<%= pkg.version %> | " +
-						"(c) 2005, 2013 jQuery Foundation, Inc. | " +
+						"(c) 2005, <%= grunt.template.today('yyyy') %> jQuery Foundation, Inc. | " +
 						"jquery.org/license */",
 					compress: {
 						hoist_funs: false,
