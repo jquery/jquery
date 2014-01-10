68a02e146165024e7b0fef03b7926a94e646ee88
S. Andrew Sheppard <andrew@wq.io>
2014-01-10 10:53:00 -0600

    reference RequireJS wiki

--- src/exports/amd.js
+++ src/exports/amd.js
@@ -12,7 +12,8 @@ define([
 
 // Note that for maximum portability, libraries that are not jQuery should
 // declare themselves as anonymous modules, and avoid setting a global if an
-// AMD loader is present. jQuery is a special case.
+// AMD loader is present. jQuery is a special case. For more information, see
+// https://github.com/jrburke/requirejs/wiki/Updating-existing-libraries#wiki-anon
 
 if ( typeof define === "function" && define.amd ) {
 	define( "jquery", [], function() {
