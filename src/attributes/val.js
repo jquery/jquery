7842cee38ce69532cd8c874594e2db4ff3ac5464
Oleg <markelog@gmail.com>
2014-01-15 02:26:56 +0400

    Attrs: Force reflow in select value setter
    When new option element is added to select box we need to force reflow
    of newly added node in order to workaround delay of initialization props.
    
    try...catch statment is required for bug isolation
    
    Fixes #2252

--- src/attributes/val.js
+++ src/attributes/val.js
@@ -119,21 +119,37 @@ jQuery.extend({
 			set: function( elem, value ) {
 				var optionSet, option,
 					options = elem.options,
-					values = jQuery.makeArray( value ),
 					i = options.length;
 
 				while ( i-- ) {
 					option = options[ i ];
-					if ( (option.selected = jQuery.inArray( jQuery(option).val(), values ) >= 0) ) {
-						optionSet = true;
+
+					if ( jQuery.valHooks.option.get( option ) === value ) {
+
+						// Support: IE6
+						// When new option element is added to select box we need to
+						// force reflow of newly added node in order to workaround delay
+						// of initialization properties
+						try {
+							option.selected = optionSet = true;
+
+						} catch ( _ ) {
+
+							// Will be executed only in IE6
+							option.scrollHeight;
+						}
+
+					} else {
+						option.selected = false;
 					}
 				}
 
-				// force browsers to behave consistently when non-matching value is set
+				// Force browsers to behave consistently when non-matching value is set
 				if ( !optionSet ) {
 					elem.selectedIndex = -1;
 				}
-				return values;
+
+				return options;
 			}
 		}
 	}
