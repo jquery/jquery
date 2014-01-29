f9182e36b67379b8a03408297bb7c8d5edff1db1
Nate <namoricoo@hotmail.com>
2014-01-29 11:07:50 -0800

    modify spacing in Jquery -> var -> selector-sizzle.js, to comply with JsLint rules

--- src/selector-sizzle.js
+++ src/selector-sizzle.js
@@ -1,14 +1,13 @@
 define([
-	"./core",
-	"sizzle"
-], function( jQuery, Sizzle ) {
-
-jQuery.find = Sizzle;
-jQuery.expr = Sizzle.selectors;
-jQuery.expr[":"] = jQuery.expr.pseudos;
-jQuery.unique = Sizzle.uniqueSort;
-jQuery.text = Sizzle.getText;
-jQuery.isXMLDoc = Sizzle.isXML;
-jQuery.contains = Sizzle.contains;
-
+    "./core",
+    "sizzle"
+], function(jQuery, Sizzle) {
+    "use strict";
+    jQuery.find = Sizzle;
+    jQuery.expr = Sizzle.selectors;
+    jQuery.expr[":"] = jQuery.expr.pseudos;
+    jQuery.unique = Sizzle.uniqueSort;
+    jQuery.text = Sizzle.getText;
+    jQuery.isXMLDoc = Sizzle.isXML;
+    jQuery.contains = Sizzle.contains;
 });
