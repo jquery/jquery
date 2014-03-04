ff9a1832acb6040d981f80ee6a671f6e23cd1d6f
Dave Methvin <dave.methvin@gmail.com>
2014-03-03 22:18:50 -0500

    Core: Arrays like [42] should fail .isNumeric()
    Fixes #14179

--- src/core.js
+++ src/core.js
@@ -216,7 +216,7 @@ jQuery.extend({
 		// parseFloat NaNs numeric-cast false positives (null|true|false|"")
 		// ...but misinterprets leading-number strings, particularly hex literals ("0x...")
 		// subtraction forces infinities to NaN
-		return obj - parseFloat( obj ) >= 0;
+		return !jQuery.isArray( obj ) && obj - parseFloat( obj ) >= 0;
 	},
 
 	isPlainObject: function( obj ) {
