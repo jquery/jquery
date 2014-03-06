9ec429cf6270e455aba4eba85f4db80e633806b6
John Hoven <hovenj@gmail.com>
2014-03-06 13:56:09 -0600

    Attributes: Trim whitespace from option text when returned as a value
    Fixes #14858
    Ref #14686
    Closes gh-1531

--- src/attributes/val.js
+++ src/attributes/val.js
@@ -74,7 +74,9 @@ jQuery.extend({
 				var val = jQuery.find.attr( elem, "value" );
 				return val != null ?
 					val :
-					jQuery.text( elem );
+					// Support: IE10-11+
+					// option.text throws exceptions (#14686, #14858)
+					jQuery.trim( jQuery.text( elem ) );
 			}
 		},
 		select: {
