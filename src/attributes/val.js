c37bcc27a4d06e2b7939c46194b35640292191af
John Hoven <hovenj@gmail.com>
2014-03-18 05:38:57 -0500

    Conforming to multi-line comment style guide

--- src/attributes/val.js
+++ src/attributes/val.js
@@ -74,14 +74,10 @@ jQuery.extend({
 				var val = jQuery.find.attr( elem, "value" );
 				if ( val != null ) {
 					return val;
-				} else {
-					
-					/* In IE10/11 (at least before a certain version)
-					accessing text of an empty option will throw an
-					exception: "Invalid argument"
-					Reproduced in 10.0.9200.16798 with
-					document.createElement('option').text
-					*/
+				} else {					
+					// Support: IE10-11+ Accessing text of an empty option will 
+					// throw an exception: "Invalid argument", example: 
+					// document.createElement('option').text
 					try {
 						return elem.text;
 					} catch ( _ ) {
