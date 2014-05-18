e488d985cfb10ab8c684bbc6a9b8ff3eae23bf83
Oleg Gaidarenko <markelog@gmail.com>
2014-05-18 20:47:37 +0400

    CSS: Do not throw on frame elements in FF
    IE9-10 throws on elements created in popups (see #14150), FF meanwhile throws
    on frame elements through "defaultView.getComputedStyle" (see #15098)
    
    Use "defaultView" if in the popup which would fix IE issue,
    use "window.getComputedStyle" which would fix FF issue.
    
    And everybody wins, except performance, but who cares right?
    
    Fixes #15098
    Closes gh-1583

--- src/css/var/getStyles.js
+++ src/css/var/getStyles.js
@@ -1,5 +1,12 @@
 define(function() {
 	return function( elem ) {
-		return elem.ownerDocument.defaultView.getComputedStyle( elem, null );
+		// Support: IE<=11+, Firefox<=30+ (#15098, #14150)
+		// IE throws on elements created in popups
+		// FF meanwhile throws on frame elements through "defaultView.getComputedStyle"
+		if ( elem.ownerDocument.defaultView.opener ) {
+			return elem.ownerDocument.defaultView.getComputedStyle( elem, null );
+		}
+
+		return window.getComputedStyle( elem, null );
 	};
 });
