5550efc3bb58190cc46f1854aae9947be7efd1b0
Timmy Willison <timmywillisn@gmail.com>
2014-05-23 12:45:19 -0400

    Build: run jshint and jscs on release scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,8 +82,7 @@ module.exports = function( grunt ) {
 		jshint: {
 			all: {
 				src: [
-					"src/**/*.js", "Gruntfile.js", "test/**/*.js", "build/tasks/*",
-					"build/{bower-install,release-notes,release}.js"
+					"src/**/*.js", "Gruntfile.js", "test/**/*.js", "build/**/*.js"
 				],
 				options: {
 					jshintrc: true
@@ -100,6 +99,7 @@ module.exports = function( grunt ) {
 
 			// Right know, check only test helpers
 			test: [ "test/data/testrunner.js", "test/data/testinit.js" ],
+			release: "build/*.js",
 			tasks: "build/tasks/*.js"
 		},
 		testswarm: {
