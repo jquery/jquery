ff4b84a0b6243890df31b6a8bc022b7c0d56aa92
Richard Gibson <richard.gibson@gmail.com>
2014-06-17 09:48:17 -0400

    Manipulation: Tolerate XMLNode host object input to getAll
    Fixes #15151
    Ref 25712d77c3bc0221b5b2b9b9492c20a9cfbe1b17
    Closes gh-1602

--- src/manipulation.js
+++ src/manipulation.js
@@ -121,8 +121,12 @@ function cloneCopyEvent( src, dest ) {
 }
 
 function getAll( context, tag ) {
-	var ret = context.getElementsByTagName ? context.getElementsByTagName( tag || "*" ) :
-			context.querySelectorAll ? context.querySelectorAll( tag || "*" ) :
+	// Support: IE9-11+
+	// Use typeof to avoid zero-argument method invocation on host objects (#15151)
+	var ret = typeof context.getElementsByTagName !== "undefined" ?
+			context.getElementsByTagName( tag || "*" ) :
+			typeof context.querySelectorAll !== "undefined" ?
+				context.querySelectorAll( tag || "*" ) :
 			[];
 
 	return tag === undefined || tag && jQuery.nodeName( context, tag ) ?
