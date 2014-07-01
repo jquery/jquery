bcca4f041b4c98dc9cb127ea3a2a7f40d2db4796
Timmy Willison <timmywillisn@gmail.com>
2014-07-01 16:50:27 -0400

    Build: update Sizzle to 2.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,7 @@ module.exports = function( grunt ) {
 				},
 				files: {
 					"sizzle/dist": "sizzle/dist",
-					"sizzle/MIT-LICENSE.txt": "sizzle/MIT-LICENSE.txt",
+					"sizzle/LICENSE.txt": "sizzle/LICENSE.txt",
 
 					"qunit/qunit.js": "qunit/qunit/qunit.js",
 					"qunit/qunit.css": "qunit/qunit/qunit.css",
