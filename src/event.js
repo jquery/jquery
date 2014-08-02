81f40c4561ec021d2e8a6463b37b4ac7dfc6e0fe
mishyTheBear <hamish.dickson@gmail.com>
2014-08-02 10:50:51 +0100

    Fix #15073 Infinite loop on empty namespace
    Get an infinite loop if types parameter of .remove() has any trailing
    dots. Fix is to remove those dots

--- src/event.js
+++ src/event.js
@@ -159,6 +159,9 @@ jQuery.event = {
 			return;
 		}
 
+                // Remove empty namespace (ie trailing dots)
+                types = types.replace(/\.+\s/, " ").replace(/\.+$/, "");
+
 		// Once for each type.namespace in types; type may be omitted
 		types = ( types || "" ).match( rnotwhite ) || [ "" ];
 		t = types.length;
