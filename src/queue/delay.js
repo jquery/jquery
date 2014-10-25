f374b71c4aaa81025236f634a6bc1a9c2fed5f0c
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-10-25 09:42:19 +0200

    Change broken url to wayback one

--- src/queue/delay.js
+++ src/queue/delay.js
@@ -5,7 +5,7 @@ define([
 ], function( jQuery ) {
 
 // Based off of the plugin by Clint Helfers, with permission.
-// http://blindsignals.com/index.php/2009/07/jquery-delay/
+// http://web.archive.org/web/20100324014747/http://blindsignals.com/index.php/2009/07/jquery-delay/
 jQuery.fn.delay = function( time, type ) {
 	time = jQuery.fx ? jQuery.fx.speeds[ time ] || time : time;
 	type = type || "fx";
