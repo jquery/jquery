f110360f65a268e959ae892ca36e85da3d91e606
Michał Gołębiowski <m.goleb@gmail.com>
2014-10-30 01:18:42 +0100

    Deprecated: Drop size and andSelf methods
    Fixes gh-1749
    Closes gh-1822

--- test/unit/deprecated.js
+++ test/unit/deprecated.js
@@ -1,8 +1,2 @@
 module("deprecated", { teardown: moduleTeardown });
 
-if ( jQuery.fn.size ) {
-	test("size()", function() {
-		expect(1);
-		equal( jQuery("#qunit-fixture p").size(), 6, "Get Number of Elements Found" );
-	});
-}
