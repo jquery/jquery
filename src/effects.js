jquery/jquery

commit 877306738f931a711c41d907e69fc8930f985830

Unroll the ( || ) in the math - Fixes #12497 - Thanks @lukemella @curiousdannii - Closes gh-1019

src/effects.js
@@ -81,7 +81,9 @@ function Animation( elem, properties, options ) {
 		tick = function() {
 			var currentTime = fxNow || createFxNow(),
 				remaining = Math.max( 0, animation.startTime + animation.duration - currentTime ),
-				percent = 1 - ( remaining / animation.duration || 0 ),
+				// archaic crash bug won't allow us to use 1 - ( 0.5 || 0 ) (#12497)
+				temp = remaining / animation.duration || 0,
+				percent = 1 - temp,
 				index = 0,
 				length = animation.tweens.length;
 

commit 3c26028fa24a0128cee62222f4ad96384ae51d52

Effects: Remove a workaround for a crash bug on some Android 2.3 phones
This reverts the change introduced in:
https://github.com/jquery/jquery/commit/877306738f931a711c41d907e69fc8930f985830

src/effects.js
@@ -309,10 +309,7 @@ function Animation( elem, properties, options ) {
 			}
 			var currentTime = fxNow || createFxNow(),
 				remaining = Math.max( 0, animation.startTime + animation.duration - currentTime ),
-				// Support: Android 2.3
-				// Archaic crash bug won't allow us to use `1 - ( 0.5 || 0 )` (#12497)
-				temp = remaining / animation.duration || 0,
-				percent = 1 - temp,
+				percent = 1 - (remaining / animation.duration || 0),
 				index = 0,
 				length = animation.tweens.length;
 