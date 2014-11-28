bc1902ddc08502d60ee77e00ea95d454666d7e56
Bin Xin <rhyzix@gmail.com>
2014-11-28 14:09:29 +0800

    Manipulation: support data-URI scripts insertion
    Ref 15f4dec7894f1e00adbfb9bce4f870441a527bd6

--- test/unit/manipulation.js
+++ test/unit/manipulation.js
@@ -2461,3 +2461,12 @@ test( "Make sure jQuery.fn.remove can work on elements in documentFragment", 1, 
 
 	equal( fragment.childNodes.length, 0, "div element was removed from documentFragment" );
 });
+
+asyncTest( "Insert script with data-URI (gh-1887)", 1, function() {
+	Globals.register( "testFoo" );
+	jQuery( "#qunit-fixture" ).append( "<script src=\"data:text/javascript,testFoo = 'foo';\"></script>" );
+	setTimeout(function (){
+		strictEqual( window[ "testFoo" ], "foo", "data-URI script executed" );
+		start();
+	}, 100 );
+});
