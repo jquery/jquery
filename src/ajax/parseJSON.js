a92d305a44b2b0a70f8f1777ce0ad3e52d5889e8
Adrien Delorme <gratator@gmail.com>
2014-12-05 15:10:13 +0100

    CONTRIBUTING: fix(parseJSON): do not fail if data is null

--- src/ajax/parseJSON.js
+++ src/ajax/parseJSON.js
@@ -5,6 +5,9 @@ define([
 // Support: Android 2.3
 // Workaround failure to string-cast null input
 jQuery.parseJSON = function( data ) {
+	if (!data) {
+		return null;
+	}
 	return JSON.parse( data + "" );
 };
 
