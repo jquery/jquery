6b71401941f7e2c3746bb49234fce20c06d23076
Oleg Gaidarenko <markelog@gmail.com>
2014-12-08 10:32:57 +0300

    Manipulation: make wrapAll funarg execute only once
    Fixes gh-1843

--- src/wrap.js
+++ src/wrap.js
@@ -9,13 +9,10 @@ jQuery.fn.extend({
 	wrapAll: function( html ) {
 		var wrap;
 
-		if ( jQuery.isFunction( html ) ) {
-			return this.each(function( i ) {
-				jQuery( this ).wrapAll( html.call(this, i) );
-			});
-		}
-
 		if ( this[ 0 ] ) {
+			if ( jQuery.isFunction( html ) ) {
+				html = html.call( this[ 0 ] );
+			}
 
 			// The elements to wrap the target around
 			wrap = jQuery( html, this[ 0 ].ownerDocument ).eq( 0 ).clone( true );
