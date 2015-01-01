b2caddf15d249030db1dcf33baf90ed71b6cfa9c
Anne-Gaelle Colom <coloma@westminster.ac.uk>
2015-01-01 19:34:48 +0000

    Build: Remove dates from copyright notice

--- Gruntfile.js
+++ Gruntfile.js
@@ -137,7 +137,7 @@ module.exports = function( grunt ) {
 						"ascii_only": true
 					},
 					banner: "/*! jQuery v<%= pkg.version %> | " +
-						"(c) 2005, <%= grunt.template.today('yyyy') %> jQuery Foundation, Inc. | " +
+						"Copyright jQuery Foundation and other contributors " +
 						"jquery.org/license */",
 					compress: {
 						"hoist_funs": false,
