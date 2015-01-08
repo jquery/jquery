e2ec5da2a7f1b3de1f9dc7e2c01f27cd23714e60
Dave Methvin <dave.methvin@gmail.com>
2015-01-08 13:35:28 -0500

    Core: Remove deprecated context and selector properties
    Fixes gh-1908
    Closes gh-2000
    (cherry picked from commit 0ea8c32863af31fb5cfc184e8d513bbae35583e8)

--- src/core.js
+++ src/core.js
@@ -39,9 +39,6 @@ jQuery.fn = jQuery.prototype = {
 
 	constructor: jQuery,
 
-	// Start with an empty selector
-	selector: "",
-
 	// The default length of a jQuery object is 0
 	length: 0,
 
@@ -70,7 +67,6 @@ jQuery.fn = jQuery.prototype = {
 
 		// Add the old object onto the stack (as a reference)
 		ret.prevObject = this;
-		ret.context = this.context;
 
 		// Return the newly-formed element set
 		return ret;
