2524da09c6aa2e3227246ecb1b382bbfe4cead40
Michał Gołębiowski <m.goleb@gmail.com>
2015-01-25 01:24:22 +0100

    Ajax: Run the PATCH test only in IE8 on TestSwarm
    Fixes gh-1994
    Closes gh-2026

--- test/unit/ajax.js
+++ test/unit/ajax.js
@@ -1578,16 +1578,19 @@ module( "ajax", {
 		}
 	} );
 
-	ajaxTest( "#13240 - jQuery.ajax() - support non-RFC2616 methods", 1, {
-		url: "data/echoQuery.php",
-		method: "PATCH",
-		success: function() {
-			ok( true, "success" );
-		},
-		error: function() {
-			ok( false, "error" );
-		}
-	});
+	// BrowserStack PATCH support sometimes breaks so on TestSwarm run the test in IE8 only.
+	if ( location.search.indexOf( "swarmURL=" ) === -1 || document.documentMode < 9 ) {
+		ajaxTest( "#13240 - jQuery.ajax() - support non-RFC2616 methods", 1, {
+			url: "data/echoQuery.php",
+			method: "PATCH",
+			success: function() {
+				ok( true, "success" );
+			},
+			error: function() {
+				ok( false, "error" );
+			}
+		} );
+	}
 
 	testIframeWithCallback( "#14379 - jQuery.ajax() on unload", "ajax/onunload.html", function( status ) {
 		expect( 1 );
