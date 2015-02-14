04fc80121756189e1270b1336b4202f99726fa14
Oleg Gaidarenko <markelog@gmail.com>
2015-02-15 02:27:54 +0300

    Ajax: move explanatory comment to appropriate place
    Ref 4b3e63066dd480d07b9ce8057cb0c02b8ad7e990

--- src/manipulation/_evalUrl.js
+++ src/manipulation/_evalUrl.js
@@ -5,11 +5,11 @@ define([
 jQuery._evalUrl = function( url ) {
 	return jQuery.ajax({
 		url: url,
+
+		// Make this explicit, since user can override this through ajaxSetup (#11264)
 		type: "GET",
 		dataType: "script",
 		cache: true,
-
-		// Make this explicit, since user can override this through ajaxSetup (#11264)
 		async: false,
 		global: false,
 		"throws": true
