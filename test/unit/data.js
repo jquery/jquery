852529c9f148de6df205be01659a79731ce8ebef
Michał Gołębiowski <m.goleb@gmail.com>
2015-03-25 23:10:12 +0100

    Data: Drop the tests relying on applets
    BrowserStack regularly has outdated Java in their IE instances causing our
    data tests to hang & give no output. Skip the tests that create applets,
    applets are treated in the same way as an embed elements which we're already
    testing.
    
    (partially cherry-picked from a4a18e84ec89d862e5dbbea9d04ce05dfdf90554)
    
    Fixes gh-1938
    Refs gh-2028
    Refs gh-2166

--- test/unit/data.js
+++ test/unit/data.js
@@ -142,23 +142,22 @@ test("Data is not being set on comment and text nodes", function() {
 });
 
 test("jQuery.acceptData", function() {
-	expect( 11 );
+	expect( 10 );
 
-	var flash, applet;
+	var flash, pdf;
 
 	ok( jQuery.acceptData( document ), "document" );
 	ok( jQuery.acceptData( document.documentElement ), "documentElement" );
 	ok( jQuery.acceptData( {} ), "object" );
 	ok( !jQuery.acceptData( document.createElement( "embed" ) ), "embed" );
-	ok( !jQuery.acceptData( document.createElement( "applet" ) ), "applet" );
 
 	flash = document.createElement( "object" );
 	flash.setAttribute( "classid", "clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" );
 	ok( jQuery.acceptData( flash ), "flash" );
 
-	applet = document.createElement( "object" );
-	applet.setAttribute( "classid", "clsid:8AD9C840-044E-11D1-B3E9-00805F499D93" );
-	ok( !jQuery.acceptData( applet ), "applet" );
+	pdf = document.createElement( "object" );
+	pdf.setAttribute( "classid", "clsid:CA8A9780-280D-11CF-A24D-444553540000" );
+	ok( !jQuery.acceptData( pdf ), "pdf" );
 
 	ok( !jQuery.acceptData( document.createComment( "" ) ), "comment" );
 	ok( !jQuery.acceptData( document.createTextNode( "" ) ), "text" );
