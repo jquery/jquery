38f881974aa9711e57fa4c694c8ce8f530bdabe0
Richard Gibson <richard.gibson@gmail.com>
2015-03-30 12:22:23 -0400

    CSS: Ignore the CSS cascade in show()/hide()/etc.
    WIP (failing effects tests)
    
    Fixes gh-1767

--- src/css.js
+++ src/css.js
@@ -152,58 +152,39 @@ function getWidthOrHeight( elem, name, extra ) {
 }
 
 function showHide( elements, show ) {
-	var display, elem, hidden,
+	var display, elem,
 		values = [],
 		index = 0,
 		length = elements.length;
 
+	// Determine new display value for elements that need to change
 	for ( ; index < length; index++ ) {
 		elem = elements[ index ];
 		if ( !elem.style ) {
 			continue;
 		}
 
-		values[ index ] = dataPriv.get( elem, "olddisplay" );
 		display = elem.style.display;
 		if ( show ) {
-			// Reset the inline display of this element to learn if it is
-			// being hidden by cascaded rules or not
-			if ( !values[ index ] && display === "none" ) {
-				elem.style.display = "";
-			}
-
-			// Set elements which have been overridden with display: none
-			// in a stylesheet to whatever the default browser style is
-			// for such an element
-			if ( elem.style.display === "" && isHidden( elem ) ) {
-				values[ index ] = dataPriv.access(
-					elem,
-					"olddisplay",
-					defaultDisplay(elem.nodeName)
-				);
+			if ( display === "none" ) {
+				// Restore a pre-hide() value if we have one
+				values[ index ] = dataPriv.get( elem, "olddisplay" ) || "";
 			}
 		} else {
-			hidden = isHidden( elem );
+			if ( display !== "none" ) {
+				values[ index ] = "none";
 
-			if ( display !== "none" || !hidden ) {
-				dataPriv.set(
-					elem,
-					"olddisplay",
-					hidden ? display : jQuery.css( elem, "display" )
-				);
+				// Remember the value we're replacing
+				dataPriv.set( elem, "olddisplay", display );
 			}
 		}
 	}
 
-	// Set the display of most of the elements in a second loop
+	// Set the display of the elements in a second loop
 	// to avoid the constant reflow
 	for ( index = 0; index < length; index++ ) {
-		elem = elements[ index ];
-		if ( !elem.style ) {
-			continue;
-		}
-		if ( !show || elem.style.display === "none" || elem.style.display === "" ) {
-			elem.style.display = show ? values[ index ] || "" : "none";
+		if ( values[ index ] != null ) {
+			elements[ index ].style.display = values[ index ];
 		}
 	}
 
--- test/unit/effects.js
+++ test/unit/effects.js
@@ -2212,5 +2212,27 @@ test( "Animation should go to its end state if document.hidden = true", 1, funct
 	}
 });
 
+asyncTest( "Make sure initialized display value for disconnected nodes is correct (#13310)", 4, function() {
+	var display = jQuery("#display").css("display"),
+		div = jQuery("<div/>");
+
+	equal( div.css( "display", "inline" ).hide().show().appendTo("body").css( "display" ), "inline", "Initialized display value has returned" );
+	div.remove();
+
+	div.css( "display", "none" ).hide();
+	equal( jQuery._data( div[ 0 ], "olddisplay" ), undefined, "olddisplay is undefined after hiding a detached and hidden element" );
+	div.remove();
+
+	div.css( "display", "inline-block" ).hide().appendTo("body").fadeIn(function() {
+		equal( div.css( "display" ), "inline-block", "Initialized display value has returned" );
+		div.remove();
+
+		start();
+	});
+
+	equal( jQuery._data( jQuery("#display").css( "display", "inline" ).hide()[ 0 ], "olddisplay" ), display,
+	"display: * !Important value should used as initialized display" );
+	jQuery._removeData( jQuery("#display")[ 0 ] );
+});
 
 })();
