64f7d10980a5e9f2862f1239a37d95e6c39e37ec
Timmy Willison <timmywillisn@gmail.com>
2015-04-04 15:34:07 -0400

    Core: add workaround for iOS JIT error in isArrayLike
    Fixes gh-2145

--- src/core.js
+++ src/core.js
@@ -478,7 +478,12 @@ jQuery.each("Boolean Number String Function Array Date RegExp Object Error".spli
 });
 
 function isArraylike( obj ) {
-	var length = obj.length,
+
+	// Support: iOS 8.2 (not reproducible in simulator)
+	// `in` check used to prevent JIT error (gh-2145)
+	// hasOwn isn't used here due to false negatives
+	// regarding Nodelist length in IE
+	var length = "length" in obj && obj.length,
 		type = jQuery.type( obj );
 
 	if ( type === "function" || jQuery.isWindow( obj ) ) {
