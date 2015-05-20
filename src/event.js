37c3d087824b51e742126252ced8be592858102d
Oleg Gaidarenko <markelog@gmail.com>
2015-05-20 18:14:13 +0300

    Event: improve originalEvent hack
    Ref 6df669f0fb87cd9975a18bf6bbe3c3548afa4fee
    Ref gh-2336

--- src/event.js
+++ src/event.js
@@ -648,17 +648,22 @@ jQuery.event = {
 		}
 	},
 
+	// Piggyback on a donor event to simulate a different one
 	simulate: function( type, elem, event, bubble ) {
-		// Piggyback on a donor event to simulate a different one.
-		// Fake originalEvent to avoid donor's stopPropagation, but if the
-		// simulated event prevents default then we do the same on the donor.
 		var e = jQuery.extend(
 			new jQuery.Event(),
 			event,
 			{
 				type: type,
-				isSimulated: true,
-				originalEvent: {}
+				isSimulated: true
+				// Previously, `originalEvent: {}` was set here, so stopPropagation call
+				// would not be triggered on donor event, since in our own
+				// jQuery.event.stopPropagation function we had a check for existence of
+				// originalEvent.stopPropagation method, so, consequently it would be a noop.
+				//
+				// Guard for simulated events was moved to jQuery.event.stopPropagation function
+				// since `originalEvent` should point to the original event for the
+				// constancy with other events and for more focused logic
 			}
 		);
 		if ( bubble ) {
@@ -763,7 +768,8 @@ jQuery.Event.prototype = {
 		var e = this.originalEvent;
 
 		this.isPropagationStopped = returnTrue;
-		if ( !e ) {
+
+		if ( !e || this.isSimulated ) {
 			return;
 		}
 		// If stopPropagation exists, run it on the original event
