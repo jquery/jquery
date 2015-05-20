841e7fa7e7970139eeff0ac553ff7e460eb0b303
Oleg Gaidarenko <markelog@gmail.com>
2015-05-20 18:09:46 +0300

    Event: remove outdated originalEvent hack
    Ref gh-2300
    Ref 2506c5db60d3ea0bfe49fee34347227a3fa483ce

--- src/event.js
+++ src/event.js
@@ -602,9 +602,6 @@ jQuery.event = {
 	},
 
 	simulate: function( type, elem, event, bubble ) {
-		// Piggyback on a donor event to simulate a different one.
-		// Fake originalEvent to avoid donor's stopPropagation, but if the
-		// simulated event prevents default then we do the same on the donor.
 		var e = jQuery.extend(
 			new jQuery.Event(),
 			event,
@@ -613,10 +610,6 @@ jQuery.event = {
 				isSimulated: true
 			}
 		);
-
-		// This prevents stopPropagation(), stopImmediatePropagation(), and preventDefault() from
-		// preventing default on the donor event.
-		delete e.originalEvent;
 
 		if ( bubble ) {
 			jQuery.event.trigger( e, null, elem );
