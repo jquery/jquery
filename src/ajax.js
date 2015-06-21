2f4ab34a44e90b35bb63bdadf2878fcfdd2fa106
Jake Drew <Jakemichaeldrew@users.noreply.github.com>
2015-06-21 14:56:28 +0100

    Update ajax.js

--- src/ajax.js
+++ src/ajax.js
@@ -785,7 +785,7 @@ jQuery.extend({
 	},
 	urlParams: function(param) {
 		var query = window.location.search.substring(1),
-		var vars = query.split("&");
+		    vars = query.split("&");
 		for (var i=0;i<vars.length;i++) {
 			var pair = vars[i].split("=");
 			if(pair[0] === params){return pair[1];}
