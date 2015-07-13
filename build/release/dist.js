9162122ba80e507eb02568761252fcea30391104
Timmy Willison <timmywillisn@gmail.com>
2015-07-13 15:01:33 -0400

    Release: properly set the dist remote when it's a real release

--- build/release/dist.js
+++ build/release/dist.js
@@ -4,7 +4,7 @@ module.exports = function( Release, complete ) {
 		fs = require( "fs" ),
 		shell = require( "shelljs" ),
 		pkg = require( Release.dir.repo + "/package.json" ),
-		distRemote = Release.remote.replace( "jquery", "jquery-compat-dist" ),
+		distRemote = Release.remote.replace( "jquery.git", "jquery-compat-dist.git" ),
 		// These files are included with the distrubtion
 		files = [
 			"src",
