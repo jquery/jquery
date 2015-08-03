9adfad19865837f5dffedb1eb41e407f196ca515
Michał Gołębiowski <m.goleb@gmail.com>
2015-08-03 19:31:25 +0200

    Core: Don't expose jQuery.access
    jQuery.access was never documented, there is no need to keep it exposed.
    
    Fixes gh-2513
    Closes gh-2524

--- src/core/access.js
+++ src/core/access.js
@@ -4,7 +4,7 @@ define([
 
 // Multifunctional method to get and set values of a collection
 // The value/s can optionally be executed if it's a function
-var access = jQuery.access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
+var access = function( elems, fn, key, value, chainable, emptyGet, raw ) {
 	var i = 0,
 		len = elems.length,
 		bulk = key == null;
