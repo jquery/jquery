34e3b79fd4a44c1f25f091fcabdab246fc8af005
LongYinan <lynweklm@gmail.com>
2015-09-15 13:10:54 +0800

    Update attr.js
    fix issues that attr handler would affect to DocumentFragment.

--- src/attributes/attr.js
+++ src/attributes/attr.js
@@ -26,8 +26,8 @@ jQuery.extend( {
 		var ret, hooks,
 			nType = elem.nodeType;
 
-		// Don't get/set attributes on text, comment and attribute nodes
-		if ( nType === 3 || nType === 8 || nType === 2 ) {
+		// Don't get/set attributes on text, comment and attribute nodes and documentFragments
+		if ( nType === 3 || nType === 8 || nType === 2 || nType === 11 ) {
 			return;
 		}
 
