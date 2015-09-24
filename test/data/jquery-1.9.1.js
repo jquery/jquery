28bd31d570d610261ec0fbe5c04d4f72d03ce359
Taehee Kim <slhyvaa@nate.com>
2015-09-25 06:15:12 +0900

    Fix Typos

--- test/data/jquery-1.9.1.js
+++ test/data/jquery-1.9.1.js
@@ -3525,7 +3525,7 @@ jQuery.each( {
 				related = event.relatedTarget,
 				handleObj = event.handleObj;
 
-			// For mousenter/leave call the handler if related is outside the target.
+			// For mouseenter/leave call the handler if related is outside the target.
 			// NB: No relatedTarget if the mouse left/entered the browser window
 			if ( !related || ( related !== target && !jQuery.contains( target, related ) ) ) {
 				event.type = handleObj.origType;
