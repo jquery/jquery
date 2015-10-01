7e78c2ec81dbb7bac9222864c4981ed0a54b66e5
Dave Methvin <dave.methvin@gmail.com>
2015-10-01 16:03:10 -0400

    Event: Move .bind() and .delegate() to deprecated
    Cherry-picked from ee0854f85bd686b55757e8854a10480f23c928da
    Fixes gh-2288
    Closes gh-2624

--- src/event/alias.js
+++ src/event/alias.js
@@ -19,24 +19,6 @@ jQuery.each( ( "blur focus focusin focusout resize scroll click dblclick " +
 jQuery.fn.extend( {
 	hover: function( fnOver, fnOut ) {
 		return this.mouseenter( fnOver ).mouseleave( fnOut || fnOver );
-	},
-
-	bind: function( types, data, fn ) {
-		return this.on( types, null, data, fn );
-	},
-	unbind: function( types, fn ) {
-		return this.off( types, null, fn );
-	},
-
-	delegate: function( selector, types, data, fn ) {
-		return this.on( types, selector, data, fn );
-	},
-	undelegate: function( selector, types, fn ) {
-
-		// ( namespace ) or ( selector, types [, fn] )
-		return arguments.length === 1 ?
-			this.off( selector, "**" ) :
-			this.off( types, selector || "**", fn );
 	}
 } );
 
