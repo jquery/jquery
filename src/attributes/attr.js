5c086c3782459307c44397549fef15a87c8b90c4
Timmy Willison <timmywillisn@gmail.com>
2015-10-18 17:21:41 -0400

    Attributes: do not set properties to false when removing booleans
    Fixes gh-1759
    
    (cherry picked from commit 47ccf3daadc4b312f850502300129952e70f9d9d)
    
    Conflicts:
    	src/attributes/attr.js

--- src/attributes/attr.js
+++ src/attributes/attr.js
@@ -92,29 +92,12 @@ jQuery.extend( {
 	},
 
 	removeAttr: function( elem, value ) {
-		var name, propName,
+		var name,
 			i = 0,
 			attrNames = value && value.match( rnotwhite );
 
 		if ( attrNames && elem.nodeType === 1 ) {
 			while ( ( name = attrNames[ i++ ] ) ) {
-				propName = jQuery.propFix[ name ] || name;
-
-				// Boolean attributes get special treatment (#10870)
-				if ( jQuery.expr.match.bool.test( name ) ) {
-
-					// Set corresponding property to false
-					if ( getSetInput || !ruseDefault.test( name ) ) {
-						elem[ propName ] = false;
-
-					// Support: IE<9
-					// Also clear defaultChecked/defaultSelected (if appropriate)
-					} else {
-						elem[ jQuery.camelCase( "default-" + name ) ] =
-							elem[ propName ] = false;
-					}
-				}
-
 				elem.removeAttribute( name );
 			}
 		}
