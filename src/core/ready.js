8c293e62bb39cdf347bd045a1e89a54549f972eb
Timmy Willison <timmywillisn@gmail.com>
2015-10-28 13:54:04 -0400

    Core: use interactive to evaluate dom ready, barring IE6-10
    Fixes gh-2100
    Close gh-2821

--- src/core/ready.js
+++ src/core/ready.js
@@ -97,11 +97,10 @@ jQuery.ready.promise = function( obj ) {
 
 		// Catch cases where $(document).ready() is called
 		// after the browser event has already occurred.
-		// we once tried to use readyState "interactive" here,
-		// but it caused issues like the one
-		// discovered by ChrisS here:
-		// http://bugs.jquery.com/ticket/12282#comment:15
-		if ( document.readyState === "complete" ) {
+		// Support: IE6-10
+		// Older IE sometimes signals "interactive" too soon
+		if ( document.readyState === "complete" ||
+			( document.readyState !== "loading" && !document.documentElement.doScroll ) ) {
 
 			// Handle it asynchronously to allow scripts the opportunity to delay ready
 			window.setTimeout( jQuery.ready );
