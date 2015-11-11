ad358fd62b0ab548abe379594ea00441940461f6
Oleg Gaidarenko <markelog@gmail.com>
2015-11-11 18:55:44 +0300

    Revert "Ajax: Mitigate possible XSS vulnerability"
    This reverts commit b078a62013782c7424a4a61a240c23c4c0b42614.

--- src/ajax/script.js
+++ src/ajax/script.js
@@ -4,13 +4,6 @@ define( [
 	"../ajax"
 ], function( jQuery, document ) {
 
-// Prevent auto-execution of scripts when no explicit dataType was provided (See gh-2432)
-jQuery.ajaxPrefilter( function( s ) {
-	if ( s.crossDomain ) {
-		s.contents.script = false;
-	}
-} );
-
 // Install script dataType
 jQuery.ajaxSetup( {
 	accepts: {
