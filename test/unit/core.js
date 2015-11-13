aaff6a87a1956b05d7793a97425f81855db11393
Oleg Gaidarenko <markelog@gmail.com>
2015-11-13 14:54:33 +0300

    Revert "Offset: allow offset setter to throw for disconnected elements"
    This reverts commit 0d11c1182f2012cd6eb06ce1e3fa5a495af9bee3.

--- test/unit/core.js
+++ test/unit/core.js
@@ -36,6 +36,10 @@ QUnit.test( "jQuery()", function( assert ) {
 		expected++;
 		attrObj[ "width" ] = 10;
 	}
+	if ( jQuery.fn.offset ) {
+		expected++;
+		attrObj["offset"] = { "top": 1, "left": 1 };
+	}
 	if ( jQuery.fn.css ) {
 		expected += 2;
 		attrObj[ "css" ] = { "paddingLeft": 1, "paddingRight": 1 };
@@ -101,10 +105,14 @@ QUnit.test( "jQuery()", function( assert ) {
 	elem = jQuery( "\n\n<em>world</em>" )[ 0 ];
 	assert.equal( elem.nodeName.toLowerCase(), "em", "leading newlines" );
 
-	elem = jQuery( "<div/>", attrObj );
+	elem = jQuery("<div/>", attrObj );
 
 	if ( jQuery.fn.width ) {
 		assert.equal( elem[ 0 ].style.width, "10px", "jQuery() quick setter width" );
+	}
+
+	if ( jQuery.fn.offset ) {
+		equal( elem[0].style.top, "1px", "jQuery() quick setter offset");
 	}
 
 	if ( jQuery.fn.css ) {
