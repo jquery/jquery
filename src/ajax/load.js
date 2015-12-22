fa92796af8f80b7ab86c8d63de6bced0dfe305b6
Oleg Gaidarenko <markelog@gmail.com>
2015-12-22 15:17:35 +0300

    Revert "Ajax: Remove remnants of the load event alias handling"
    This reverts commit 38bd5feddeca3acdd740300f370450621d4e2731.

--- src/ajax/load.js
+++ src/ajax/load.js
@@ -4,14 +4,23 @@ define( [
 	"../ajax",
 	"../traversing",
 	"../manipulation",
-	"../selector"
+	"../selector",
+	// Optional event/alias dependency
+	"../event/alias"
 ], function( jQuery ) {
+
+// Keep a copy of the old load method
+var _load = jQuery.fn.load;
 
 /**
  * Load a url into a page
  */
 jQuery.fn.load = function( url, params, callback ) {
-	var selector, response, type,
+	if ( typeof url !== "string" && _load ) {
+		return _load.apply( this, arguments );
+	}
+
+	var selector, type, response,
 		self = this,
 		off = url.indexOf( " " );
 
