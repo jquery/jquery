86f0c5af9cbe6378d793e0b129bcd28a05e4a3ba
Oleg Gaidarenko <markelog@gmail.com>
2015-12-22 15:26:56 +0300

    Revert "Core: remove custom ready event"
    This reverts commit b35bea14a9b1ff6b9ba96e0c51e21bfeecfc4c4a.

--- test/unit/ready.js
+++ test/unit/ready.js
@@ -1,6 +1,6 @@
 QUnit.module( "ready" );
 
-( function() {
+(function(){
 	var notYetReady, noEarlyExecution,
 		order = [],
 		args = {};
@@ -16,7 +16,6 @@ QUnit.module( "ready" );
 
 	// Create an event handler.
 	function makeHandler( testId ) {
-
 		// When returned function is executed, push testId onto `order` array
 		// to ensure execution order. Also, store event handler arg to ensure
 		// the correct arg is being passed into the event handler.
@@ -27,44 +26,48 @@ QUnit.module( "ready" );
 	}
 
 	// Bind to the ready event in every possible way.
-	jQuery( makeHandler( "a" ) );
-	jQuery( document ).ready( makeHandler( "b" ) );
+	jQuery(makeHandler("a"));
+	jQuery(document).ready(makeHandler("b"));
+	jQuery(document).on("ready.readytest", makeHandler("c"));
 
 	// Do it twice, just to be sure.
-	jQuery( makeHandler( "c" ) );
-	jQuery( document ).ready( makeHandler( "d" ) );
+	jQuery(makeHandler("d"));
+	jQuery(document).ready(makeHandler("e"));
+	jQuery(document).on("ready.readytest", makeHandler("f"));
 
 	noEarlyExecution = order.length === 0;
 
 	// This assumes that QUnit tests are run on DOM ready!
-	QUnit.test( "jQuery ready", function( assert ) {
-		assert.expect( 8 );
+	QUnit.test("jQuery ready", function( assert ) {
+		expect(10);
 
-		assert.ok( noEarlyExecution,
-			"Handlers bound to DOM ready should not execute before DOM ready" );
+		ok(noEarlyExecution, "Handlers bound to DOM ready should not execute before DOM ready");
 
 		// Ensure execution order.
-		assert.deepEqual( order, [ "a", "b", "c", "d" ],
-			"Bound DOM ready handlers should execute in on-order" );
+		deepEqual(order, ["a", "b", "d", "e", "c", "f"], "Bound DOM ready handlers should execute in on-order, but those bound with jQuery(document).on( 'ready', fn ) will always execute last");
 
 		// Ensure handler argument is correct.
-		assert.equal( args.a, jQuery,
-			"Argument passed to fn in jQuery( fn ) should be jQuery" );
-		assert.equal( args.b, jQuery,
-			"Argument passed to fn in jQuery(document).ready( fn ) should be jQuery" );
+		equal(args["a"], jQuery, "Argument passed to fn in jQuery( fn ) should be jQuery");
+		equal(args["b"], jQuery, "Argument passed to fn in jQuery(document).ready( fn ) should be jQuery");
+		ok(args["c"] instanceof jQuery.Event, "Argument passed to fn in jQuery(document).on( 'ready', fn ) should be an event object");
 
 		order = [];
 
 		// Now that the ready event has fired, again bind to the ready event
 		// in every possible way. These event handlers should execute immediately.
-		jQuery( makeHandler( "g" ) );
-		assert.equal( order.pop(), "g", "Event handler should execute immediately" );
-		assert.equal( args.g, jQuery, "Argument passed to fn in jQuery( fn ) should be jQuery" );
+		jQuery(makeHandler("g"));
+		equal(order.pop(), "g", "Event handler should execute immediately");
+		equal(args["g"], jQuery, "Argument passed to fn in jQuery( fn ) should be jQuery");
 
-		jQuery( document ).ready( makeHandler( "h" ) );
-		assert.equal( order.pop(), "h", "Event handler should execute immediately" );
-		assert.equal( args.h, jQuery,
-			"Argument passed to fn in jQuery(document).ready( fn ) should be jQuery" );
-	} );
+		jQuery(document).ready(makeHandler("h"));
+		equal(order.pop(), "h", "Event handler should execute immediately");
+		equal(args["h"], jQuery, "Argument passed to fn in jQuery(document).ready( fn ) should be jQuery");
+
+		jQuery(document).on("ready.readytest", makeHandler("never"));
+		equal(order.length, 0, "Event handler should never execute since DOM ready has already passed");
+
+		// Cleanup.
+		jQuery(document).off("ready.readytest");
+	});
 
 } )();
