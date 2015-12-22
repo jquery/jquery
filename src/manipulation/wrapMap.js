e73541dfe9fc649d9a519ec18297ab2928740c26
Oleg Gaidarenko <markelog@gmail.com>
2015-12-22 20:55:57 +0300

    Revert "Manipulation: simplification of manipulation wrappers"
    This reverts commit 3e8712062d92feea8723e6185c47a2cc98e0e7af.

--- src/manipulation/wrapMap.js
+++ src/manipulation/wrapMap.js
@@ -3,23 +3,19 @@ define( [
 ], function( support ) {
 
 // We have to close these tags to support XHTML (#13200)
-var wrapMap = {
+var wrapMap = wrapMap = {
 	option: [ 1, "<select multiple='multiple'>", "</select>" ],
-
-	// Support: IE8
+	legend: [ 1, "<fieldset>", "</fieldset>" ],
+	area: [ 1, "<map>", "</map>" ],
 	param: [ 1, "<object>", "</object>" ],
-
-	// XHTML parsers do not magically insert elements in the
-	// same way that tag soup parsers do. So we cannot shorten
-	// this by omitting <tbody> or other required elements.
 	thead: [ 1, "<table>", "</table>" ],
-	col: [ 2, "<table><colgroup>", "</colgroup></table>" ],
 	tr: [ 2, "<table><tbody>", "</tbody></table>" ],
+	col: [ 2, "<table><tbody></tbody><colgroup>", "</colgroup></table>" ],
 	td: [ 3, "<table><tbody><tr>", "</tr></tbody></table>" ],
 
 	// IE8 can't serialize link, script, style, or any html5 (NoScope) tags,
 	// unless wrapped in a div with non-breaking characters in front of it.
-	_default: support.htmlSerialize ? [ 0, "", "" ] : [ 1, "X<div>", "</div>" ]
+	_default: support.htmlSerialize ? [ 0, "", "" ] : [ 1, "X<div>", "</div>"  ]
 };
 
 // Support: IE8-IE9
