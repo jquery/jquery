a5be90fb293e329500bb13ccd979161dddfcd075
Oleg Gaidarenko <markelog@gmail.com>
2015-12-22 21:41:14 +0300

    Manipulation: correct wrapMap assign

--- src/manipulation/wrapMap.js
+++ src/manipulation/wrapMap.js
@@ -3,7 +3,7 @@ define( [
 ], function( support ) {
 
 // We have to close these tags to support XHTML (#13200)
-var wrapMap = wrapMap = {
+var wrapMap = {
 	option: [ 1, "<select multiple='multiple'>", "</select>" ],
 	legend: [ 1, "<fieldset>", "</fieldset>" ],
 	area: [ 1, "<map>", "</map>" ],
