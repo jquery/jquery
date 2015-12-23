de90f0a04e47ebc32f310b2c71b954b98d5ed8b6
Dave Methvin <dave.methvin@gmail.com>
2015-12-23 11:53:58 -0500

    [SQUASH]: Use passed-in stack from pre-setTimeout era

--- src/deferred/exceptionHook.js
+++ src/deferred/exceptionHook.js
@@ -7,17 +7,13 @@ define( [
 // warn about them ASAP rather than swallowing them by default.
 var rerrorNames = /^(Eval|Internal|Range|Reference|Syntax|Type|URI)Error$/;
 
-jQuery.Deferred.exceptionHook = function( error ) {
+jQuery.Deferred.exceptionHook = function( error, stack ) {
 
 	// Support: IE9
 	// Console exists when dev tools are open, which can happen at any time
-	var stack,
-		warn = window.console && window.console.warn;
+	var warn = window.console && window.console.warn;
 
 	if ( warn && error && rerrorNames.test( error.name ) ) {
-		if ( jQuery.Deferred.getStackHook ) {
-			stack = jQuery.Deferred.getStackHook();
-		}
 		warn.call( window.console, "jQuery.Deferred exception: " + error.message, stack );
 	}
 };
