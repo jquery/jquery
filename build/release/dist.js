47c21efce522204d0c3e779faa10de2ee63cedd7
Timmy Willison <timmywillisn@gmail.com>
2016-01-08 13:20:19 -0500

    Release: allow local and github dists

--- build/release/dist.js
+++ build/release/dist.js
@@ -4,7 +4,10 @@ module.exports = function( Release, complete ) {
 		fs = require( "fs" ),
 		shell = require( "shelljs" ),
 		pkg = require( Release.dir.repo + "/package.json" ),
-		distRemote = Release.remote.replace( "jquery.git", "jquery-dist.git" ),
+		distRemote = Release.remote
+
+			// For local and github dists
+			.replace( /jquery(\.git|$)/, "jquery-dist$1" ),
 
 		// These files are included with the distribution
 		files = [
