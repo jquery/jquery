35c314827d8b5b4eb996cf590d8254eea48a3896
Todor Prikumov <tono_pr@abv.bg>
2016-01-26 11:58:34 +0200

    CSS: Make sure elem.ownerDocument.defaultView is not null
    Fixes gh-2866
    Close gh-2867

--- src/css/var/getStyles.js
+++ src/css/var/getStyles.js
@@ -6,7 +6,7 @@ define( function() {
 		// FF meanwhile throws on frame elements through "defaultView.getComputedStyle"
 		var view = elem.ownerDocument.defaultView;
 
-		if ( !view.opener ) {
+		if ( !view || !view.opener ) {
 			view = window;
 		}
 
