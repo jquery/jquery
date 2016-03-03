0402963845be8d71c4e8ddf65e7c055014739b60
Timmy Willison <timmywillisn@gmail.com>
2016-03-03 15:33:57 -0500

    Selector: filters -> pseudos
    Fixes gh-2073
    Close gh-2969

--- src/css/hiddenVisibleSelectors.js
+++ src/css/hiddenVisibleSelectors.js
@@ -3,10 +3,10 @@ define( [
 	"../selector"
 ], function( jQuery ) {
 
-jQuery.expr.filters.hidden = function( elem ) {
-	return !jQuery.expr.filters.visible( elem );
+jQuery.expr.pseudos.hidden = function( elem ) {
+	return !jQuery.expr.pseudos.visible( elem );
 };
-jQuery.expr.filters.visible = function( elem ) {
+jQuery.expr.pseudos.visible = function( elem ) {
 	return !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length );
 };
 
