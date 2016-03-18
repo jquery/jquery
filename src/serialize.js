0a2f5bfec18993643a73488b105b62267b446b38
Joe Trumbull <trumbull.j@gmail.com>
2016-03-18 08:51:04 -0400

    Converted ternary statement to standard if

--- src/serialize.js
+++ src/serialize.js
@@ -59,7 +59,9 @@ jQuery.param = function( a, traditional ) {
 
 			// If value is a function, invoke it and return its value
 			value = jQuery.isFunction( value ) ? value() : value;
-			value = value == null ? "" : value;
+			if (value == null) {
+				value = "";
+			}
 			s[ s.length ] = encodeURIComponent( key ) + "=" + encodeURIComponent( value );
 		};
 
