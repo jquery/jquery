84b91c5573abbe8d84562b58c1d03d8e8edc460a
Richard Gibson <richard.gibson@gmail.com>
2016-04-04 13:13:45 -0400

    fixup! Improve comments

--- src/css/var/isHiddenWithinTree.js
+++ src/css/var/isHiddenWithinTree.js
@@ -5,17 +5,27 @@ define( [
 	// css is assumed
 ], function( jQuery ) {
 
-	// This function differs from the :hidden selector
-	// in that it intentionally ignores hidden ancestors (gh-2404)
+	// isHiddenWithinTree reports if an element has a non-"none" display style (inline and/or
+	// through the CSS cascade), which is useful in deciding whether or not to make it visible.
+	// It differs from the :hidden selector (jQuery.expr.pseudos.hidden) in two important ways:
+	// * A hidden ancestor does not force an element to be classified as hidden.
+	// * Being disconnected from the document does not force an element to be classified as hidden.
+	// These differences improve the behavior of .toggle() et al. when applied to elements that are
+	// detached or contained within hidden ancestors (gh-2404, gh-2863).
 	return function( elem, el ) {
 
 		// isHiddenWithinTree might be called from jQuery#filter function;
 		// in that case, element will be second argument
 		elem = el || elem;
-		return elem.style.display === "none" ||
 
-			// Support: Firefox 43
-			// Disconnected elements can have computed display: none
+		// Inline style trumps all
+		return elem.style.display === "none" ||
+			elem.style.display === "" &&
+
+			// Otherwise, check computed style
+			// Support: Firefox <=43 - 45
+			// Disconnected elements can have computed display: none, so first confirm that elem is
+			// in the document.
 			jQuery.contains( elem.ownerDocument, elem ) &&
 
 			jQuery.css( elem, "display" ) === "none";
