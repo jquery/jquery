18473805b18b3c47722bcc8b31234dcc654947e4
Hozaifa Abdalla <Abdallahozaifa19527@gmail.com>
2016-04-06 22:34:07 -0400

    Update wrap.js

--- src/wrap.js
+++ src/wrap.js
@@ -10,17 +10,13 @@ jQuery.fn.extend( {
 		var wrap;
 
 		if ( this[ 0 ] ) {
-			if ( jQuery.isFunction( html ) ) {
-				html = html.call( this[ 0 ] );
-			}
+			if ( jQuery.isFunction( html ) ) html = html.call( this[ 0 ] );
 
 			// The elements to wrap the target around
 			wrap = jQuery( html, this[ 0 ].ownerDocument ).eq( 0 ).clone( true );
 
-			if ( this[ 0 ].parentNode ) {
-				wrap.insertBefore( this[ 0 ] );
-			}
-
+			if ( this[ 0 ].parentNode ) wrap.insertBefore( this[ 0 ] );
+			
 			wrap.map( function() {
 				var elem = this;
 
