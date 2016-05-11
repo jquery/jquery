f3e0ee0d82b3be65866a275e3009b30c4c9711d1
Dave Methvin <dave.methvin@gmail.com>
2016-05-10 21:50:00 -0400

    Deferred: Give better stack diagnostics on exceptions
    Ref gh-2736
    
    The exception stack has the name of the immediately outer function where the
    exception occurred, which can be very handy for tracing errors. Since we already
    have the exception object we might as well use it.

--- src/deferred/exceptionHook.js
+++ src/deferred/exceptionHook.js
@@ -11,10 +11,10 @@ var rerrorNames = /^(Eval|Internal|Range|Reference|Syntax|Type|URI)Error$/;
 
 jQuery.Deferred.exceptionHook = function( error, stack ) {
 
-	// Support: IE 8 - 9 only
+	// Support: IE 9 only
 	// Console exists when dev tools are open, which can happen at any time
 	if ( window.console && window.console.warn && error && rerrorNames.test( error.name ) ) {
-		window.console.warn( "jQuery.Deferred exception: " + error.message, stack );
+		window.console.warn( "jQuery.Deferred exception: " + error.message, error.stack, stack );
 	}
 };
 
