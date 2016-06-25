715366f0984ce5d48aadbb304ab131bab6de2159
Nick Louloudakis <nlouloudak@yahoo.gr>
2016-06-26 02:25:20 +0300

    Deferred: Fix exceptions silent swallowing (Update)
    In order for errors of "Error" type to be printed, the
    regex in the deferred.exceptionHook() needed a slight modification,
    adding the '?' character for the Error prefixes.
    
    Additionally the deferred.exceptionHook() function will print
    the process stack trace only if not undefined, something that
    could be issued as another bug, but it is now fixed.
    
    The fix works on the ready implementation,
    not the ready-not-deferred one.
    
    Fixes #3174

--- src/deferred/exceptionHook.js
+++ src/deferred/exceptionHook.js
@@ -7,14 +7,20 @@ define( [
 
 // These usually indicate a programmer mistake during development,
 // warn about them ASAP rather than swallowing them by default.
-var rerrorNames = /^(Eval|Internal|Range|Reference|Syntax|Type|URI)Error$/;
+// Notice that Error is a choice too, as ? regex means (0-1) times.
+var rerrorNames = /^(Eval|Internal|Range|Reference|Syntax|Type|URI)?Error$/;
 
 jQuery.Deferred.exceptionHook = function( error, stack ) {
 
 	// Support: IE 8 - 9 only
 	// Console exists when dev tools are open, which can happen at any time
 	if ( window.console && window.console.warn && error && rerrorNames.test( error.name ) ) {
-		window.console.warn( "jQuery.Deferred exception: " + error.message, error.stack, stack );
+		window.console.warn( "jQuery.Deferred exception: " + error.message, error.stack );
+
+		// If process stack exists, then give it out as a second warning too.
+		if ( stack ) {
+			window.console.warn( stack );
+		}
 	}
 };
 
