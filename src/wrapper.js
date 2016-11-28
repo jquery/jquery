7fbab88110725c6e0b89f6513b9f7cff50320e1e
Timmy Willison <4timmywil@gmail.com>
2016-11-28 11:11:30 -0500

    Build: jQuery Foundation -> JS Foundation
    Close gh-3414

--- src/wrapper.js
+++ src/wrapper.js
@@ -6,7 +6,7 @@
  * Includes Sizzle.js
  * https://sizzlejs.com/
  *
- * Copyright jQuery Foundation and other contributors
+ * Copyright JS Foundation and other contributors
  * Released under the MIT license
  * https://jquery.org/license
  *
