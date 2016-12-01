66e507265e16951029b13a39efbd96e6f55decf1
David Mark <david.mark@cinsoft.biz>
2016-11-30 21:42:03 -0500

    Remove trailing whitespace

--- src/exports/global.js
+++ src/exports/global.js
@@ -7,11 +7,11 @@ define( [
         "use strict";
 
         // Map over jQuery in case of overwrite
-	
+
         var _jQuery = jQuery;
 
 	// Map over the $ in case of overwrite
-        
+
 	var _$ = $;
 
 	jQueryDefined.noConflict = function( deep ) {
@@ -29,7 +29,7 @@ define( [
 	// Expose jQuery and $ identifiers, even in AMD
 	// (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
 	// and CommonJS for browser emulators (#13566)
-        
+
 	if ( !noGlobal ) {
 		jQuery = $ = jQueryDefined;
 	}
