7baf6e05305de05ed1af6db01ba2df3dfaf57f63
David Mark <david.mark@cinsoft.biz>
2016-11-30 21:19:14 -0500

    Remove window object references and fix whitespace
    Always declare global variables.
    
    https://gist.github.com/david-mark/7cf93afccf3696a4f6754b36cd6ab34e

--- src/exports/global.js
+++ src/exports/global.js
@@ -1,34 +1,36 @@
+var $, jQuery;
+
 define( [
 	"../core"
-], function( jQuery, noGlobal ) {
+], function( jQueryDefined, noGlobal ) {
 
-"use strict";
+        "use strict";
 
-var
-
-	// Map over jQuery in case of overwrite
-	_jQuery = window.jQuery,
+        // Map over jQuery in case of overwrite
+	
+        var _jQuery = jQuery;
 
 	// Map over the $ in case of overwrite
-	_$ = window.$;
+        
+	var _$ = $;
 
-jQuery.noConflict = function( deep ) {
-	if ( window.$ === jQuery ) {
-		window.$ = _$;
+	jQueryDefined.noConflict = function( deep ) {
+		if ( $ === jQueryDefined ) {
+			$ = _$;
+		}
+
+		if ( deep && jQuery === jQueryDefined ) {
+			jQuery = _jQuery;
+		}
+
+		return jQueryDefined;
+	};
+
+	// Expose jQuery and $ identifiers, even in AMD
+	// (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
+	// and CommonJS for browser emulators (#13566)
+        
+	if ( !noGlobal ) {
+		jQuery = $ = jQueryDefined;
 	}
-
-	if ( deep && window.jQuery === jQuery ) {
-		window.jQuery = _jQuery;
-	}
-
-	return jQuery;
-};
-
-// Expose jQuery and $ identifiers, even in AMD
-// (#7102#comment:10, https://github.com/jquery/jquery/pull/557)
-// and CommonJS for browser emulators (#13566)
-if ( !noGlobal ) {
-	window.jQuery = window.$ = jQuery;
-}
-
 } );
