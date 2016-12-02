8f2400a3c6f835183dd6ad0de700a1d2e114a98d
David Mark <david.mark@cinsoft.biz>
2016-12-02 16:11:31 -0500

    Update wrapper.js

--- src/wrapper.js
+++ src/wrapper.js
@@ -20,18 +20,16 @@
 
 		// For CommonJS and CommonJS-like environments where a proper `window`
 		// is present, execute the factory and get jQuery.
-		// For environments that do not have a `window` with a `document`
+		// For environments that do not have a global `document`
 		// (such as Node.js), expose a factory as module.exports.
-		// This accentuates the need for the creation of a real `window`.
-		// e.g. var jQuery = require("jquery")(window);
-		// See ticket #14549 for more info.
+		
 		module.exports = ( global.window && global.window.document ) ?
 			factory( global.window, true ) :
-			function( w ) {
-				if ( !w.document ) {
-					throw new Error( "jQuery requires a window with a document" );
+			function( global ) {
+				if ( !global.document ) {
+					throw new Error( "jQuery requires a document" );
 				}
-				return factory( w );
+				return factory( global );
 			};
 	} else {
 		factory( global.window || global );
