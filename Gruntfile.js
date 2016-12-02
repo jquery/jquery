443ae9f1b8c4bc140968ed39d464466366d9cafe
Blaine Schmeisser <blainesch@gmail.com>
2016-12-01 16:14:20 -0800

    Build: Include the source code in the source map.

--- Gruntfile.js
+++ Gruntfile.js
@@ -166,6 +166,7 @@ module.exports = function( grunt ) {
 				},
 				options: {
 					preserveComments: false,
+					sourceMapIncludeSources: true,
 					sourceMap: true,
 					ASCIIOnly: true,
 					sourceMapName:
