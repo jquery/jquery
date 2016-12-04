d0c174060071aa0962c85d26d10856ce3373aa70
David Mark <david.mark@cinsoft.biz>
2016-12-03 19:18:34 -0500

    Back out indentation clean up and go back to tabs

--- src/wrapper.js
+++ src/wrapper.js
@@ -66,14 +66,14 @@
 
 } )( typeof global == "undefined" ? this : global, function( window, noGlobal, globalObject ) {
 
-        // Edge <= 12 - 13+, Firefox <=18 - 45+, IE 10 - 11, Safari 5.1 - 9+, iOS 6 - 9.1
-        // throw exceptions when non-strict code (e.g., ASP.NET 4.5) accesses strict mode
-        // arguments.callee.caller (trac-13335). But as of jQuery 3.0 (2016), strict mode should be common
-        // enough that all such attempts are guarded in a try block.
-        "use strict";
+// Edge <= 12 - 13+, Firefox <=18 - 45+, IE 10 - 11, Safari 5.1 - 9+, iOS 6 - 9.1
+// throw exceptions when non-strict code (e.g., ASP.NET 4.5) accesses strict mode
+// arguments.callee.caller (trac-13335). But as of jQuery 3.0 (2016), strict mode should be common
+// enough that all such attempts are guarded in a try block.
+"use strict";
 
-        // @CODE
-        // build.js inserts compiled jQuery here
+// @CODE
+// build.js inserts compiled jQuery here
 
-        return jQuery;
+return jQuery;
 } );
