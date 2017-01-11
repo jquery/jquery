b442abacbb8464f0165059e8da734e3143d0721f
Alex Padilla <alexonezero@outlook.com>
2017-01-11 15:19:30 -0700

    Event: Trigger checkbox and radio click events identically
    Fixes gh-3423
    Closes gh-3494

--- src/event.js
+++ src/event.js
@@ -3,12 +3,12 @@ define( [
 	"./var/document",
 	"./var/documentElement",
 	"./var/rnothtmlwhite",
+	"./var/rcheckableType",
 	"./var/slice",
 	"./data/var/dataPriv",
-
 	"./core/init",
 	"./selector"
-], function( jQuery, document, documentElement, rnothtmlwhite, slice, dataPriv ) {
+], function( jQuery, document, documentElement, rnothtmlwhite, rcheckableType, slice, dataPriv ) {
 
 "use strict";
 
@@ -474,9 +474,11 @@ jQuery.event = {
 		},
 		click: {
 
-			// For checkbox, fire native event so checked state will be right
+			// For checkable types, fire native event so checked state will be right
 			trigger: function() {
-				if ( this.type === "checkbox" && this.click && jQuery.nodeName( this, "input" ) ) {
+				if ( rcheckableType.test( this.type ) &&
+					this.click && jQuery.nodeName( this, "input" ) ) {
+
 					this.click();
 					return false;
 				}
