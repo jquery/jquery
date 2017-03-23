cd4aaa0bbfb51cff41ebbd2b9bcf2687c1d6e5a6
Erik Lax <erik@datahack.se>
2017-03-23 15:31:43 +0100

    Fix line length

--- src/ajax/xhr.js
+++ src/ajax/xhr.js
@@ -75,7 +75,8 @@ jQuery.ajaxTransport( function( options ) {
 					return function() {
 						if ( callback ) {
 							callback = errorCallback = xhr.onload =
-								xhr.onerror = xhr.onabort = xhr.ontimeout = xhr.onreadystatechange = null;
+								xhr.onerror = xhr.onabort = xhr.ontimeout =
+								xhr.onreadystatechange = null;
 
 							if ( type === "abort" ) {
 								xhr.abort();
