2f380c4130b60589d8cc25987cf792dfc7830e05
Pierre Spring <pierre@nelm.io>
2017-06-20 14:59:38 +0200

    Tests: trigger with previously stopped event

--- test/unit/event.js
+++ test/unit/event.js
@@ -460,6 +460,27 @@ QUnit.test( "triggered events stopPropagation() for natively-bound events", func
 	$button.off( "click", stopPropagationCallback );
 } );
 
+QUnit.test( "trigger() works with events that were previously stopped", function( assert ) {
+	assert.expect( 0 );
+
+	var $button = jQuery( "#button" ),
+		$parent = $button.parent(),
+		neverCallMe = function() {
+			assert.ok( false, "propagation should have been stopped" );
+		};
+
+	$parent[ 0 ].addEventListener( "click", neverCallMe );
+	$button.on( "click", neverCallMe );
+
+	var clickEvent =  jQuery.Event( "click" );
+	clickEvent.stopPropagation();
+	$button.trigger( clickEvent );
+
+	$parent[ 0 ].removeEventListener( "click", neverCallMe );
+	$button.off( "click", neverCallMe );
+} );
+
+
 QUnit.test( "on(), iframes", function( assert ) {
 	assert.expect( 1 );
 
