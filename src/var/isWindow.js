296d20eac9d3e5f6c714ee492c3f5f893ad990c4
Shashanka Nataraj <shashankan.10@gmail.com>
2017-06-25 08:13:55 +0530

    WIP: Fixes #3629 retained obj!=null instead of using obj!==null

--- src/var/isWindow.js
+++ src/var/isWindow.js
@@ -2,7 +2,7 @@ define( function() {
 	"use strict";
 
 	return function isWindow( obj ) {
-		return obj !== null && typeof obj !== "undefined" && obj === obj.window;
+		return obj != null && obj === obj.window;
 	};
 
 } );
