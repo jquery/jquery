0d0ed37d80fe89df5abb56acd5db10669f9cb97b
Jordan Beland <jordan.beland@gmail.com>
2017-08-15 10:52:11 -0700

    Fixing whitespace between parenthesis

--- test/jquery.js
+++ test/jquery.js
@@ -2,7 +2,7 @@
 ( function() {
 	/* global loadTests: false */
 
-	var path = window.location.pathname.substring(0, window.location.pathname.lastIndexOf("test")),
+	var path = window.location.pathname.substring( 0, window.location.pathname.lastIndexOf( "test" ) ),
 		QUnit = window.QUnit || parent.QUnit,
 		require = window.require || parent.require,
 
