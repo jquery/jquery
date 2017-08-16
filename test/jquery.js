b1b949d35ee995ee75c6968715a7f8c8ea601157
Jordan Beland <jordan.beland@gmail.com>
2017-08-15 22:07:49 -0700

    Tests: Update path calculation
    Fixes gh-3756
    Closes gh-3757

--- test/jquery.js
+++ test/jquery.js
@@ -2,7 +2,8 @@
 ( function() {
 	/* global loadTests: false */
 
-	var path = window.location.pathname.split( "test" )[ 0 ],
+	var pathname = window.location.pathname,
+		path = pathname.slice( 0, pathname.lastIndexOf( "test" ) ),
 		QUnit = window.QUnit || parent.QUnit,
 		require = window.require || parent.require,
 
