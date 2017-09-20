781647b1820206e95e5c32be2dffb20e9096b1e4
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2017-09-20 15:32:35 +0200

    Build: Drop cross-spawn, use child_process.spawn shell option

--- build/tasks/lib/spawn_test.js
+++ build/tasks/lib/spawn_test.js
@@ -3,11 +3,13 @@
 // Run Node with provided parameters: the first one being the Grunt
 // done function and latter ones being files to be tested.
 // See the comment in ../node_smoke_tests.js for more information.
-module.exports = function spawnTest( done ) {
-	var testPaths = [].slice.call( arguments, 1 ),
-		spawn = require( "cross-spawn" );
+module.exports = function spawnTest( done, command ) {
+	var spawn = require( "child_process" ).spawn;
 
-	spawn( "node", testPaths, { stdio: "inherit" } )
+	spawn( command, {
+		stdio: "inherit",
+		shell: true
+	} )
 		.on( "close", function( code ) {
 			done( code === 0 );
 		} );
