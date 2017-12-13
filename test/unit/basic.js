757fd0abe72ffe41c0f0725152dcb62347a92966
Jason Bedard <jason+github@jbedard.ca>
2017-12-12 21:51:49 -0800

    Core: deprecate jQuery.isNumeric
    Fixes gh-2960
    Closes gh-3888

--- test/unit/basic.js
+++ test/unit/basic.js
@@ -76,7 +76,7 @@ QUnit.test( "show/hide", function( assert ) {
 }
 
 QUnit.test( "core", function( assert ) {
-	assert.expect( 23 );
+	assert.expect( 21 );
 
 	var elem = jQuery( "<div></div><span></span>" );
 
@@ -89,9 +89,6 @@ QUnit.test( "core", function( assert ) {
 
 	assert.ok( jQuery.isPlainObject( { "a": 2 } ), "jQuery.isPlainObject(object)" );
 	assert.ok( !jQuery.isPlainObject( "foo" ), "jQuery.isPlainObject(String)" );
-
-	assert.ok( jQuery.isNumeric( "-2" ), "jQuery.isNumeric(String representing a number)" );
-	assert.ok( !jQuery.isNumeric( "" ), "jQuery.isNumeric(\"\")" );
 
 	assert.ok( jQuery.isXMLDoc( jQuery.parseXML(
 		"<?xml version='1.0' encoding='UTF-8'?><foo bar='baz'></foo>"
