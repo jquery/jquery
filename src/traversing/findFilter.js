5a8e33cb1c18b08831289334a1bcdfa5d24d5da3
Saptak Sengupta <saptak013@gmail.com>
2018-01-10 11:04:31 +0530

    Filter: Use direct filter in winnow
    For both simple and complex selectors, use direct
    filter in winnow
    
    Closes gh-3272

--- src/traversing/findFilter.js
+++ src/traversing/findFilter.js
@@ -7,8 +7,6 @@ define( [
 
 "use strict";
 
-var risSimple = /^.[^:#\[\.,]*$/;
-
 // Implement the identical functionality for filter and not
 function winnow( elements, qualifier, not ) {
 	if ( jQuery.isFunction( qualifier ) ) {
@@ -31,16 +29,8 @@ function winnow( elements, qualifier, not ) {
 		} );
 	}
 
-	// Simple selector that can be filtered directly, removing non-Elements
-	if ( risSimple.test( qualifier ) ) {
-		return jQuery.filter( qualifier, elements, not );
-	}
-
-	// Complex selector, compare the two sets, removing non-Elements
-	qualifier = jQuery.filter( qualifier, elements );
-	return jQuery.grep( elements, function( elem ) {
-		return ( indexOf.call( qualifier, elem ) > -1 ) !== not && elem.nodeType === 1;
-	} );
+	// Filtered directly for both simple and complex selectors
+	return jQuery.filter( qualifier, elements, not );
 }
 
 jQuery.filter = function( expr, elems, not ) {
