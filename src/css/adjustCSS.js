f5e36bd8f2c2b28231bbed926c6c3dead94db545
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2018-04-30 18:52:39 +0200

    CSS: Skip the px-appending logic for animations of non-element props
    Without this change animating properties from jQuery.cssNumber on non-elements
    throws an error.
    
    Ref gh-4055
    Closes gh-4061

--- src/css/adjustCSS.js
+++ src/css/adjustCSS.js
@@ -19,7 +19,8 @@ function adjustCSS( elem, prop, valueParts, tween ) {
 		unit = valueParts && valueParts[ 3 ] || ( jQuery.cssNumber[ prop ] ? "" : "px" ),
 
 		// Starting value computation is required for potential unit mismatches
-		initialInUnit = ( jQuery.cssNumber[ prop ] || unit !== "px" && +initial ) &&
+		initialInUnit = elem.nodeType &&
+			( jQuery.cssNumber[ prop ] || unit !== "px" && +initial ) &&
 			rcssNum.exec( jQuery.css( elem, prop ) );
 
 	if ( initialInUnit && initialInUnit[ 3 ] !== unit ) {
