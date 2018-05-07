821bf34353a6baf97f7944379a6459afb16badae
Richard Gibson <richard.gibson@gmail.com>
2018-05-07 09:28:18 -0400

    CSS: Correctly detect scrollbox support with non-default zoom
    Fixes gh-4029
    Closes gh-4030

--- test/unit/css.js
+++ test/unit/css.js
@@ -1167,9 +1167,10 @@ testIframe(
 testIframe(
 	"css('width') should work correctly with browser zooming",
 	"css/cssWidthBrowserZoom.html",
-	function( assert, jQuery, window, document, cssWidthBrowserZoom ) {
-		assert.expect( 1 );
-		assert.strictEqual( cssWidthBrowserZoom, "100px", "elem.css('width') works correctly with browser zoom" );
+	function( assert, jQuery, window, document, widthBeforeSet, widthAfterSet ) {
+		assert.expect( 2 );
+		assert.strictEqual( widthBeforeSet, "100px", "elem.css('width') works correctly with browser zoom" );
+		assert.strictEqual( widthAfterSet, "100px", "elem.css('width', val) works correctly with browser zoom" );
 	}
 );
 
