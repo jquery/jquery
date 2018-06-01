71981dab77cfa4c323b05198670fe324542e185a
a.vitkovsky <a.vitkovsky@aisnovations.com>
2018-06-01 16:47:05 +0300

    sometimes a non-string value does not come

--- src/core/camelCase.js
+++ src/core/camelCase.js
@@ -15,7 +15,8 @@ function fcamelCase( all, letter ) {
 // Support: IE <=9 - 11, Edge 12 - 15
 // Microsoft forgot to hump their vendor prefix (#9572)
 function camelCase( string ) {
-	return string.replace( rmsPrefix, "ms-" ).replace( rdashAlpha, fcamelCase );
+	if (typeof(string) == "string")
+		return string.replace( rmsPrefix, "ms-" ).replace( rdashAlpha, fcamelCase );
 }
 
 return camelCase;
