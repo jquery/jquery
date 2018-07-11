762f859fb2f9616d542619d25bc8ea16a5798064
Richard Gibson <richard.gibson@gmail.com>
2018-07-11 18:22:05 -0400

    Manipulation: Improve regex documentation
    Ref https://github.com/jquery/jquery/pull/1988#discussion_r22442173

--- src/core/var/rsingleTag.js
+++ src/core/var/rsingleTag.js
@@ -1,6 +1,7 @@
 define( function() {
 	"use strict";
 
-	// Match a standalone tag
+	// rsingleTag matches a string consisting of a single HTML element with no attributes
+	// and captures the element's name
 	return ( /^<([a-z][^\/\0>:\x20\t\r\n\f]*)[\x20\t\r\n\f]*\/?>(?:<\/\1>|)$/i );
 } );
