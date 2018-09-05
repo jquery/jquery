22d3349d2dc4597b709dde06cfda547a2f4ca006
Richard Gibson <richard.gibson@gmail.com>
2018-09-05 11:41:22 -0400

    Tests: Remove errant space

--- test/data/testinit.js
+++ test/data/testinit.js
@@ -279,7 +279,7 @@ this.iframeCallback = undefined;
 
 // Tests are always loaded async
 // except when running tests in Karma (See Gruntfile)
-if ( !window.__karma__  ) {
+if ( !window.__karma__ ) {
 	QUnit.config.autostart = false;
 }
 
