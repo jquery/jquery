92214b2b3b8efd9fb989595ac433acc9b9de9d52
Saptak Sengupta <saptak013@gmail.com>
2018-11-09 12:06:00 +0530

    Core: Reduce size of distribution

--- src/core/isAttached.js
+++ src/core/isAttached.js
@@ -5,20 +5,18 @@ define( [
 ], function( jQuery, documentElement ) {
 	"use strict";
 
-	// Replace the use of contains function to check attachment (gh-3504)
-
-	var isAttached;
+	var isAttached = function( elem ) {
+			return jQuery.contains( elem.ownerDocument, elem );
+		},
+		composed = { composed: true };
 
 	// Check attachment across shadow DOM boundaries when possible (gh-3504)
 	if ( documentElement.attachShadow ) {
 		isAttached = function( elem ) {
 			return jQuery.contains( elem.ownerDocument, elem ) ||
-				elem.getRootNode( { composed: true } ) === elem.ownerDocument;
-		};
-	} else {
-		isAttached = function( elem ) {
-			return jQuery.contains( elem.ownerDocument, elem );
+				elem.getRootNode( composed ) === elem.ownerDocument;
 		};
 	}
+
 	return isAttached;
 } );
