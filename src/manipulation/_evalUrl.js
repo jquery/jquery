59aae91f4181ceeb64d202b01860007a00afbf94
Richard Gibson <richard.gibson@gmail.com>
2018-11-26 13:11:33 -0500

    Manipulation: Only evaluate HTTP-successful script src
    Fixes gh-4126

--- src/manipulation/_evalUrl.js
+++ src/manipulation/_evalUrl.js
@@ -10,11 +10,16 @@ jQuery._evalUrl = function( url ) {
 
 		// Make this explicit, since user can override this through ajaxSetup (#11264)
 		type: "GET",
-		dataType: "script",
+		dataType: "text",
 		cache: true,
 		async: false,
 		global: false,
-		"throws": true
+		"throws": true,
+
+		// Only evaluate the response if it is successful (gh-4126)
+		success: function( text ) {
+			jQuery.globalEval( text );
+		}
 	} );
 };
 
