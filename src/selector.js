9da580f36bdec41176666f061328b1f6730a45ee
Sarun Rattanasiri <midnight_w@gmx.tw>
2019-03-09 20:11:35 +0700

    Add comment regarding selector module and custom built

--- src/selector.js
+++ src/selector.js
@@ -1,3 +1,8 @@
+// You should not import this module directly in your custom built.
+// This module has a circular dependency on "./core/init"
+// Consider importing "./core/init" instead.
+// It depends on "./traversing/findFilter" which depends on this module.
+
 define( [ "./selector-sizzle" ], function() {
 	"use strict";
 } );
