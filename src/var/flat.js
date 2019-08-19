ad5d01c6b82628250c70ddd0d51d07a5b07c5005
Ahmed.S.ElAfifi <ahmed.s.elafifi@gmail.com>
2019-08-19 14:52:26 +0200

    Modify support browser versions

--- src/var/flat.js
+++ src/var/flat.js
@@ -5,7 +5,7 @@ define( [
 
 "use strict";
 
-// Support: IE, chrome < 69, firefox < 62, opera < 56, safari < 12
+// Support: IE 11+, Edge 18+
 // Provide fallback for browsers without Array#flat.
 return arr.flat ? function( array ) {
 
