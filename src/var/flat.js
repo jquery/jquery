6e4a28f7a705baa2b63136ee9b5192b528da620c
Ahmed.S.ElAfifi <ahmed.s.elafifi@gmail.com>
2019-08-19 13:29:19 +0200

    Add support comment

--- src/var/flat.js
+++ src/var/flat.js
@@ -5,6 +5,8 @@ define( [
 
 "use strict";
 
+// Support: IE, chrome < 69, firefox < 62, opera < 56, safari < 12
+// IE and other browsers earlier versions doesn't have `flat` method
 return arr.flat ? function() {
 
 	// Flat with depth = 1 to give the same result of concat.apply.
