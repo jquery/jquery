94ce632d2b4593730ed808db146dfc7afafd88dc
Ahmed.S.ElAfifi <ahmed.s.elafifi@gmail.com>
2019-08-19 13:14:29 +0200

    Remove indent
    To avoid massive indentation changes when migrating to ES modules.

--- src/var/flat.js
+++ src/var/flat.js
@@ -2,13 +2,15 @@ define( [
 	"./arr",
 	"./concat"
 ], function( arr, concat ) {
-	"use strict";
 
-	return arr.flat ? function() {
+"use strict";
 
-		// Flat with depth = 1 to give the same result of concat.apply.
-		return arr.flat.call( this, 1 );
-	} : function() {
-		return concat.apply( [], this );
-	};
+return arr.flat ? function() {
+
+	// Flat with depth = 1 to give the same result of concat.apply.
+	return arr.flat.call( this, 1 );
+} : function() {
+	return concat.apply( [], this );
+};
+
 } );
