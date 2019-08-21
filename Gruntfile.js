2e5ed839ada2f267e0e322c686053e7267258b4d
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2019-08-22 00:04:05 +0200

    Build: Remove the external directory, read from node_modules directly
    Now that Sizzle is gone & we use npm, we can read from node_modules directly
    and skip the setup that copies some files to the external directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,25 +68,6 @@ module.exports = function( grunt ) {
 				}
 			}
 		},
-		npmcopy: {
-			all: {
-				options: {
-					destPrefix: "external"
-				},
-				files: {
-					"npo/npo.js": "native-promise-only/lib/npo.src.js",
-
-					"qunit/qunit.js": "qunit/qunit/qunit.js",
-					"qunit/qunit.css": "qunit/qunit/qunit.css",
-					"qunit/LICENSE.txt": "qunit/LICENSE.txt",
-
-					"requirejs/require.js": "requirejs/require.js",
-
-					"sinon/sinon.js": "sinon/pkg/sinon.js",
-					"sinon/LICENSE.txt": "sinon/LICENSE"
-				}
-			}
-		},
 		jsonlint: {
 			pkg: {
 				src: [ "package.json" ]
@@ -161,9 +142,9 @@ module.exports = function( grunt ) {
 				],
 				files: [
 					"test/data/jquery-1.9.1.js",
-					"external/sinon/sinon.js",
-					"external/npo/npo.js",
-					"external/requirejs/require.js",
+					"node_modules/sinon/pkg/sinon.js",
+					"node_modules/native-promise-only/lib/npo.src.js",
+					"node_modules/requirejs/require.js",
 					"test/data/testinit.js",
 
 					"test/jquery.js",
@@ -196,7 +177,7 @@ module.exports = function( grunt ) {
 
 					{ pattern: "dist/jquery.*", included: false, served: true },
 					{ pattern: "src/**", included: false, served: true },
-					{ pattern: "external/**", included: false, served: true },
+					{ pattern: "node_modules/**", included: false, served: true },
 					{
 						pattern: "test/**/*.@(js|css|jpg|html|xml|svg)",
 						included: false,
@@ -231,7 +212,6 @@ module.exports = function( grunt ) {
 						// jsdom only runs basic tests
 						"test/unit/basic.js",
 
-						{ pattern: "external/**", included: false, served: true },
 						{
 							pattern: "test/**/*.@(js|css|jpg|html|xml|svg)",
 							included: false,
