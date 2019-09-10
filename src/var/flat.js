dea751ac8a970b6b63c6ebf0f31371111071103c
Ahmed.S.ElAfifi <ahmed.s.elafifi@gmail.com>
2019-09-10 11:28:32 +0200

    Core: remove hardcoded default value for Array#flat

--- src/var/flat.js
+++ src/var/flat.js
@@ -8,8 +8,6 @@ define( [
 // Support: IE 11+, Edge 18+
 // Provide fallback for browsers without Array#flat.
 return arr.flat ? function( array ) {
-
-	// Flat with depth = 1 to give the same result of concat.apply.
 	return arr.flat.call( array );
 } : function( array ) {
 	return concat.apply( [], array );
