039ef2269d884eea68de9eac1d4205db21dd220d
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2019-11-26 22:29:33 +0100

    Build: Fix the Windows build
    This commit gets rid of rollup-plugin-hypothetical in favor of a simpler
    inline Rollup plugin that fits our need and is compatible with Windows.
    
    Fixes gh-4548

---
+++ build/tasks/lib/rollup-plugin-file-overrides.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * A Rollup plugin accepting a file overrides map and changing
+ * module sources to the overridden ones where provided. Files
+ * without overrides are loaded from disk.
+ *
+ * @param {Map<string, string>} fileOverrides
+ */
+module.exports = ( fileOverrides ) => {
+	return {
+		name: "jquery-file-overrides",
+		load( id ) {
+			if ( fileOverrides.has( id ) ) {
+
+				// Replace the module by a fake source.
+				return fileOverrides.get( id );
+			}
+
+			// Handle this module via the file system.
+			return null;
+		}
+	};
+};
