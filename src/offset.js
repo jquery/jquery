3db6971fbc5ac8d28fae98153e3cb1d1574654e0
Conjee Zou <shishiv30@live.cn>
2019-12-18 11:37:29 -0800

    fix $(document).offset() error
    the document didn't have getClientRects, 
    we should check whether the getClientRects is undefined before use it

--- src/offset.js
+++ src/offset.js
@@ -84,7 +84,7 @@ jQuery.fn.extend( {
 		// Support: IE <=11+
 		// Running getBoundingClientRect on a
 		// disconnected node in IE throws an error
-		if ( !elem.getClientRects().length ) {
+		if ( !elem.getClientRects || !elem.getClientRects().length ) {
 			return { top: 0, left: 0 };
 		}
 
