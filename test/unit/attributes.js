005a88d03df5c3b54fe7aee28966f16ba919006a
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-01-08 17:33:30 +0100

    Tests: Remove obsolete jQuery data tests
    The tests relied on `jQuery.cache` so they only ever worked in jQuery 1.x.

--- test/unit/attributes.js
+++ test/unit/attributes.js
@@ -1411,10 +1411,6 @@ var testToggleClass = function( valueObj, assert ) {
 	e.toggleClass( false );
 	e.toggleClass();
 	assert.ok( e.is( ".testD.testE" ), "Assert class present (restored from data)" );
-
-	// Cleanup
-	e.removeClass( "testD" );
-	assert.expectJqData( this, e[ 0 ], "__className__" );
 };
 
 QUnit.test( "toggleClass(String|boolean|undefined[, boolean])", function( assert ) {
