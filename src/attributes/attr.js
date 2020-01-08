4ec4a7e9bf3a2c543d44a04fe2a9da0cdb66add1
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2020-01-08 18:23:17 +0100

    Attributes: Don't set the type attr hook at all outside of IE
    This removes a needless function call in modern browsers.

--- src/attributes/attr.js
+++ src/attributes/attr.js
@@ -64,23 +64,7 @@ jQuery.extend( {
 		return ret == null ? undefined : ret;
 	},
 
-	attrHooks: {
-		type: {
-			set: function( elem, value ) {
-
-				// Support: IE <=11+
-				// An input loses its value after becoming a radio
-				if ( isIE && value === "radio" && nodeName( elem, "input" ) ) {
-					var val = elem.value;
-					elem.setAttribute( "type", value );
-					if ( val ) {
-						elem.value = val;
-					}
-					return value;
-				}
-			}
-		}
-	},
+	attrHooks: {},
 
 	removeAttr: function( elem, value ) {
 		var name,
@@ -97,6 +81,23 @@ jQuery.extend( {
 		}
 	}
 } );
+
+// Support: IE <=11+
+// An input loses its value after becoming a radio
+if ( isIE ) {
+	jQuery.attrHooks.type = {
+		set: function( elem, value ) {
+			if ( value === "radio" && nodeName( elem, "input" ) ) {
+				var val = elem.value;
+				elem.setAttribute( "type", value );
+				if ( val ) {
+					elem.value = val;
+				}
+				return value;
+			}
+		}
+	};
+}
 
 jQuery.each( jQuery.expr.match.bool.source.match( /\w+/g ), function( _i, name ) {
 	jQuery.attrHooks[ name ] = {
