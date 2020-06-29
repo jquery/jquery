10bf69aa1d0ce84040a350874580a9f71c49edbb
Steve Davis <60626689+SteveDavis-TR@users.noreply.github.com>
2020-06-29 14:29:46 -0500

    Resolve JQMigrate Warning when using .fadeTo
    JQMigrate generates a warning when using effects .fadeTo method.  Send 0 value as a string in fadeTo to resolve.
    
    JQMIGRATE: Use of number-typed values is deprecated in jQuery.fn.css
    Cause: In past versions, when a number-typed value was passed to .css() jQuery converted it to a string and added "px" to the end. As the CSS standard has evolved, an increasingly large set of CSS properties now accept values that are unitless numbers, where this behavior is incorrect. It has become impractical to manage these exceptions in the jQuery.cssNumber object. In addition, some CSS properties like line-height can accept both a bare number 2 or a pixel value 2px. jQuery cannot know the correct way to interpret $.css("line-height", 2) and currently treats it as "2px".
    
    Solution: Always pass string values to .css(), and explicitly add units where required. For example, use $.css("line-height", "2") to specify 200% of the current line height or $.css("line-height", "2px") to specify pixels. When the numeric value is in a variable, ensure the value is converted to string, e.g. $.css("line-height", String(height)) and $.css("line-height", height+"px").

--- src/effects.js
+++ src/effects.js
@@ -493,7 +493,7 @@ jQuery.fn.extend( {
 	fadeTo: function( speed, to, easing, callback ) {
 
 		// Show any hidden elements after setting opacity to 0
-		return this.filter( isHiddenWithinTree ).css( "opacity", 0 ).show()
+		return this.filter( isHiddenWithinTree ).css( "opacity", "0" ).show()
 
 			// Animate to the value specified
 			.end().animate( { opacity: to }, speed, easing, callback );
