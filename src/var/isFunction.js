9ec526ebf59a1514c6b74613b53ea349a4a2b968
Natalia Sroka <nathaliiie@gmail.com>
2020-07-22 19:58:50 +0200

    Core: Bring back QtWebKit support for jQuery 3.x
    Fix allows jQuery to recognize that DOM types such as HTMLCollection
    or NodeList are not functions.
    
    Code review changes:
    - use toString from jQuery instead of native
    - improve support comment
    
    Fixes gh-4756

--- src/var/isFunction.js
+++ src/var/isFunction.js
@@ -1,13 +1,17 @@
-define( function() {
+define( [
+	"./toString"
+], function() {
 	"use strict";
 
 	return function isFunction( obj ) {
 
-		// Support: Chrome <=57, Firefox <=52, QtWeb <=3.8.5, WebKit <=534.34,
-		// wkhtmltopdf tool <=0.12.5
+		// Support: Chrome <=57, Firefox <=52
 		// In some browsers, typeof returns "function" for HTML <object> elements
 		// (i.e., `typeof document.createElement( "object" ) === "function"`).
-		// We don't want to classify *any* DOM node or DOM collection as a function.
+		// We don't want to classify *any* DOM node as a function.
+		// Support: QtWeb <=3.8.5, WebKit <=534.34, wkhtmltopdf tool <=0.12.5
+		// Plus for old WebKit, typeof returns "function" for HTML collections
+		// (e.g., `typeof document.getElementsByTagName("div") === "function"`). (gh-4756)
 		return typeof obj === "function" && typeof obj.nodeType !== "number" &&
 			[ "[object HTMLCollection]", "[object NodeList]" ]
 				.indexOf( toString.call( obj ) ) === -1;
