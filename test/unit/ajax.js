ad1e4c7dfabb9fb08b62f634093513633c43338d
Dallas Fraser <dfraser@digitaled.com>
2020-08-20 12:44:02 -0400

    Appears do not need new mock and fix up the test

--- test/unit/ajax.js
+++ test/unit/ajax.js
@@ -807,43 +807,15 @@ QUnit.module( "ajax", {
 	} );
 
 	ajaxTest( "jQuery.ajax() - do execute scripts if JSONP from unsuccessful responses", 1, function( assert ) {
-		var globalEval = jQuery.globalEval;
-
-		var failConverters = {
-			"text script json": function() {
-				assert.ok( true, "Converter for unsuccessful response" );
+		const testMsg = "Unsuccessful JSONP requests should have a JSON body"
+		return {
+			dataType: "jsonp",
+			url: url( "mock.php?action=errorWithScript" ),
+			failure: function(xhr) {
+				const expected = { "status": 404, "msg": "Not Found" };
+				assert.strictEqual( xhr.responseJSON, expected, testMsg );
 			}
 		};
-
-		function request( title, options ) {
-			var testMsg = title + ": expected JSON response body";
-			return jQuery.extend( {
-				beforeSend: function() {
-					jQuery.globalEval = function() {
-						assert.ok( true, "Should eval" );
-					};
-				},
-				complete: function() {
-					jQuery.globalEval = globalEval;
-				},
-				// error is the significant assertion
-				error: function( xhr ) {
-					assert.strictEqual( xhr.responseBody, { id: 1 }, testMsg );
-				},
-				success: function() {
-					assert.ok( false, "Unanticipated success" );
-				}
-			}, options );
-		}
-		return [
-			request(
-				"JSONP reply with dataType",
-				{
-					dataType: "jsonp",
-					url: url( "mock.php?action=errorWithJSONPAndResponseBodyScript" )
-				}
-			)
-		];
 	} );
 
 	ajaxTest( "jQuery.ajax() - do not execute scripts from unsuccessful responses (gh-4250)", 11, function( assert ) {
