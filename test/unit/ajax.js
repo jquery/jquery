1a29c8ba799daa663a57ac95413b232c67d90191
高灰 <www@zeroplace.cn>
2020-09-19 14:14:10 +0800

    Apply suggestions from code review
    Co-authored-by: Michał Gołębiowski-Owczarek <m.goleb@gmail.com>

--- test/unit/ajax.js
+++ test/unit/ajax.js
@@ -3035,8 +3035,10 @@ if ( typeof window.ArrayBuffer === "undefined" || typeof new XMLHttpRequest().re
 		} );
 	} );
 
-	// We need to simulate cross-domain requests with the feature that both 127.0.0.1 and localhost point to the mock http server
-	// So skip the the test if we are not in localhost.
+	// We need to simulate cross-domain requests with the feature that
+	// both 127.0.0.1 and localhost point to the mock http server.
+ 	// Skip the the test if we are not in localhost but make sure we run
+ 	// it in Karma.
 	QUnit[ ( !QUnit.isSwarm || location.hostname === "localhost" ) ? "test" : "skip" ](
 		"jQuery.append with crossorigin attribute", function( assert ) {
 		assert.expect( 1 );
@@ -3046,7 +3048,7 @@ if ( typeof window.ArrayBuffer === "undefined" || typeof new XMLHttpRequest().re
 
 		Globals.register( "corsCallback" );
 		window.corsCallback = function( response ) {
-			assert.ok( response === true, "Callback fired" );
+			assert.ok( response === true, "Origin header sent" );
 			window.clearTimeout( timeout );
 			done();
 		};
@@ -3062,7 +3064,7 @@ if ( typeof window.ArrayBuffer === "undefined" || typeof new XMLHttpRequest().re
 
 		jQuery( document.body ).append( html );
 		timeout = window.setTimeout( function() {
-			assert.ok( false, "Callback should have fired" );
+			assert.ok( false, "Origin header should have been sent );
 			done();
 		}, 2000 );
 	} );
