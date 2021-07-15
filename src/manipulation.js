f1fa169507cb8645e099b84cc4a69d4fd1fe4c6e
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2021-07-15 15:44:38 +0200

    Manipulation: Don't remove HTML comments from scripts
    When evaluating scripts, jQuery strips out the possible wrapping HTML comment
    and a CDATA section. However, all supported browsers are already doing that
    when loading JS via appending a script tag to the DOM which is how we've been
    doing `jQuery.globalEval` since jQuery 3.0.0. jQuery logic was imperfect, e.g.
    it just stripped the `<!--` and `-->` markers, respectively at the beginning or
    the end of the script contents. However, browsers are also stripping everything
    following those markers in the same line, treating them as single-line comments
    delimiters; this is now also mandated by ECMAScript 2015 in Annex B. Instead
    of fixing the jQuery logic, just let the browser do its thing.
    
    We still need to strip CDATA sections for backwards compatibility. This
    shouldn't be needed as in XML documents they're already not visible when
    inspecting element contents and in HTML documents they have no meaning but
    we're preserving that logic for backwards compatibility. This will be removed
    completely in 4.0.
    
    Fixes gh-4904

--- src/manipulation.js
+++ src/manipulation.js
@@ -40,7 +40,8 @@ var
 
 	// checked="checked" or checked
 	rchecked = /checked\s*(?:[^=]|=\s*.checked.)/i,
-	rcleanScript = /^\s*<!(?:\[CDATA\[|--)|(?:\]\]|--)>\s*$/g;
+
+	rcleanScript = /^\s*<!\[CDATA\[|\]\]>\s*$/g;
 
 // Prefer a tbody over its parent table for containing new rows
 function manipulationTarget( elem, content ) {
@@ -195,6 +196,12 @@ function domManip( collection, args, callback, ignored ) {
 								}, doc );
 							}
 						} else {
+
+							// Clean the CDATA sections from script contents. This shouldn't be
+							// needed as in XML documents they're already not visible when
+							// inspecting element contents and in HTML documents they have no
+							// meaning but we're preserving that logic for backwards compatibility.
+							// This will be removed completely in 4.0. See gh-4904.
 							DOMEval( node.textContent.replace( rcleanScript, "" ), node, doc );
 						}
 					}
