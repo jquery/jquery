b6a958e91519eda18ebde889111ca8f6a4e14ba6
fecore1 <fecore@huawei.com>
2021-09-14 10:16:03 +0800

    CSS: add limit to trim whitespace only for custom prop

--- src/css/curCSS.js
+++ src/css/curCSS.js
@@ -1,18 +1,22 @@
 import jQuery from "../core.js";
 import isAttached from "../core/isAttached.js";
 import getStyles from "./var/getStyles.js";
+import rcustomProp from "./var/rcustomProp.js";
 
 function curCSS( elem, name, computed ) {
-	var ret;
+	var ret,
+		isCustomProp = rcustomProp.test( name );
 
 	computed = computed || getStyles( elem );
 
 	// getPropertyValue is needed for `.css('--customProperty')` (gh-3144)
 	if ( computed ) {
-		ret = computed.getPropertyValue( name );
+		ret = computed.getPropertyValue( name ) || computed[ name ];
 
-		// trim whitespace (issue #4926)
-		ret = ( ret && ret.trim() ) || computed[ name ];
+		// trim whitespace for custom property (issue #4926)
+		if ( isCustomProp ) {
+			ret = ret.trim();
+		}
 
 		if ( ret === "" && !isAttached( elem ) ) {
 			ret = jQuery.style( elem, name );
