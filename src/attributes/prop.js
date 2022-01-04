4b0d8900eca0180720636e15a4038e066bc95280
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2022-01-04 16:33:43 +0100

    Docs: Remove links to Web Archive from source
    Neither of the removed links is crucial; one of them refers to a site that has
    since started being malicious; while the Web Archive links remain safe, some
    scanners warn about such links. Removing them is the safest thing to do.
    
    Fixes gh-4981
    Closes gh-4991
    
    (cherry picked from commit e24f2dcf3f6bda1a672502e0233c732065cbbe89)

--- src/attributes/prop.js
+++ src/attributes/prop.js
@@ -62,7 +62,6 @@ jQuery.extend( {
 				// Support: IE <=9 - 11 only
 				// elem.tabIndex doesn't always return the
 				// correct value when it hasn't been explicitly set
-				// https://web.archive.org/web/20141116233347/http://fluidproject.org/blog/2008/01/09/getting-setting-and-removing-tabindex-values-with-javascript/
 				// Use proper attribute retrieval(#12072)
 				var tabindex = jQuery.find.attr( elem, "tabindex" );
 
--- src/queue/delay.js
+++ src/queue/delay.js
@@ -7,7 +7,6 @@ define( [
 "use strict";
 
 // Based off of the plugin by Clint Helfers, with permission.
-// https://web.archive.org/web/20100324014747/http://blindsignals.com/index.php/2009/07/jquery-delay/
 jQuery.fn.delay = function( time, type ) {
 	time = jQuery.fx ? jQuery.fx.speeds[ time ] || time : time;
 	type = type || "fx";
