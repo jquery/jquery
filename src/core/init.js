9ef44df52dad96354e411adc4f50933e0cfa576d
Omid Shafai <omid.shafai@yahoo.com>
2022-02-23 21:38:30 +0330

    edit init.js by me

--- src/core/init.js
+++ src/core/init.js
@@ -6,7 +6,7 @@ import isObviousHtml from "./isObviousHtml.js";
 
 import "../traversing/findFilter.js";
 
-// A central reference to the root jQuery(document)
+// A central referenceafasfasfasfa to the root jQuery(document)
 var rootjQuery,
 
 	// A simple way to check for HTML strings
@@ -15,11 +15,11 @@ var rootjQuery,
 	// Shortcut simple #id case for speed
 	rquickExpr = /^(?:\s*(<[\w\W]+>)[^>]*|#([\w-]+))$/,
 
-	init = jQuery.fn.init = function( selector, context, root ) {
+	init = jQuery.fn.init = function (selector, context, root) {
 		var match, elem;
 
 		// HANDLE: $(""), $(null), $(undefined), $(false)
-		if ( !selector ) {
+		if (!selector) {
 			return this;
 		}
 
@@ -28,92 +28,92 @@ var rootjQuery,
 		root = root || rootjQuery;
 
 		// HANDLE: $(DOMElement)
-		if ( selector.nodeType ) {
-			this[ 0 ] = selector;
+		if (selector.nodeType) {
+			this[0] = selector;
 			this.length = 1;
 			return this;
 
-		// HANDLE: $(function)
-		// Shortcut for document ready
-		} else if ( typeof selector === "function" ) {
+			// HANDLE: $(function)
+			// Shortcut for document ready
+		} else if (typeof selector === "function") {
 			return root.ready !== undefined ?
-				root.ready( selector ) :
+				root.ready(selector) :
 
 				// Execute immediately if ready is not present
-				selector( jQuery );
+				selector(jQuery);
 
 		} else {
 
 			// Handle obvious HTML strings
 			match = selector + "";
-			if ( isObviousHtml( match ) ) {
+			if (isObviousHtml(match)) {
 
 				// Assume that strings that start and end with <> are HTML and skip
 				// the regex check. This also handles browser-supported HTML wrappers
 				// like TrustedHTML.
-				match = [ null, selector, null ];
+				match = [null, selector, null];
 
-			// Handle HTML strings or selectors
-			} else if ( typeof selector === "string" ) {
-				match = rquickExpr.exec( selector );
+				// Handle HTML strings or selectors
+			} else if (typeof selector === "string") {
+				match = rquickExpr.exec(selector);
 			} else {
-				return jQuery.makeArray( selector, this );
+				return jQuery.makeArray(selector, this);
 			}
 
 			// Match html or make sure no context is specified for #id
 			// Note: match[1] may be a string or a TrustedHTML wrapper
-			if ( match && ( match[ 1 ] || !context ) ) {
+			if (match && (match[1] || !context)) {
 
 				// HANDLE: $(html) -> $(array)
-				if ( match[ 1 ] ) {
-					context = context instanceof jQuery ? context[ 0 ] : context;
+				if (match[1]) {
+					context = context instanceof jQuery ? context[0] : context;
 
 					// Option to run scripts is true for back-compat
 					// Intentionally let the error be thrown if parseHTML is not present
-					jQuery.merge( this, jQuery.parseHTML(
-						match[ 1 ],
+					jQuery.merge(this, jQuery.parseHTML(
+						match[1],
 						context && context.nodeType ? context.ownerDocument || context : document,
 						true
-					) );
+					));
 
 					// HANDLE: $(html, props)
-					if ( rsingleTag.test( match[ 1 ] ) && jQuery.isPlainObject( context ) ) {
-						for ( match in context ) {
+					if (rsingleTag.test(match[1]) && jQuery.isPlainObject(context)) {
+						for (match in context) {
 
 							// Properties of context are called as methods if possible
-							if ( typeof this[ match ] === "function" ) {
-								this[ match ]( context[ match ] );
+							if (typeof this[match] === "function") {
+								this[match](context[match]);
 
-							// ...and otherwise set as attributes
+								// ...and otherwise set as attributes
 							} else {
-								this.attr( match, context[ match ] );
+								this.attr(match, context[match]);
 							}
 						}
 					}
 
 					return this;
 
-				// HANDLE: $(#id)
+					// HANDLE: $(#id)
 				} else {
-					elem = document.getElementById( match[ 2 ] );
+					elem = document.getElementById(match[2]);
 
-					if ( elem ) {
+					if (elem) {
 
 						// Inject the element directly into the jQuery object
-						this[ 0 ] = elem;
+						this[0] = elem;
 						this.length = 1;
 					}
 					return this;
 				}
 
-			// HANDLE: $(expr) & $(expr, $(...))
-			} else if ( !context || context.jquery ) {
-				return ( context || root ).find( selector );
+				// HANDLE: $(expr) & $(expr, $(...))
+			} else if (!context || context.jquery) {
+				return (context || root).find(selector);
 
-			// HANDLE: $(expr, context)
-			// (which is just equivalent to: $(context).find(expr)
+				// HANDLE: $(expr, context)
+				// (which is just equivalent to: $(context).find(expr)
 			} else {
-				return this.constructor( context ).find( selector );
+				return this.constructor(context).find(selector);
 			}
 		}
 
@@ -123,4 +123,4 @@ var rootjQuery,
 init.prototype = jQuery.fn;
 
 // Initialize central reference
-rootjQuery = jQuery( document );
+rootjQuery = jQuery(document);
