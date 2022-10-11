8c7da22caeae8c2c3f7e9869d5f47414669f106c
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2022-10-11 11:55:46 +0200

    Selector: Drop support for legacy pseudos, test custom pseudos
    This backports custom pseudos tests from Sizzle; they were missed in original
    test backports. Also, the support for legacy custom pseudos has been dropped.
    
    The `jQuery.expr` test cleanup has been wrapped in `try-finally` for cleaner
    test isolation in case anything goes wrong.
    
    Closes gh-5137

--- src/selector.js
+++ src/selector.js
@@ -804,33 +804,14 @@ Expr = jQuery.expr = {
 			// https://www.w3.org/TR/selectors/#pseudo-classes
 			// Prioritize by case sensitivity in case custom pseudos are added with uppercase letters
 			// Remember that setFilters inherits from pseudos
-			var args,
-				fn = Expr.pseudos[ pseudo ] || Expr.setFilters[ pseudo.toLowerCase() ] ||
-					selectorError( "unsupported pseudo: " + pseudo );
+			var fn = Expr.pseudos[ pseudo ] || Expr.setFilters[ pseudo.toLowerCase() ] ||
+				selectorError( "unsupported pseudo: " + pseudo );
 
 			// The user may use createPseudo to indicate that
 			// arguments are needed to create the filter function
 			// just as jQuery does
 			if ( fn[ expando ] ) {
 				return fn( argument );
-			}
-
-			// But maintain support for old signatures
-			if ( fn.length > 1 ) {
-				args = [ pseudo, pseudo, "", argument ];
-				return Expr.setFilters.hasOwnProperty( pseudo.toLowerCase() ) ?
-					markFunction( function( seed, matches ) {
-						var idx,
-							matched = fn( seed, argument ),
-							i = matched.length;
-						while ( i-- ) {
-							idx = indexOf.call( seed, matched[ i ] );
-							seed[ idx ] = !( matches[ idx ] = matched[ i ] );
-						}
-					} ) :
-					function( elem ) {
-						return fn( elem, 0, args );
-					};
 			}
 
 			return fn;
