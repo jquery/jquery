8bed510cf8c9ffe9f781feb325f2bfebf7f76988
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2022-11-14 17:56:30 +0100

    Tests: Add a direct jQuery.contains test

--- test/unit/selector.js
+++ test/unit/selector.js
@@ -1895,6 +1895,19 @@ QUnit.test( "jQuery.contains in SVG (jQuery trac-10832)", function( assert ) {
 		"parent (negative)" );
 } );
 
+QUnit.test( "jQuery.contains within <template/> doesn't throw (gh-5147)", function( assert ) {
+	assert.expect( 1 );
+
+	var template = jQuery( "<template><div><div class='a'></div></div></template>" ),
+		a = jQuery( template[ 0 ].content ).find( ".a" );
+
+	template.appendTo( "#qunit-fixture" );
+
+	jQuery.contains( a[ 0 ].ownerDocument, a[ 0 ] );
+
+	assert.ok( true, "Didn't throw" );
+} );
+
 QUnit.test( "jQuery.uniqueSort", function( assert ) {
 	assert.expect( 14 );
 
