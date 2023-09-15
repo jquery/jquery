9fb93f92da8e9f70083917cbb54123f5e4ef7be9
Timmy Willison <timmywil@users.noreply.github.com>
2023-09-14 23:06:54 -0400

    Build: consolidate node_smoke_tests tasks into one
    - move all the combinations to the JS file

--- build/tasks/node_smoke_tests.js
+++ build/tasks/node_smoke_tests.js
@@ -4,8 +4,6 @@ const fs = require( "fs" );
 const util = require( "util" );
 const exec = util.promisify( require( "child_process" ).exec );
 const verifyNodeVersion = require( "./lib/verifyNodeVersion" );
-const { argv } = require( "process" );
-const args = argv.slice( 2 );
 
 const allowedModules = [ "commonjs", "module" ];
 
@@ -19,26 +17,34 @@ if ( !verifyNodeVersion() ) {
 // important so that the tests & the main process don't interfere with
 // each other, e.g. so that they don't share the `require` cache.
 
-async function runTests() {
-	const promises = args.map( async( arg ) => {
-		const [ moduleType, module ] = arg.split( ":" );
-		if ( !allowedModules.includes( moduleType ) ) {
-			throw new Error(
-				`Usage: \`node_smoke_tests [${allowedModules.join( "|" )}]:JQUERY\``
-			);
-		}
-		const dir = `./test/node_smoke_tests/${moduleType}`;
-		const files = await fs.promises.readdir( dir, { withFileTypes: true } );
-		const testFiles = files.filter( ( testFilePath ) => testFilePath.isFile() );
-		return Promise.all(
-			testFiles.map( ( testFile ) =>
-				exec( `node "${dir}/${testFile.name}" "${module}"` )
-			)
+async function runTests( sourceType, module ) {
+	if ( !allowedModules.includes( sourceType ) ) {
+		throw new Error(
+			`Usage: \`node_smoke_tests [${allowedModules.join( "|" )}]:JQUERY\``
 		);
-	} );
-	Promise.all( promises ).then( () => {
-		console.log( `Node smoke tests passed for ${args.join( ", " )}.` );
-	} );
+	}
+	const dir = `./test/node_smoke_tests/${sourceType}`;
+	const files = await fs.promises.readdir( dir, { withFileTypes: true } );
+	const testFiles = files.filter( ( testFilePath ) => testFilePath.isFile() );
+	await Promise.all(
+		testFiles.map( ( testFile ) =>
+			exec( `node "${dir}/${testFile.name}" "${module}"` )
+		)
+	);
+	console.log( `Node smoke tests passed for ${sourceType} "${module}".` );
 }
 
-runTests();
+async function runDefaultTests() {
+	await Promise.all( [
+		runTests( "commonjs", "jquery" ),
+		runTests( "commonjs", "jquery/slim" ),
+		runTests( "commonjs", "./dist/jquery.js" ),
+		runTests( "commonjs", "./dist/jquery.slim.js" ),
+		runTests( "module", "jquery" ),
+		runTests( "module", "jquery/slim" ),
+		runTests( "module", "./dist-module/jquery.module.js" ),
+		runTests( "module", "./dist-module/jquery.slim.module.js" )
+	] );
+}
+
+runDefaultTests();
