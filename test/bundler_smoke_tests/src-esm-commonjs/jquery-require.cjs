d83fdadaff13f20f9e5059680779ab9bca3fde66
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2024-03-11 00:16:44 +0100

    Tests: Add bundler dual module tests for slim & factory builds

--- test/bundler_smoke_tests/src-esm-commonjs/jquery-require.cjs
+++ test/bundler_smoke_tests/src-esm-commonjs/jquery-require.cjs
@@ -1,5 +1,12 @@
 "use strict";
 
 const $ = require( "jquery" );
+const $slim = require( "jquery/slim" );
 
-module.exports.$ = $;
+const { jQueryFactory } = require( "jquery/factory" );
+const { jQueryFactory: jQueryFactorySlim } = require( "jquery/factory-slim" );
+
+module.exports.$required = $;
+module.exports.$slimRequired = $slim;
+module.exports.jQueryFactoryRequired = jQueryFactory;
+module.exports.jQueryFactorySlimRequired = jQueryFactorySlim;
