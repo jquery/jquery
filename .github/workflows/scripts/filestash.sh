80bd4151bd84e935855aef099b3d10a4d60582e2
Cedric Willekens <c.s.willekens@student.tudelft.nl>
2024-04-18 21:40:08 +0200

    Fix gha smells
    - Steps should only perform a single command
    - Run CI on multiple language versions
    - Use cache parameter instead of cache option
    - Run tests on multiple OS's

---
+++ .github/workflows/scripts/filestash.sh
@@ -0,0 +1,15 @@
+rsync dist/jquery.js filestash@"$FILESTASH_SERVER":jquery-git.js
+rsync dist/jquery.min.js filestash@"$FILESTASH_SERVER":jquery-git.min.js
+rsync dist/jquery.min.map filestash@"$FILESTASH_SERVER":jquery-git.min.map
+
+rsync dist/jquery.slim.js filestash@"$FILESTASH_SERVER":jquery-git.slim.js
+rsync dist/jquery.slim.min.js filestash@"$FILESTASH_SERVER":jquery-git.slim.min.js
+rsync dist/jquery.slim.min.map filestash@"$FILESTASH_SERVER":jquery-git.slim.min.map
+
+rsync dist-module/jquery.module.js filestash@"$FILESTASH_SERVER":jquery-git.module.js
+rsync dist-module/jquery.module.min.js filestash@"$FILESTASH_SERVER":jquery-git.module.min.js
+rsync dist-module/jquery.module.min.map filestash@"$FILESTASH_SERVER":jquery-git.module.min.map
+
+rsync dist-module/jquery.slim.module.js filestash@"$FILESTASH_SERVER":jquery-git.slim.module.js
+rsync dist-module/jquery.slim.module.min.js filestash@"$FILESTASH_SERVER":jquery-git.slim.module.min.js
+rsync dist-module/jquery.slim.module.min.map filestash@"$FILESTASH_SERVER":jquery-git.slim.module.min.map
