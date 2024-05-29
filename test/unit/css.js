7c935e21f14ead2ead55e6ad4136a8bdce98c69d
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2024-05-29 13:47:25 +0200

    CSS:Tests: Fix tests & support tests under CSS Zoom
    Firefox 126+ implements CSS zoom in a way it affects width computed style
    very slightly (`100.008px` instead of `100px`); accept that difference.
    
    Also, skip the width setter under zoom test in Firefox 126 completely - that
    version has CSS zoom affecting `offsetWidth` values. This has been fixed in
    Firefox 127 so it's not worth working around it.
    
    Add a test for support tests resolving the same under CSS zoom & without one.
    That test uncovered Chrome failing the `reliableTrDimensions` support test
    under zoom; the test has been fixed.
    
    Fixes gh-5489
    Closes gh-5496
    Ref gh-5495

--- test/unit/css.js
+++ test/unit/css.js
@@ -1396,8 +1396,20 @@ testIframe(
 	"css/cssWidthBrowserZoom.html",
 	function( assert, jQuery, window, document, widthBeforeSet, widthAfterSet ) {
 		assert.expect( 2 );
-		assert.strictEqual( widthBeforeSet, "100px", "elem.css('width') works correctly with browser zoom" );
-		assert.strictEqual( widthAfterSet, "100px", "elem.css('width', val) works correctly with browser zoom" );
+
+		// Support: Firefox 126+
+		// Newer Firefox implements CSS zoom in a way it affects
+		// those values slightly.
+		assert.ok( /^100(?:|\.0\d*)px$/.test( widthBeforeSet ), "elem.css('width') works correctly with browser zoom" );
+
+		// Support: Firefox 126 only
+		// In Firefox 126 only, CSS zoom affects `offsetWidth`. Since the issue
+		// is fixed in v127, let's just skip the test in v126.
+		if ( /\bfirefox\/126\.\d\b/i.test( navigator.userAgent ) ) {
+			assert.ok( true, "elem.css('width', val) works incorrectly with browser zoom in Firefox 126 and we accept that" );
+		} else {
+			assert.ok( /^100(?:|\.0\d*)px$/.test( widthAfterSet ), "elem.css('width', val) works correctly with browser zoom" );
+		}
 	}
 );
 
