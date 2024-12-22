12d1430afb7496548f5077a99ded12980a9564cb
Yuva <94527987+yuvashrikarunakaran@users.noreply.github.com>
2024-12-22 09:53:08 +0530

    Update command.js
    Specify defaults directly in yargs for better user experience

--- build/command.js
+++ build/command.js
@@ -1,79 +1,91 @@
-import yargs from "yargs/yargs";
+ import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
 import { build } from "./tasks/build.js";
 import slimExclude from "./tasks/lib/slim-exclude.js";
 
-const argv = yargs( process.argv.slice( 2 ) )
-	.version( false )
-	.command( {
-		command: "[options]",
-		describe: "Build a jQuery bundle"
-	} )
-	.option( "filename", {
-		alias: "f",
-		type: "string",
-		description:
-			"Set the filename of the built file. Defaults to jquery.js."
-	} )
-	.option( "dir", {
-		alias: "d",
-		type: "string",
-		description:
-			"Set the dir to which to output the built file. Defaults to /dist."
-	} )
-	.option( "version", {
-		alias: "v",
-		type: "string",
-		description:
-			"Set the version to include in the built file. " +
-			"Defaults to the version in package.json plus the " +
-			"short commit SHA and any excluded modules."
-	} )
-	.option( "watch", {
-		alias: "w",
-		type: "boolean",
-		description:
-			"Watch the source files and rebuild when they change."
-	} )
-	.option( "exclude", {
-		alias: "e",
-		type: "array",
-		description:
-			"Modules to exclude from the build. " +
-			"Specifying this option will cause the " +
-			"specified modules to be excluded from the build."
-	} )
-	.option( "include", {
-		alias: "i",
-		type: "array",
-		description:
-			"Modules to include in the build. " +
-			"Specifying this option will override the " +
-			"default included modules and only include these modules."
-	} )
-	.option( "esm", {
-		type: "boolean",
-		description:
-			"Build an ES module (ESM) bundle. " +
-			"By default, a UMD bundle is built."
-	} )
-	.option( "factory", {
-		type: "boolean",
-		description:
-			"Build the factory bundle. " +
-			"By default, a UMD bundle is built."
-	} )
-	.option( "slim", {
-		alias: "s",
-		type: "boolean",
-		description: "Build a slim bundle, which excludes " +
-			slimExclude.join( ", " )
-	} )
-	.option( "amd", {
-		type: "string",
-		description:
-			"Set the name of the AMD module. Leave blank to make an anonymous module."
-	} )
-	.help()
-	.argv;
+// Define CLI options
+const options = [
+    {
+        name: "filename",
+        alias: "f",
+        type: "string",
+        description: "Set the filename of the built file.",
+        default: "jquery.js",
+    },
+    {
+        name: "dir",
+        alias: "d",
+        type: "string",
+        description: "Set the output directory for the built file.",
+        default: "./dist",
+    },
+    {
+        name: "version",
+        alias: "v",
+        type: "string",
+        description: "Set the version to include in the built file.",
+    },
+    {
+        name: "watch",
+        alias: "w",
+        type: "boolean",
+        description: "Watch source files and rebuild on changes.",
+        default: false,
+    },
+    {
+        name: "exclude",
+        alias: "e",
+        type: "array",
+        description: "Modules to exclude from the build.",
+    },
+    {
+        name: "include",
+        alias: "i",
+        type: "array",
+        description: "Modules to include in the build.",
+    },
+    {
+        name: "esm",
+        type: "boolean",
+        description: "Build an ES module (ESM) bundle.",
+        default: false,
+    },
+    {
+        name: "factory",
+        type: "boolean",
+        description: "Build the factory bundle.",
+        default: false,
+    },
+    {
+        name: "slim",
+        alias: "s",
+        type: "boolean",
+        description: `Build a slim bundle excluding: ${slimExclude.join(", ")}`,
+        default: false,
+    },
+    {
+        name: "amd",
+        type: "string",
+        description: "Set the name of the AMD module.",
+    },
+];
 
-build( argv );
+// Initialize yargs
+const argv = yargs(hideBin(process.argv))
+    .version(false)
+    .usage("Usage: $0 [options]")
+    .command("$0", "Build a jQuery bundle", (yargs) => {
+        options.forEach(({ name, alias, type, description, default: def }) => {
+            yargs.option(name, {
+                alias,
+                type,
+                description,
+                default: def,
+            });
+        });
+    })
+    .help()
+    .argv;
+
+// Run the build process with the parsed arguments
+build(argv);
