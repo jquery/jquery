733e62d20328dd3e5b226fd9793b159637d922b8
Timmy Willison <timmywil@users.noreply.github.com>
2025-01-13 22:36:10 -0500

    Tests: migrate test runner to jquery-test-runner
    Closes gh-5604

--- test/runner/browserstack/createAuthHeader.js
+++
@@ -1,7 +0,0 @@
-const textEncoder = new TextEncoder();
-
-export function createAuthHeader( username, accessKey ) {
-	const encoded = textEncoder.encode( `${ username }:${ accessKey }` );
-	const base64 = btoa( String.fromCodePoint.apply( null, encoded ) );
-	return `Basic ${ base64 }`;
-}
