f701292199b4066f8d30b6ff445c8bf79c902ed8
Irah <119393196+harimsd07@users.noreply.github.com>
2026-05-02 20:08:17 +0530

    LegacyFix: modernize src/css.js

--- src/css.js
+++ src/css.js
@@ -18,7 +18,7 @@ import { support } from "./css/support.js";
 import "./core/init.js";
 import "./core/ready.js";
 
-var cssShow = { position: "absolute", visibility: "hidden", display: "block" },
+let cssShow = { position: "absolute", visibility: "hidden", display: "block" },
 	cssNormalTransform = {
 		letterSpacing: "0",
 		fontWeight: "400"
@@ -28,7 +28,7 @@ function setPositiveNumber( _elem, value, subtract ) {
 
 	// Any relative (+/-) values have already been
 	// normalized at this point
-	var matches = rcssNum.exec( value );
+	let matches = rcssNum.exec( value );
 	return matches ?
 
 		// Guard against undefined "subtract", e.g., when used as in cssHooks
@@ -37,7 +37,7 @@ function setPositiveNumber( _elem, value, subtract ) {
 }
 
 function boxModelAdjustment( elem, dimension, box, isBorderBox, styles, computedVal ) {
-	var i = dimension === "width" ? 1 : 0,
+	let i = dimension === "width" ? 1 : 0,
 		extra = 0,
 		delta = 0,
 		marginDelta = 0;
@@ -110,7 +110,7 @@ function boxModelAdjustment( elem, dimension, box, isBorderBox, styles, computed
 function getWidthOrHeight( elem, dimension, extra ) {
 
 	// Start with computed style
-	var styles = getStyles( elem ),
+	let styles = getStyles( elem ),
 
 		// To avoid forcing a reflow, only fetch boxSizing if we need it (gh-4322).
 		// Fake content-box until we know it's needed to know the true value.
@@ -195,7 +195,7 @@ jQuery.extend( {
 		}
 
 		// Make sure that we're working with the right name
-		var ret, type, hooks,
+		let ret, type, hooks,
 			origName = cssCamelCase( name ),
 			isCustomProp = rdoubleDash.test( name ),
 			style = elem.style;
@@ -264,7 +264,7 @@ jQuery.extend( {
 	},
 
 	css: function( elem, name, extra, styles ) {
-		var val, num, hooks,
+		let val, num, hooks,
 			origName = cssCamelCase( name ),
 			isCustomProp = rdoubleDash.test( name );
 
@@ -311,7 +311,7 @@ jQuery.each( [ "height", "width" ], function( _i, dimension ) {
 				// Elements with `display: none` can have dimension info if
 				// we invisibly show them.
 				return jQuery.css( elem, "display" ) === "none" ?
-					swap( elem, cssShow, function() {
+					swap( elem, cssShow, () => {
 						return getWidthOrHeight( elem, dimension, extra );
 					} ) :
 					getWidthOrHeight( elem, dimension, extra );
@@ -319,7 +319,7 @@ jQuery.each( [ "height", "width" ], function( _i, dimension ) {
 		},
 
 		set: function( elem, value, extra ) {
-			var matches,
+			let matches,
 				styles = getStyles( elem ),
 
 				// To avoid forcing a reflow, only fetch boxSizing if we need it (gh-3991)
@@ -356,7 +356,7 @@ jQuery.each( {
 }, function( prefix, suffix ) {
 	jQuery.cssHooks[ prefix + suffix ] = {
 		expand: function( value ) {
-			var i = 0,
+			let i = 0,
 				expanded = {},
 
 				// Assumes a single number if not a string
@@ -379,7 +379,7 @@ jQuery.each( {
 jQuery.fn.extend( {
 	css: function( name, value ) {
 		return access( this, function( elem, name, value ) {
-			var styles, len,
+			let styles, len,
 				map = {},
 				i = 0;
 
