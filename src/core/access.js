5c8e9a435d2895829b6a6cff0d44b4c876b334a9
Irah <119393196+harimsd07@users.noreply.github.com>
2026-05-02 20:07:55 +0530

    LegacyFix: modernize src/core/access.js

--- src/core/access.js
+++ src/core/access.js
@@ -4,7 +4,7 @@ import { toType } from "../core/toType.js";
 // Multifunctional method to get and set values of a collection
 // The value/s can optionally be executed if it's a function
 export function access( elems, fn, key, value, chainable, emptyGet, raw ) {
-	var i = 0,
+	let i = 0,
 		len = elems.length,
 		bulk = key == null;
 
