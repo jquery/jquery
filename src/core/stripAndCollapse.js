07ad1af87bc1944b4a730e7cd379390ae5e4a156
Irah <119393196+harimsd07@users.noreply.github.com>
2026-05-02 20:08:14 +0530

    LegacyFix: modernize src/core/stripAndCollapse.js

--- src/core/stripAndCollapse.js
+++ src/core/stripAndCollapse.js
@@ -3,6 +3,6 @@ import { rnothtmlwhite } from "../var/rnothtmlwhite.js";
 // Strip and collapse whitespace according to HTML spec
 // https://infra.spec.whatwg.org/#strip-and-collapse-ascii-whitespace
 export function stripAndCollapse( value ) {
-	var tokens = value.match( rnothtmlwhite ) || [];
+	let tokens = value.match( rnothtmlwhite ) || [];
 	return tokens.join( " " );
 }
